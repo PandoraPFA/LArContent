61dfc2d95739042d8c766bb5cc8d9e3f4eededdc
John Marshall <marshall@hep.phy.cam.ac.uk>
2013-09-11 14:57:21 +0000

    Cosmetic changes.
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@407 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- include/LArThreeDSeed/ThreeDPairedTracksAlgorithm.h
+++ include/LArThreeDSeed/ThreeDPairedTracksAlgorithm.h
@@ -33,15 +33,19 @@ public:
     };
 
 private:
-    pandora::StatusCode Run();    
+    pandora::StatusCode Run();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
-
-    void CalculateOverlapResult(const pandora::ClusterVector& clusterVectorU, const pandora::ClusterVector& clusterVectorV, const pandora::ClusterVector& clusterVectorW);
+    void CalculateOverlapResult(const pandora::ClusterVector &clusterVectorU, const pandora::ClusterVector &clusterVectorV, const pandora::ClusterVector &clusterVectorW);
     void CalculateOverlapResult(pandora::Cluster *pClusterU, pandora::Cluster *pClusterV, pandora::Cluster *pClusterW);
 
-    bool BuildNextParticle();
+    /**
+     *  @brief  Identify next particle and try to build it; return true if a particle if created
+     * 
+     *  @return boolean
+     */
+    bool BuildNextParticle() const;
 
-    OverlapTensor<TrackOverlapResult> m_overlapTensor;                ///< The overlap tensor
+    OverlapTensor<TrackOverlapResult> m_overlapTensor;          ///< The overlap tensor
 
     std::string                 m_inputClusterListNameU;        ///< The name of the view U cluster list
     std::string                 m_inputClusterListNameV;        ///< The name of the view V cluster list
--- src/LArThreeDSeed/ThreeDPairedTracksAlgorithm.cc
+++ src/LArThreeDSeed/ThreeDPairedTracksAlgorithm.cc
@@ -20,87 +20,56 @@ namespace lar
 
 StatusCode ThreeDPairedTracksAlgorithm::Run()
 {
-    
     m_overlapTensor.Clear();
-    
 
-
-    const ClusterList* pInputClusterListU = NULL;
-    const ClusterList* pInputClusterListV = NULL;
-    const ClusterList* pInputClusterListW = NULL;
-
-    PANDORA_THROW_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_INITIALIZED, !=, PandoraContentApi::GetClusterList(*this,
-	    m_inputClusterListNameU, pInputClusterListU));
-
-    PANDORA_THROW_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_INITIALIZED, !=, PandoraContentApi::GetClusterList(*this,
-            m_inputClusterListNameV, pInputClusterListV));
-        
-    PANDORA_THROW_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_INITIALIZED, !=, PandoraContentApi::GetClusterList(*this,
-            m_inputClusterListNameW, pInputClusterListW));
+    const ClusterList *pInputClusterListU(NULL), *pInputClusterListV(NULL), *pInputClusterListW(NULL);
+    PANDORA_THROW_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_INITIALIZED, !=, PandoraContentApi::GetClusterList(*this, m_inputClusterListNameU, pInputClusterListU));
+    PANDORA_THROW_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_INITIALIZED, !=, PandoraContentApi::GetClusterList(*this, m_inputClusterListNameV, pInputClusterListV));
+    PANDORA_THROW_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_INITIALIZED, !=, PandoraContentApi::GetClusterList(*this, m_inputClusterListNameW, pInputClusterListW));
 
     if ((NULL == pInputClusterListU) || (NULL == pInputClusterListV) || (NULL == pInputClusterListW))
     {
         std::cout << "ThreeDPairedTracksAlgorithm: one or more input cluster lists unavailable" << std::endl;
-        throw StatusCodeException(STATUS_CODE_SUCCESS);
+        return STATUS_CODE_SUCCESS;
     }
 
-
-    ClusterVector availableClustersU, availableClustersV, availableClustersW;
-    ClusterVector particleClustersU, particleClustersV, particleClustersW;
+    ClusterVector availableClustersU, availableClustersV, availableClustersW, particleClustersU, particleClustersV, particleClustersW;
 
     for (ClusterList::const_iterator iter = pInputClusterListU->begin(), iterEnd = pInputClusterListU->end(); iter != iterEnd; ++iter)
     {
-        if( (*iter)->IsAvailable() )
-            availableClustersU.push_back(*iter);
-        else
-            particleClustersU.push_back(*iter);
+        ClusterVector &clusterVector((*iter)->IsAvailable() ? availableClustersU : particleClustersU);
+        clusterVector.push_back(*iter);
     }
 
     for (ClusterList::const_iterator iter = pInputClusterListV->begin(), iterEnd = pInputClusterListV->end(); iter != iterEnd; ++iter)
     {
-        if( (*iter)->IsAvailable() )
-            availableClustersV.push_back(*iter);
-        else
-            particleClustersV.push_back(*iter);
+        ClusterVector &clusterVector((*iter)->IsAvailable() ? availableClustersV : particleClustersV);
+        clusterVector.push_back(*iter);
     }
 
     for (ClusterList::const_iterator iter = pInputClusterListW->begin(), iterEnd = pInputClusterListW->end(); iter != iterEnd; ++iter)
     {
-        if( (*iter)->IsAvailable() )
-            availableClustersW.push_back(*iter);
-        else
-            particleClustersW.push_back(*iter);
+        ClusterVector &clusterVector((*iter)->IsAvailable() ? availableClustersW : particleClustersW);
+        clusterVector.push_back(*iter);
     }
-       
+
     std::sort(availableClustersU.begin(), availableClustersU.end(), LArClusterHelper::SortByNOccupiedLayers);
     std::sort(availableClustersV.begin(), availableClustersV.end(), LArClusterHelper::SortByNOccupiedLayers);
     std::sort(availableClustersW.begin(), availableClustersW.end(), LArClusterHelper::SortByNOccupiedLayers);
-    
+
     std::sort(particleClustersU.begin(), particleClustersU.end(), LArClusterHelper::SortByNOccupiedLayers);
     std::sort(particleClustersV.begin(), particleClustersV.end(), LArClusterHelper::SortByNOccupiedLayers);
     std::sort(particleClustersW.begin(), particleClustersW.end(), LArClusterHelper::SortByNOccupiedLayers);
 
-
-    
     this->CalculateOverlapResult(availableClustersU, availableClustersV, particleClustersW);
+    this->CalculateOverlapResult(particleClustersU, availableClustersV, availableClustersW);
+    this->CalculateOverlapResult(availableClustersU, particleClustersV, availableClustersW);
 
-    
-    this->CalculateOverlapResult(particleClustersU,  availableClustersV, availableClustersW);
-
-    
-    this->CalculateOverlapResult(availableClustersU, particleClustersV,  availableClustersW);
-    
-
-
-
-    while ( this->BuildNextParticle() )
+    while (this->BuildNextParticle())
     {
-
     }
 
-
-
-
+    m_overlapTensor.Clear();
     return STATUS_CODE_SUCCESS;
 }
 
@@ -113,7 +82,7 @@ void ThreeDPairedTracksAlgorithm::CalculateOverlapResult(const ClusterVector& cl
         for (ClusterVector::const_iterator iterV = clusterVectorV.begin(), iterEndV = clusterVectorV.end(); iterV != iterEndV; ++iterV)
         {
             for (ClusterVector::const_iterator iterW = clusterVectorW.begin(), iterEndW = clusterVectorW.end(); iterW != iterEndW; ++iterW)
-	    {
+            {
                 this->CalculateOverlapResult(*iterU, *iterV, *iterW);
             }
         }
@@ -124,47 +93,42 @@ void ThreeDPairedTracksAlgorithm::CalculateOverlapResult(const ClusterVector& cl
 
 void ThreeDPairedTracksAlgorithm::CalculateOverlapResult(Cluster *pClusterU, Cluster *pClusterV, Cluster *pClusterW)
 {
-    if ( ( pClusterU->IsAvailable() == false && pClusterV->IsAvailable() == false ) ||
-         ( pClusterV->IsAvailable() == false && pClusterW->IsAvailable() == false ) ||
-         ( pClusterW->IsAvailable() == false && pClusterU->IsAvailable() == false ) )
+    if ((!pClusterU->IsAvailable() && !pClusterV->IsAvailable()) ||
+        (!pClusterV->IsAvailable() && !pClusterW->IsAvailable()) ||
+        (!pClusterW->IsAvailable() && !pClusterU->IsAvailable()))
+    {
         return;
-
+    }
 
     LArClusterHelper::TwoDSlidingFitResult slidingFitResultU, slidingFitResultV, slidingFitResultW;
     LArClusterHelper::LArTwoDSlidingFit(pClusterU, 10, slidingFitResultU);
     LArClusterHelper::LArTwoDSlidingFit(pClusterV, 10, slidingFitResultV);
     LArClusterHelper::LArTwoDSlidingFit(pClusterW, 10, slidingFitResultW);
 
+    const float minX(std::max(std::min(slidingFitResultU.GetGlobalMinLayerPosition().GetX(), slidingFitResultU.GetGlobalMaxLayerPosition().GetX()),
+        std::max(std::min(slidingFitResultV.GetGlobalMinLayerPosition().GetX(), slidingFitResultV.GetGlobalMaxLayerPosition().GetX()),
+        std::min(slidingFitResultW.GetGlobalMinLayerPosition().GetX(), slidingFitResultW.GetGlobalMaxLayerPosition().GetX()))));
+    const float maxX(std::min(std::max(slidingFitResultU.GetGlobalMinLayerPosition().GetX(), slidingFitResultU.GetGlobalMaxLayerPosition().GetX()),
+        std::min(std::max(slidingFitResultV.GetGlobalMinLayerPosition().GetX(), slidingFitResultV.GetGlobalMaxLayerPosition().GetX()),
+        std::max(slidingFitResultW.GetGlobalMinLayerPosition().GetX(), slidingFitResultW.GetGlobalMaxLayerPosition().GetX()))));
 
-    const float UminX( slidingFitResultU.GetGlobalMinLayerPosition().GetX() );
-    const float UmaxX( slidingFitResultU.GetGlobalMaxLayerPosition().GetX() );
-    const float VminX( slidingFitResultV.GetGlobalMinLayerPosition().GetX() );
-    const float VmaxX( slidingFitResultV.GetGlobalMaxLayerPosition().GetX() );
-    const float WminX( slidingFitResultW.GetGlobalMinLayerPosition().GetX() );
-    const float WmaxX( slidingFitResultW.GetGlobalMaxLayerPosition().GetX() );
-
-    const float minX( std::max( std::min(UminX,UmaxX), std::max( std::min(VminX,VmaxX), std::min(WminX,WmaxX) ) ) );
-    const float maxX( std::min( std::max(UminX,UmaxX), std::min( std::max(VminX,VmaxX), std::max(WminX,WmaxX) ) ) );
-
-    const float xSpanU(UmaxX - UminX);
-    const float xSpanV(VmaxX - VminX);
-    const float xSpanW(WmaxX - WminX);
+    const float xSpanU(slidingFitResultU.GetGlobalMaxLayerPosition().GetX() - slidingFitResultU.GetGlobalMinLayerPosition().GetX());
+    const float xSpanV(slidingFitResultV.GetGlobalMaxLayerPosition().GetX() - slidingFitResultV.GetGlobalMinLayerPosition().GetX());
+    const float xSpanW(slidingFitResultW.GetGlobalMaxLayerPosition().GetX() - slidingFitResultW.GetGlobalMinLayerPosition().GetX());
     const float xOverlap(maxX - minX);
 
     const float nPointsU(std::fabs((xOverlap / xSpanU) * static_cast<float>(slidingFitResultU.GetMaxLayer() - slidingFitResultU.GetMinLayer())));
     const float nPointsV(std::fabs((xOverlap / xSpanV) * static_cast<float>(slidingFitResultV.GetMaxLayer() - slidingFitResultV.GetMinLayer())));
     const float nPointsW(std::fabs((xOverlap / xSpanW) * static_cast<float>(slidingFitResultW.GetMaxLayer() - slidingFitResultW.GetMinLayer())));
-
     const float xPitch(3.f * xOverlap / (nPointsU + nPointsV + nPointsW));
 
-    // Chi2 calculations
     float pseudoChi2Sum(0.f);
     unsigned int nSamplingPoints(0), nMatchedSamplingPoints(0);
 
     for (float x = minX; x < maxX; x += xPitch)
     {
         try
-	{
+        {
             CartesianVector fitUVector(0.f, 0.f, 0.f), fitVVector(0.f, 0.f, 0.f), fitWVector(0.f, 0.f, 0.f);
             slidingFitResultU.GetGlobalFitPosition(x, true, fitUVector);
             slidingFitResultV.GetGlobalFitPosition(x, true, fitVVector);
@@ -190,28 +154,26 @@ void ThreeDPairedTracksAlgorithm::CalculateOverlapResult(Cluster *pClusterU, Clu
 
             if (pseudoChi2 < 3.0)
                 ++nMatchedSamplingPoints;
-	}
+        }
         catch (StatusCodeException &)
-	{
-	}
+        {
+        }
     }
 
-    if ( nMatchedSamplingPoints<5 ) return;
+    if (nMatchedSamplingPoints < 5)
+        return;
 
     m_overlapTensor.SetOverlapResult(pClusterU, pClusterV, pClusterW, TrackOverlapResult(nMatchedSamplingPoints,nSamplingPoints,pseudoChi2Sum));
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-bool ThreeDPairedTracksAlgorithm::BuildNextParticle()
+bool ThreeDPairedTracksAlgorithm::BuildNextParticle() const
 {
-    
-
-    unsigned int bestSamplingPoints(5);
     float bestReducedChi2(0.f);
+    unsigned int bestSamplingPoints(5);
 
     Cluster *pBestClusterU(NULL), *pBestClusterV(NULL), *pBestClusterW(NULL);
-
     const ClusterList &clusterListU(m_overlapTensor.GetClusterListU());
     const ClusterList &clusterListV(m_overlapTensor.GetClusterListV());
     const ClusterList &clusterListW(m_overlapTensor.GetClusterListW());
@@ -222,48 +184,42 @@ bool ThreeDPairedTracksAlgorithm::BuildNextParticle()
         {
             for (ClusterList::const_iterator iterW = clusterListW.begin(), iterWEnd = clusterListW.end(); iterW != iterWEnd; ++iterW)
             {
-	        Cluster* pClusterU = *iterU;
-                Cluster* pClusterV = *iterV;
-                Cluster* pClusterW = *iterW;
+                Cluster *pClusterU = *iterU;
+                Cluster *pClusterV = *iterV;
+                Cluster *pClusterW = *iterW;
 
-                if ( ( pClusterU->IsAvailable() == false && pClusterV->IsAvailable() == false ) ||
-                     ( pClusterV->IsAvailable() == false && pClusterW->IsAvailable() == false ) ||
-                     ( pClusterW->IsAvailable() == false && pClusterU->IsAvailable() == false ) )
-		    continue;
+                if ((!pClusterU->IsAvailable() && !pClusterV->IsAvailable()) ||
+                    (!pClusterV->IsAvailable() && !pClusterW->IsAvailable()) ||
+                    (!pClusterW->IsAvailable() && !pClusterU->IsAvailable()) )
+                {
+                    continue;
+                }
 
-	        try{
+                try
+                {
                     const TrackOverlapResult &overlapResult(m_overlapTensor.GetOverlapResult(pClusterU, pClusterV, pClusterW));
 
-                    if ( (overlapResult.GetNMatchedSamplingPoints() > bestSamplingPoints) ||
-                         (overlapResult.GetNMatchedSamplingPoints() == bestSamplingPoints && overlapResult.GetReducedChi2() < bestReducedChi2) )
-		    {
-		        bestSamplingPoints = overlapResult.GetNMatchedSamplingPoints();
+                    if ((overlapResult.GetNMatchedSamplingPoints() > bestSamplingPoints) ||
+                        ((overlapResult.GetNMatchedSamplingPoints() == bestSamplingPoints) && (overlapResult.GetReducedChi2() < bestReducedChi2)))
+                    {
+                        bestSamplingPoints = overlapResult.GetNMatchedSamplingPoints();
                         bestReducedChi2 = overlapResult.GetReducedChi2();
 
                         pBestClusterU = pClusterU;
                         pBestClusterV = pClusterV;
                         pBestClusterW = pClusterW;
-		    }
-		}
+                    }
+                }
                 catch (StatusCodeException &)
-		{
-		}
-                
+                {
+                }
             }
         }
     }
 
-    if ( pBestClusterU && pBestClusterV && pBestClusterW )
+    if (pBestClusterU && pBestClusterV && pBestClusterW)
     {
-// ClusterList tempListU; tempListU.insert(pBestClusterU);
-// ClusterList tempListV; tempListV.insert(pBestClusterV);
-// ClusterList tempListW; tempListW.insert(pBestClusterW); 
-// PandoraMonitoringApi::SetEveDisplayParameters(0, 0, -1.f, 1.f);
-// PandoraMonitoringApi::VisualizeClusters(&tempListU, "ClusterU", RED);
-// PandoraMonitoringApi::VisualizeClusters(&tempListV, "ClusterV", GREEN);
-// PandoraMonitoringApi::VisualizeClusters(&tempListW, "ClusterW", BLUE);
-// PandoraMonitoringApi::ViewEvent();
-
+        // TODO should we make a new particle here, or add the track pair to the existing particle?
         const PfoList *pPfoList = NULL; std::string pfoListName;
         PANDORA_THROW_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::CreateTemporaryPfoListAndSetCurrent(*this, pPfoList, pfoListName));
 
@@ -274,21 +230,26 @@ bool ThreeDPairedTracksAlgorithm::BuildNextParticle()
         pfoParameters.m_energy = 0.f;
         pfoParameters.m_momentum = CartesianVector(0., 0., 0.);
 
-        if ( pBestClusterU->IsAvailable() ) pfoParameters.m_clusterList.insert(pBestClusterU);
-	if ( pBestClusterV->IsAvailable() ) pfoParameters.m_clusterList.insert(pBestClusterV);
-	if ( pBestClusterW->IsAvailable() ) pfoParameters.m_clusterList.insert(pBestClusterW);
+        if (pBestClusterU->IsAvailable())
+            pfoParameters.m_clusterList.insert(pBestClusterU);
 
-        PANDORA_THROW_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::ParticleFlowObject::Create(*this, pfoParameters));
-    
+        if (pBestClusterV->IsAvailable())
+            pfoParameters.m_clusterList.insert(pBestClusterV);
+
+        if (pBestClusterW->IsAvailable())
+            pfoParameters.m_clusterList.insert(pBestClusterW);
+
+        if (!pfoParameters.m_clusterList.empty())
+            PANDORA_THROW_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::ParticleFlowObject::Create(*this, pfoParameters));
+
         if (!pPfoList->empty())
-	{
+        {
             PANDORA_THROW_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::SavePfoList(*this, m_outputPfoListName));
             PANDORA_THROW_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::ReplaceCurrentPfoList(*this, m_outputPfoListName));
-	}
+        }
 
         return true;
     }
-                
 
     return false;
 }
@@ -298,11 +259,8 @@ bool ThreeDPairedTracksAlgorithm::BuildNextParticle()
 StatusCode ThreeDPairedTracksAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
 {
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "InputClusterListNameU", m_inputClusterListNameU));
-
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "InputClusterListNameV", m_inputClusterListNameV));
-
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "InputClusterListNameW", m_inputClusterListNameW));
-
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "OutputPfoListName", m_outputPfoListName));
 
     return STATUS_CODE_SUCCESS;
