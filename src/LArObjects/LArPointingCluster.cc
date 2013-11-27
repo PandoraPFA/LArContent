e81093a0893fff93244e16f97eab387d0bb31eac
Andrew Blake <blake@hep.phy.cam.ac.uk>
2013-11-27 21:52:09 +0000

    Now use sliding linear fit to find vertex properties of pointing cluster
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@486 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- src/LArObjects/LArPointingCluster.cc
+++ src/LArObjects/LArPointingCluster.cc
@@ -13,6 +13,10 @@
 
 #include "LArObjects/LArPointingCluster.h"
 
+#include "LArHelpers/LArClusterHelper.h"
+
+#include "PandoraMonitoringApi.h"
+
 using namespace pandora;
 
 namespace lar
@@ -26,6 +30,51 @@ LArPointingCluster::LArPointingCluster::Vertex::Vertex(Cluster *const pCluster, 
     m_isInner(useInnerVertex),
     m_nSkippedLayers(nLayersToSkip)
 {
+    LArClusterHelper::TwoDSlidingFitResult slidingFitResult;
+    LArClusterHelper::LArTwoDSlidingFit(pCluster, 10, slidingFitResult);
+
+    const float minLayerZ(slidingFitResult.GetGlobalMinLayerPosition().GetZ());
+    const float maxLayerZ(slidingFitResult.GetGlobalMaxLayerPosition().GetZ());
+
+    const int minLayer(std::min(slidingFitResult.GetMinLayer() + static_cast<int>(nLayersToSkip), slidingFitResult.GetMaxLayer()));
+    const int maxLayer(std::max(slidingFitResult.GetMinLayer(), slidingFitResult.GetMaxLayer() - static_cast<int>(nLayersToSkip)));
+
+    const int innerLayer( (minLayerZ < maxLayerZ) ? minLayer : maxLayer );
+    const int outerLayer( (minLayerZ < maxLayerZ) ? maxLayer : minLayer );
+    const int fitLayer( useInnerVertex ? innerLayer : outerLayer );
+
+    const float rL(slidingFitResult.GetL(fitLayer));
+    
+    CartesianVector vertexPosition(0.f,0.f,0.f);
+    CartesianVector vertexDirection(0.f,0.f,0.f);
+
+    slidingFitResult.GetGlobalFitPosition(rL,vertexPosition);
+    slidingFitResult.GetGlobalFitDirection(rL,vertexDirection);
+
+    m_rms = slidingFitResult.GetRms(rL);
+    m_position = vertexPosition;
+    m_direction = (fitLayer == minLayer) ? vertexDirection : vertexDirection * -1.f;
+    
+
+// ClusterList tempList;
+// Cluster* tempCluster = (Cluster*)(pCluster);
+// tempList.insert(tempCluster);
+// PandoraMonitoringApi::SetEveDisplayParameters(0, 0, -1.f, 1.f);
+// PandoraMonitoringApi::VisualizeClusters(&tempList, "Cluster", GREEN);
+// PandoraMonitoringApi::AddMarkerToVisualization(&m_position, "Position", RED, 1.75);
+// for( unsigned int n=0; n<10; ++n)
+// {
+// float scale = 5.f * (static_cast<float>(n)+0.5f)/10.f;
+// CartesianVector tempPosition = m_position + m_direction * scale;
+// PandoraMonitoringApi::AddMarkerToVisualization(&tempPosition, "Direction", BLUE, 1.75);
+// }
+// PandoraMonitoringApi::ViewEvent();
+
+//
+// KEEP OLD METHOD FOR DEBUGGING
+//
+
+   /*
     static const unsigned int m_numberOfLayersToFit = 30; // TODO, read from settings, probably via LArClusterHelper
     const unsigned int innerLayer(pCluster->GetInnerPseudoLayer());
     const unsigned int outerLayer(pCluster->GetOuterPseudoLayer());
@@ -68,6 +117,7 @@ LArPointingCluster::LArPointingCluster::Vertex::Vertex(Cluster *const pCluster, 
         m_position = fitIntercept + fitDirection * (fitDirection.GetDotProduct(outerCentroid - fitIntercept));
         m_direction = fitDirection;
     }
+    */
 }
 
 } // namespace lar
