ba3dd9fddf9fa827e51e510df49aee6cc39ab53c
John Marshall <marshall@hep.phy.cam.ac.uk>
2013-11-29 14:45:44 +0000

    Temporarily use MC information for low energy track/shower 2D cluster selection.
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@495 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- src/LArReclustering/LowEnergyTrackSplittingAlgorithm.cc
+++ src/LArReclustering/LowEnergyTrackSplittingAlgorithm.cc
@@ -9,7 +9,6 @@
 #include "Pandora/AlgorithmHeaders.h"
 
 #include "LArHelpers/LArClusterHelper.h"
-#include "LArHelpers/LArThreeDHelper.h"
 
 #include "LArReclustering/LowEnergyTrackSplittingAlgorithm.h"
 
@@ -88,172 +87,26 @@ StatusCode LowEnergyTrackSplittingAlgorithm::Run()
         trackLengths.push_back(trackLength);
         trackWidths.push_back(trackWidth);
 
-        ClusterList trackSeedClusters, nonSeedClusters;
-
-        // Input cluster properties
-        const unsigned int nHits(pCluster->GetNCaloHits());
-
-        if (0 == nHits)
-            return STATUS_CODE_FAILURE;
-
-        const float length((pCluster->GetCentroid(pCluster->GetOuterPseudoLayer()) - pCluster->GetCentroid(pCluster->GetInnerPseudoLayer())).GetMagnitude());
-
-        // Reduce to subclusters
-        ClusterList inputClusterList; inputClusterList.insert(pCluster); std::string inputClusterListName;
-        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::InitializeReclustering(*this, TrackList(), inputClusterList, inputClusterListName));
-
-        const ClusterList *pReclusterList = NULL; std::string reclusterListName;
-        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::RunClusteringAlgorithm(*this, m_clusteringAlgorithmName, pReclusterList, reclusterListName));
-
-        for (StringVector::const_iterator iter = m_clusterAssociationAlgorithms.begin(), iterEnd = m_clusterAssociationAlgorithms.end(); iter != iterEnd; ++iter)
+        if ((std::abs(pdgCode) == PROTON) || (std::abs(pdgCode) == MU_MINUS) || (std::abs(pdgCode) == PI_PLUS))
         {
-            PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::RunDaughterAlgorithm(*this, *iter));
+            allTrackSeedClusters.insert(pCluster);
         }
-
-        // Examine subclusters
-        ClusterVector subClusterVector;
-
-        for (ClusterList::const_iterator subIter = pReclusterList->begin(), subIterEnd = pReclusterList->end(); subIter != subIterEnd; ++subIter)
+        else if ((std::abs(pdgCode) == E_MINUS) || (std::abs(pdgCode) == PHOTON))
         {
-            Cluster *pSubCluster = *subIter;
-
-            if (pSubCluster->GetNCaloHits() >= 10)
-            {
-                subClusterVector.push_back(pSubCluster);
-            }
-            else
-            {
-                nonSeedClusters.insert(pSubCluster);
-            }
-        }
-
-        std::sort(subClusterVector.begin(), subClusterVector.end(), LArClusterHelper::SortByNHits);
-
-        // Identify clusters which are branches or prongs
-        ClusterList branches, prongs, parents;
-
-        for (ClusterVector::iterator subIter = subClusterVector.begin(), subIterEnd = subClusterVector.end(); subIter != subIterEnd; ++subIter)
-        {
-            Cluster *pParent = *subIter;
-
-            for (ClusterVector::iterator subIter2 = subIter; subIter2 != subIterEnd; ++subIter2)
-            {
-                Cluster *pDaughter = *subIter2;
-
-                if (pDaughter == pParent)
-                    continue;
-
-                const CartesianVector pInnerCentroid(pParent->GetCentroid(pParent->GetInnerPseudoLayer()));
-                const CartesianVector pOuterCentroid(pParent->GetCentroid(pParent->GetOuterPseudoLayer()));
-                const CartesianVector dInnerCentroid(pDaughter->GetCentroid(pDaughter->GetInnerPseudoLayer()));
-                const CartesianVector dOuterCentroid(pDaughter->GetCentroid(pDaughter->GetOuterPseudoLayer()));
-
-                // prongs
-                const float rsqInnerInner((pInnerCentroid - dInnerCentroid).GetMagnitudeSquared());
-                const float rsqInnerOuter((pInnerCentroid - dOuterCentroid).GetMagnitudeSquared());
-                const float rsqOuterInner((pOuterCentroid - dInnerCentroid).GetMagnitudeSquared());
-                const float rsqOuterOuter((pOuterCentroid - dOuterCentroid).GetMagnitudeSquared()); 
-
-                if (rsqInnerInner < 2.5 * 2.5 || rsqInnerOuter < 2.5 * 2.5 ||
-                    rsqOuterInner < 2.5 * 2.5 || rsqOuterOuter < 2.5 * 2.5) 
-                {
-                    parents.insert(pParent);
-                    prongs.insert(pDaughter);
-                    continue;
-                }
-
-                // branches
-                const float pOuter(LArClusterHelper::GetClosestDistance(pOuterCentroid, pDaughter));
-                const float pInner(LArClusterHelper::GetClosestDistance(pInnerCentroid, pDaughter));
-                const float dOuter(LArClusterHelper::GetClosestDistance(dOuterCentroid, pParent));
-                const float dInner(LArClusterHelper::GetClosestDistance(dInnerCentroid, pParent));
-
-                if ((pInner > 2.5) && (pOuter > 2.5) && ((dInner < 2.5) || (dOuter < 2.5)))
-                {
-                    parents.insert(pParent);
-                    branches.insert(pDaughter);
-                    continue;
-                }
-            }
-        }
-
-        // Select clean clusters
-        unsigned int nParents(0), nProngs(0), nBranches(0), nIsolated(0);
-
-        for (ClusterVector::iterator subIter = subClusterVector.begin(), subIterEnd = subClusterVector.end(); subIter != subIterEnd; ++subIter)
-        {
-            Cluster *pSubCluster = *subIter;
-
-            const bool isParent(parents.count(pSubCluster));
-            const bool isBranch(branches.count(pSubCluster));
-            const bool isProng(prongs.count(pSubCluster));
-            bool isIsolated(false);
-
-            if (isBranch)
-            {
-                ++nBranches;
-            }
-            else if (isProng)
-            {
-                ++nProngs;
-            }
-            else if (isParent)
-            {
-                ++nParents;
-            }
-            else
-            {
-                ++nIsolated;
-                isIsolated = true;
-            }
-
-            const float lengthSquared(LArClusterHelper::GetLengthSquared(pSubCluster));
-            const float hitFraction(static_cast<float>(pSubCluster->GetNCaloHits()) / static_cast<float>(nHits));
-
-            if ((lengthSquared > 200.f) || (!isBranch && (lengthSquared > 100.f)) || (isIsolated && (hitFraction > 0.8f)))
-            {
-                trackSeedClusters.insert(pSubCluster);
-            }
-            else
-            {
-                nonSeedClusters.insert(pSubCluster);
-            }
-        }
-
-        // Decision making
-        unsigned int nSelectedHits(0);
-        for (ClusterList::const_iterator tIter = trackSeedClusters.begin(), tIterEnd = trackSeedClusters.end(); tIter != tIterEnd; ++tIter)
-            nSelectedHits += (*tIter)->GetNCaloHits();
-
-        const float selectedHitFraction(static_cast<float>(nSelectedHits) / static_cast<float>(nHits));
-        const float clustersPerUnitLength(static_cast<float>(subClusterVector.size()) / length);
-        const unsigned int nProtoClusters10(subClusterVector.size());
-
-        std::string chosenListName;
-
-        if (!trackSeedClusters.empty() && (selectedHitFraction > 0.8f) && ((nProtoClusters10 == 1) || (clustersPerUnitLength < 0.1f)))
-        {
-            chosenListName = reclusterListName;
-            allTrackSeedClusters.insert(trackSeedClusters.begin(), trackSeedClusters.end());
-            allNonSeedClusters.insert(nonSeedClusters.begin(), nonSeedClusters.end());
-            LArThreeDHelper::StoreClusterComponents(trackSeedClusters, nonSeedClusters);
-        }
-        else if (!trackSeedClusters.empty())
-        {
-            chosenListName = reclusterListName;
-            allShowerSeedClusters.insert(trackSeedClusters.begin(), trackSeedClusters.end());
-            allNonSeedClusters.insert(nonSeedClusters.begin(), nonSeedClusters.end());
-            LArThreeDHelper::StoreClusterComponents(trackSeedClusters, nonSeedClusters);
+            allShowerSeedClusters.insert(pCluster);
         }
         else
         {
-            chosenListName = inputClusterListName;
             allNonSeedClusters.insert(pCluster);
-            LArThreeDHelper::StoreLoneCluster(pCluster);
         }
-
-        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::EndReclustering(*this, chosenListName));
     }
+
+std::cout << "Cheated Track/Shower Separation " << std::endl;
+PandoraMonitoringApi::SetEveDisplayParameters(0, 0, -1.f, 1.f); 
+PandoraMonitoringApi::VisualizeClusters(&allTrackSeedClusters, "allTrackSeedClusters", RED);
+PandoraMonitoringApi::VisualizeClusters(&allShowerSeedClusters, "allShowerSeedClusters", GREEN);
+PandoraMonitoringApi::VisualizeClusters(&allNonSeedClusters, "allNonSeedClusters", BLUE);
+PandoraMonitoringApi::ViewEvent();
 
     // Cluster output
     if (!allShowerSeedClusters.empty())
@@ -301,12 +154,6 @@ StatusCode LowEnergyTrackSplittingAlgorithm::ReadSettings(const TiXmlHandle xmlH
 
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "NonSeedClusterListName",
         m_nonSeedClusterListName));
-
-    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ProcessAlgorithm(*this, xmlHandle, "Clustering",
-        m_clusteringAlgorithmName));
-
-    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ProcessAlgorithmList(*this, xmlHandle, "associationAlgorithms",
-        m_clusterAssociationAlgorithms));
 
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "OutputTree", m_treeName));
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "OutputFile", m_fileName));
