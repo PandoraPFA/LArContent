9f7b32528b5ff8e0f28a61be11c354eb13bee278
Andrew Blake <blake@hep.phy.cam.ac.uk>
2014-01-10 14:24:14 +0000

    Save the current version of BranchSplittingAlgorithm (not finished yet)
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@535 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- src/LArClusterSplitting/BranchSplittingAlgorithm.cc
+++ src/LArClusterSplitting/BranchSplittingAlgorithm.cc
@@ -20,19 +20,39 @@ namespace lar
 StatusCode BranchSplittingAlgorithm::FindBestSplitPosition(const LArClusterHelper::TwoDSlidingFitResult &branchSlidingFit, const LArClusterHelper::TwoDSlidingFitResult &principalSlidingFit, CartesianVector &branchStartPosition, CartesianVector &branchStartDirection) const
 {
    
+    for (unsigned int principalForward = 0; principalForward < 2; ++principalForward)
+    {
+        const CartesianVector principalVertex(1==principalForward ? principalSlidingFit.GetGlobalMinLayerPosition() : principalSlidingFit.GetGlobalMaxLayerPosition());
+        const CartesianVector principalDirection(1==principalForward ? principalSlidingFit.GetGlobalMinLayerDirection() : principalSlidingFit.GetGlobalMaxLayerDirection() * -1.f);
           
+        float branchDistanceSquared(std::numeric_limits<float>::max());
 
-ClusterList tempList1, tempList2;
-Cluster* pBranchCluster = (Cluster*)(branchSlidingFit.GetCluster());
-Cluster* pReplacementCluster = (Cluster*)(principalSlidingFit.GetCluster());
-tempList1.insert(pBranchCluster);
-tempList2.insert(pReplacementCluster);
-PANDORA_MONITORING_API(SetEveDisplayParameters(false, false, -1, 1));
-PANDORA_MONITORING_API(VisualizeClusters(&tempList1, "BranchCluster", GREEN));
-PANDORA_MONITORING_API(VisualizeClusters(&tempList2, "PrincipalCluster", BLUE));
-PANDORA_MONITORING_API(ViewEvent());
+        try{
+	    branchStartPosition = LArPointingClusterHelper::GetProjectedPosition(principalVertex, principalDirection, branchSlidingFit.GetCluster());
+            branchDistanceSquared = (branchStartPosition - principalVertex).GetMagnitudeSquared();
+        }
+        catch (StatusCodeException &)
+        {
+        }
 
-       
+        if (branchDistanceSquared > 200.f)
+	    continue;
+
+
+// ClusterList tempList1, tempList2;
+// Cluster* pBranchCluster = (Cluster*)(branchSlidingFit.GetCluster());
+// Cluster* pReplacementCluster = (Cluster*)(principalSlidingFit.GetCluster());
+// tempList1.insert(pBranchCluster);
+// tempList2.insert(pReplacementCluster);
+// PANDORA_MONITORING_API(SetEveDisplayParameters(false, false, -1, 1));
+// PANDORA_MONITORING_API(VisualizeClusters(&tempList1, "BranchCluster", GREEN));
+// PANDORA_MONITORING_API(VisualizeClusters(&tempList2, "PrincipalCluster", BLUE));
+// PANDORA_MONITORING_API(AddMarkerToVisualization(&principalVertex, "PrincipalVertex", BLACK, 2.75));
+// PANDORA_MONITORING_API(AddMarkerToVisualization(&branchStartPosition, "SplitPosition", RED, 2.75));
+// PANDORA_MONITORING_API(ViewEvent());
+
+
+    }
              
     return STATUS_CODE_NOT_FOUND;
 }
