17a60423ce35f95da103721d494e4e5348120a7f
John Marshall <marshall@hep.phy.cam.ac.uk>
2014-02-13 15:14:33 +0000

    Cosmic removal modifications
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@607 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- include/LArUtility/ListPreparationAlgorithm.h
+++ include/LArUtility/ListPreparationAlgorithm.h
@@ -33,24 +33,26 @@ private:
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
     /**
-     *  @brief Build separate MCParticleLists for each view
-     */
-    pandora::StatusCode ProcessMCParticles();
-
-    /**
      *  @brief Build separate CaloHitLists for each view
      */
     pandora::StatusCode ProcessCaloHits();
 
+    /**
+     *  @brief Build separate MCParticleLists for each view
+     */
+    pandora::StatusCode ProcessMCParticles();
 
     float           m_mipEquivalentCut;                 ///< Minimum mip equivalent energy for calo hit
-
+    bool            m_onlyAvailableCaloHits;            ///< Whether to only include available calo hits
+    std::string     m_inputCaloHitListName;             ///< The input calo hit list name
     std::string     m_outputCaloHitListNameU;           ///< The output calo hit list name for VIEW_U hits
     std::string     m_outputCaloHitListNameV;           ///< The output calo hit list name for VIEW_V hits
     std::string     m_outputCaloHitListNameW;           ///< The output calo hit list name for VIEW_W hits
     std::string     m_filteredCaloHitListName;          ///< The output calo hit list name for all U, V and W hits
     std::string     m_currentCaloHitListReplacement;    ///< The name of the calo hit list to replace the current list (optional)
 
+    bool            m_mcNeutrinoSelection;              ///< Whether to only select MC particles associated with a neutrino
+    std::string     m_inputMCParticleListName;          ///< The input MC particle list name
     std::string     m_outputMCParticleListNameU;        ///< The output MC particle list name for MC_VIEW_U particles
     std::string     m_outputMCParticleListNameV;        ///< The output MC particle list name for MC_VIEW_V particles
     std::string     m_outputMCParticleListNameW;        ///< The output MC particle list name for MC_VIEW_W particles
