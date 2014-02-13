444891e981aac36866c8953f8e294c3013d87b16
John Marshall <marshall@hep.phy.cam.ac.uk>
2014-02-13 17:11:13 +0000

    Cheated CosmicRayShowerMatching algorithm and new MCParticleHelper functionality.
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@609 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- include/LArTwoDReco/LArCosmicRay/CosmicRayShowerMatchingAlgorithm.h
+++ include/LArTwoDReco/LArCosmicRay/CosmicRayShowerMatchingAlgorithm.h
@@ -31,6 +31,22 @@ public:
 private:
     pandora::StatusCode Run();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
+
+    /**
+     *  @brief  Perform cosmic ray shower matching for a specific cluster in a pfo
+     * 
+     *  @param  clusterListNames the list of cluster list names
+     *  @param  pPfoCluster the pfo cluster of interest
+     *  @param  pPfoList the address of the list containing the cosmic ray pfos
+     */
+    pandora::StatusCode CosmicRayShowerMatching(const pandora::StringVector &clusterListNames, const pandora::Cluster *const pPfoCluster,
+        pandora::ParticleFlowObject *pPfo) const;
+
+    std::string             m_inputPfoListName;         ///< The input pfo list name
+
+    pandora::StringVector   m_inputClusterListNamesU;   ///< The input cluster list names for the u view
+    pandora::StringVector   m_inputClusterListNamesV;   ///< The input cluster list names for the v view
+    pandora::StringVector   m_inputClusterListNamesW;   ///< The input cluster list names for the w view
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
--- src/LArTwoDReco/LArCosmicRay/CosmicRayShowerMatchingAlgorithm.cc
+++ src/LArTwoDReco/LArCosmicRay/CosmicRayShowerMatchingAlgorithm.cc
@@ -8,6 +8,9 @@
 
 #include "Pandora/AlgorithmHeaders.h"
 
+#include "LArHelpers/LArMCParticleHelper.h"
+#include "LArHelpers/LArThreeDHelper.h"
+
 #include "LArTwoDReco/LArCosmicRay/CosmicRayShowerMatchingAlgorithm.h"
 
 using namespace pandora;
@@ -17,13 +20,96 @@ namespace lar
 
 StatusCode CosmicRayShowerMatchingAlgorithm::Run()
 {
+    const PfoList *pPfoList = NULL;
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetPfoList(*this, m_inputPfoListName, pPfoList));
+
+    for (PfoList::const_iterator iter = pPfoList->begin(), iterEnd = pPfoList->end(); iter != iterEnd; ++iter)
+    {
+        ParticleFlowObject *pPfo = *iter;
+        const ClusterList &pfoClusterList(pPfo->GetClusterList());
+
+        for (ClusterList::const_iterator cIter = pfoClusterList.begin(), cIterEnd = pfoClusterList.end(); cIter != cIterEnd; ++cIter)
+        {
+            const Cluster *const pPfoCluster = *cIter;
+            const HitType hitType(LArThreeDHelper::GetClusterHitType(pPfoCluster));
+
+            if ((VIEW_U != hitType) && (VIEW_V != hitType) && (VIEW_W != hitType))
+            {
+                std::cout << "CosmicRayShowerMatchingAlgorithm: Encountered unexpected hit type " << std::endl;
+                return STATUS_CODE_INVALID_PARAMETER;
+            }
+
+            const StringVector &clusterListNames((VIEW_U == hitType) ? m_inputClusterListNamesU :
+                (VIEW_V == hitType) ? m_inputClusterListNamesV : m_inputClusterListNamesW);
+
+            PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, this->CosmicRayShowerMatching(clusterListNames, pPfoCluster, pPfo));
+        }
+    }
+
     return STATUS_CODE_SUCCESS;
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-StatusCode CosmicRayShowerMatchingAlgorithm::ReadSettings(const TiXmlHandle /*xmlHandle*/)
+StatusCode CosmicRayShowerMatchingAlgorithm::CosmicRayShowerMatching(const StringVector &clusterListNames, const Cluster *const pPfoCluster,
+    ParticleFlowObject *pPfo) const
 {
+    try
+    {
+        const MCParticle *pPfoMCParticle(MCParticleHelper::GetMainMCParticle(pPfoCluster));
+        const MCParticle *pPfoParentMCParticle(LArMCParticleHelper::GetParentMCParticle(pPfoMCParticle));
+
+        for (StringVector::const_iterator sIter = clusterListNames.begin(), sIterEnd = clusterListNames.end(); sIter != sIterEnd; ++sIter)
+        {
+            const ClusterList *pClusterList = NULL;
+            PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetClusterList(*this, *sIter, pClusterList));
+
+            for (ClusterList::const_iterator cIter = pClusterList->begin(), cIterEnd = pClusterList->end(); cIter != cIterEnd; ++cIter)
+            {
+                Cluster *pCluster = *cIter;
+
+                if (!pCluster->IsAvailable() || (pPfoCluster == pCluster))
+                    continue;
+
+                try
+                {
+                    const MCParticle *pMCParticle(MCParticleHelper::GetMainMCParticle(pCluster));
+                    const MCParticle *pParentMCParticle(LArMCParticleHelper::GetParentMCParticle(pMCParticle));
+
+                    if (pPfoParentMCParticle == pParentMCParticle)
+                    {
+                        PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::AddClusterToPfo(*this, pPfo, pCluster));
+                    }
+                }
+                catch (StatusCodeException &)
+                {
+                }
+            }
+        }
+    }
+    catch (StatusCodeException &)
+    {
+    }
+
+    return STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode CosmicRayShowerMatchingAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
+{
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle,
+        "InputPfoListName", m_inputPfoListName));
+
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadVectorOfValues(xmlHandle,
+        "InputClusterListNamesU", m_inputClusterListNamesU));
+
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadVectorOfValues(xmlHandle,
+        "InputClusterListNamesV", m_inputClusterListNamesV));
+
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadVectorOfValues(xmlHandle,
+        "InputClusterListNamesW", m_inputClusterListNamesW));
+
     return STATUS_CODE_SUCCESS;
 }
 
