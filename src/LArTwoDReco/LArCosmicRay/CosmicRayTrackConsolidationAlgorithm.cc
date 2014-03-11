bd293571b3a922db02d0d6a08d9fdceacfebf5a9
Andrew Blake <blake@hep.phy.cam.ac.uk>
2014-03-11 11:04:42 +0000

    Skeleton CosmicRayTrackConsolidationAlgorithm - need to fill in algorithm
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@667 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- src/LArTwoDReco/LArCosmicRay/CosmicRayTrackConsolidationAlgorithm.cc
+++ src/LArTwoDReco/LArCosmicRay/CosmicRayTrackConsolidationAlgorithm.cc
@@ -10,6 +10,8 @@
 
 #include "LArTwoDReco/LArCosmicRay/CosmicRayTrackConsolidationAlgorithm.h"
 
+#include "LArHelpers/LArClusterHelper.h"
+
 using namespace pandora;
 
 namespace lar
@@ -17,7 +19,16 @@ namespace lar
 
 StatusCode CosmicRayTrackConsolidationAlgorithm::Run()
 {
-    std::cout << "CosmicRayTrackConsolidationAlgorithm::Run()" << std::endl;
+    const ClusterList *pClusterList = NULL;
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetCurrentClusterList(*this, pClusterList));
+
+
+  
+
+    // TODO
+
+
+
 
     return STATUS_CODE_SUCCESS;
 }
@@ -26,6 +37,9 @@ StatusCode CosmicRayTrackConsolidationAlgorithm::Run()
 
 StatusCode CosmicRayTrackConsolidationAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
 {
+    m_minTrackLength = 10.f; // cm
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+        "MinTrackLength", m_minTrackLength));
 
     return STATUS_CODE_SUCCESS;
 }
