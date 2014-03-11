5c8800d80b679f4fa0cfb788fb7244732568a7e7
Andrew Blake <blake@hep.phy.cam.ac.uk>
2014-03-11 12:30:27 +0000

    Adding cheater for CosmicRayShowerMatchingAlgorithm
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@675 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

---
+++ include/LArCheating/CheatingCosmicRayShowerMatchingAlg.h
@@ -0,0 +1,70 @@
+/**
+ *  @file   LArContent/include/LArCheating/CheatingCosmicRayShowerMatchingAlg.h
+ * 
+ *  @brief  Header file for the cosmic ray shower matching cheater class.
+ * 
+ *  $Log: $
+ */
+#ifndef LAR_CHEATING_COSMIC_RAY_SHOWER_MATCHING_ALG_H
+#define LAR_CHEATING_COSMIC_RAY_SHOWER_MATCHING_ALG_H 1
+
+#include "Pandora/Algorithm.h"
+
+namespace lar
+{
+
+/**
+ *  @brief  CheatingCosmicRayShowerMatchingAlg class
+ */
+class CheatingCosmicRayShowerMatchingAlg : public pandora::Algorithm
+{
+public:
+    /**
+     *  @brief  Factory class for instantiating algorithm
+     */
+    class Factory : public pandora::AlgorithmFactory
+    {
+    public:
+        pandora::Algorithm *CreateAlgorithm() const;
+    };
+
+private:
+    pandora::StatusCode Run();
+    pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
+
+    /**
+     *  @brief  Perform cosmic ray shower matching for a specific cluster in a pfo
+     * 
+     *  @param  clusterListNames the list of cluster list names
+     *  @param  pPfoCluster the pfo cluster of interest
+     *  @param  pPfoList the address of the list containing the cosmic ray pfos
+     */
+    pandora::StatusCode CosmicRayShowerMatching(const pandora::StringVector &clusterListNames, const pandora::Cluster *const pPfoCluster,
+        pandora::ParticleFlowObject *pPfo) const;
+
+    /**
+     *  @brief  Sort pfos by number of constituent hits
+     * 
+     *  @param  pLhs address of first pfo
+     *  @param  pRhs address of second pfo
+     */
+    static bool SortPfosByNHits(const pandora::ParticleFlowObject *const pLhs, const pandora::ParticleFlowObject *const pRhs);
+
+    std::string             m_inputPfoListName;           ///< The input pfo list name
+    std::string             m_inputCosmicRayPfoListName;  ///< The input pfo list name (identified cosmics)
+
+    pandora::StringVector   m_inputClusterListNamesU;     ///< The input cluster list names for the u view
+    pandora::StringVector   m_inputClusterListNamesV;     ///< The input cluster list names for the v view
+    pandora::StringVector   m_inputClusterListNamesW;     ///< The input cluster list names for the w view
+};
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline pandora::Algorithm *CheatingCosmicRayShowerMatchingAlg::Factory::CreateAlgorithm() const
+{
+    return new CheatingCosmicRayShowerMatchingAlg();
+}
+
+} // namespace lar
+
+#endif // #ifndef LAR_CHEATING_COSMIC_RAY_SHOWER_MATCHING_ALG_H
