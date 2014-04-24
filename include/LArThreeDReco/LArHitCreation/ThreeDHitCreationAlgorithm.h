ad62739cccfd6edb50e2962cf209f357f036815c
John Marshall <marshall@hep.phy.cam.ac.uk>
2014-04-24 11:29:47 +0000

    Add algorithm skeleton.
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@777 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- include/LArThreeDReco/LArHitCreation/ThreeDHitCreationAlgorithm.h
+++ include/LArThreeDReco/LArHitCreation/ThreeDHitCreationAlgorithm.h
@@ -32,9 +32,50 @@ private:
     pandora::StatusCode Run();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
+    /**
+     *  @brief  Get the primary u, v and w clusters in a provided pfo
+     * 
+     *  @param  pPfo address of the pfo
+     *  @param  pClusterU to receive the address of cluster u
+     *  @param  pClusterV to receive the address of cluster v
+     *  @param  pClusterW to receive the address of cluster w
+     */
+    void GetClusters(const pandora::ParticleFlowObject *const pPfo, pandora::Cluster *&pClusterU, pandora::Cluster *&pClusterV, pandora::Cluster *&pClusterW) const;
+
+    /**
+     *  @brief  Create three dimensional hits, using an input list of two dimensional hits and two associated sliding fit results
+     * 
+     *  @param  inputTwoDHits the list of input two dimensional hits
+     *  @param  fitResult1 the first sliding fit result
+     *  @param  fitResult2 the second sliding fit result
+     *  @param  newThreeDHits to receive the new three dimensional hits
+     *  @param  omittedTwoDHits to receive the two dimensional hits for which no three dimensional hits could be created
+     */
+    void CreateThreeDHits(const pandora::CaloHitList &inputTwoDHits, const TwoDSlidingFitResult &fitResult1, const TwoDSlidingFitResult &fitResult2,
+        pandora::CaloHitList &newThreeDHits, pandora::CaloHitList &omittedTwoDHits) const;
+
+    /**
+     *  @brief  Create a new three dimensional cluster, using a list of provided three dimensional hits
+     * 
+     *  @param  caloHitList the list of three dimensional hits
+     *  @param  pCluster to receive the address of the new cluster
+     */
+    void CreateThreeDCluster(const pandora::CaloHitList &caloHitList, pandora::Cluster *&pCluster) const;
+
+    /**
+     *  @brief  Create new three dimensional hits for those hits previously omitted, using extrapolation of the three dimensional cluster
+     * 
+     *  @param  omittedHits the list of previously omitted two dimensional hits
+     *  @param  pCluster address of the three dimensional cluster
+     *  @param  extrapolatedHits to receive the list of extrapolated hits
+     */
+    void CreateExtrapolatedHits(const pandora::CaloHitList &omittedHits, pandora::Cluster *pCluster, pandora::CaloHitList &extrapolatedHits) const;
+
     std::string     m_inputPfoListName;         ///< The name of the input pfo list
     std::string     m_outputCaloHitListName;    ///< The name of the output calo hit list
     std::string     m_outputClusterListName;    ///< The name of the output cluster list
+
+    unsigned int    m_slidingFitWindow;         ///< The layer window for the sliding linear fits
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
--- src/LArThreeDReco/LArHitCreation/ThreeDHitCreationAlgorithm.cc
+++ src/LArThreeDReco/LArHitCreation/ThreeDHitCreationAlgorithm.cc
@@ -8,6 +8,11 @@
 
 #include "Pandora/AlgorithmHeaders.h"
 
+#include "LArHelpers/LArClusterHelper.h"
+#include "LArHelpers/LArThreeDHelper.h"
+
+#include "LArObjects/LArTwoDSlidingFitResult.h"
+
 #include "LArThreeDReco/LArHitCreation/ThreeDHitCreationAlgorithm.h"
 
 using namespace pandora;
@@ -24,36 +29,33 @@ StatusCode ThreeDHitCreationAlgorithm::Run()
     {
         try
         {
-//            Pfo *pPfo = *iter;
-//
-//            Cluster *pClusterU(NULL), *pClusterV(NULL), *pClusterW(NULL);
-//            this->GetClusters(pPfo, pClusterU, pClusterV, pClusterW);
-//
-//            TwoDSlidingFitResult slidingFitResultU, slidingFitResultV, slidingFitResultW;
-//            LArClusterHelper::LArTwoDSlidingFit(pClusterU, m_slidingFitWindow, slidingFitResultU);
-//            LArClusterHelper::LArTwoDSlidingFit(pClusterV, m_slidingFitWindow, slidingFitResultV);
-//            LArClusterHelper::LArTwoDSlidingFit(pClusterW, m_slidingFitWindow, slidingFitResultW);
-//
-//            CaloHitList caloHitListU, caloHitListV, caloHitListW;
-//            pClusterU->GetOrderedCaloHitList().GetCaloHitList(caloHitListU);
-//            pClusterV->GetOrderedCaloHitList().GetCaloHitList(caloHitListV);
-//            pClusterW->GetOrderedCaloHitList().GetCaloHitList(caloHitListW);
-//
-//            // TODO, need a way of obtaining address of newly created hits
-//            CaloHitList newThreeDHits, ommittedTwoDHits;
-//            this->CreateThreeDHits(caloHitListU, slidingFitResultV, slidingFitResultW, newThreeDHits, ommittedTwoDHits);
-//            this->CreateThreeDHits(caloHitListU, slidingFitResultV, slidingFitResultW, newThreeDHits, ommittedTwoDHits);
-//            this->CreateThreeDHits(caloHitListU, slidingFitResultV, slidingFitResultW, newThreeDHits, ommittedTwoDHits);
-//
-//            Cluster *pCluster3D(NULL);
-//            this->CreateCluster(pPfo, newThreeDHits, pCluster3D);
-//
-//            CaloHitList extrapolatedHits;
-//            this->GetExtrapolatedHits(pCluster3D, extrapolatedHits);
-//
-//            PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::AddToCluster(*this, pCluster3D, &extrapolatedHits));
-//
-//            // Sort out lists
+            ParticleFlowObject *pPfo = *iter;
+
+            Cluster *pClusterU(NULL), *pClusterV(NULL), *pClusterW(NULL);
+            this->GetClusters(pPfo, pClusterU, pClusterV, pClusterW);
+
+            TwoDSlidingFitResult slidingFitResultU, slidingFitResultV, slidingFitResultW;
+            LArClusterHelper::LArTwoDSlidingFit(pClusterU, m_slidingFitWindow, slidingFitResultU);
+            LArClusterHelper::LArTwoDSlidingFit(pClusterV, m_slidingFitWindow, slidingFitResultV);
+            LArClusterHelper::LArTwoDSlidingFit(pClusterW, m_slidingFitWindow, slidingFitResultW);
+
+            CaloHitList caloHitListU, caloHitListV, caloHitListW;
+            pClusterU->GetOrderedCaloHitList().GetCaloHitList(caloHitListU);
+            pClusterV->GetOrderedCaloHitList().GetCaloHitList(caloHitListV);
+            pClusterW->GetOrderedCaloHitList().GetCaloHitList(caloHitListW);
+
+            CaloHitList newThreeDHits, ommittedTwoDHits;
+            this->CreateThreeDHits(caloHitListU, slidingFitResultV, slidingFitResultW, newThreeDHits, ommittedTwoDHits);
+            this->CreateThreeDHits(caloHitListV, slidingFitResultU, slidingFitResultW, newThreeDHits, ommittedTwoDHits);
+            this->CreateThreeDHits(caloHitListW, slidingFitResultU, slidingFitResultV, newThreeDHits, ommittedTwoDHits);
+
+            Cluster *pCluster3D(NULL);
+            this->CreateThreeDCluster(newThreeDHits, pCluster3D);
+            PANDORA_THROW_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::AddToPfo(*this, pPfo, pCluster3D));
+
+            CaloHitList extrapolatedHits;
+            this->CreateExtrapolatedHits(ommittedTwoDHits, pCluster3D, extrapolatedHits);
+            PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::AddToCluster(*this, pCluster3D, &extrapolatedHits));
         }
         catch (StatusCodeException &)
         {
@@ -65,11 +67,63 @@ StatusCode ThreeDHitCreationAlgorithm::Run()
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
+void ThreeDHitCreationAlgorithm::GetClusters(const ParticleFlowObject *const pPfo, Cluster *&pClusterU, Cluster *&pClusterV, Cluster *&pClusterW) const
+{
+    pClusterU = NULL; pClusterV = NULL; pClusterW = NULL;
+    const ClusterList &clusterList(pPfo->GetClusterList());
+
+    if (3 != clusterList.size())
+        throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+
+    for (ClusterList::const_iterator iter = clusterList.begin(), iterEnd = clusterList.end(); iter != iterEnd; ++iter)
+    {
+        Cluster *pCluster(*iter);
+        const HitType hitType(LArThreeDHelper::GetClusterHitType(pCluster));
+
+        if ((TPC_VIEW_U != hitType) && (TPC_VIEW_V == hitType) && (TPC_VIEW_W != hitType))
+            throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+
+        Cluster *&pTargetCluster((TPC_VIEW_U == hitType) ? pClusterU : (TPC_VIEW_V == hitType) ? pClusterV : pClusterW);
+        pTargetCluster = pCluster;
+    }
+
+    if ((NULL == pClusterU) || (NULL == pClusterV) || (NULL == pClusterW))
+        throw StatusCodeException(STATUS_CODE_NOT_FOUND);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void ThreeDHitCreationAlgorithm::CreateThreeDHits(const CaloHitList &inputTwoDHits, const TwoDSlidingFitResult &fitResult1, const TwoDSlidingFitResult &fitResult2,
+    CaloHitList &newThreeDHits, CaloHitList &ommittedTwoDHits) const
+{
+    
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void ThreeDHitCreationAlgorithm::CreateThreeDCluster(const CaloHitList &caloHitList, Cluster *&pCluster) const
+{
+    
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void ThreeDHitCreationAlgorithm::CreateExtrapolatedHits(const CaloHitList &ommittedHits, Cluster *pCluster, CaloHitList &extrapolatedHits) const
+{
+    
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
 StatusCode ThreeDHitCreationAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
 {
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "InputPfoListName", m_inputPfoListName));
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "OutputCaloHitListName", m_outputCaloHitListName));
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "OutputClusterListName", m_outputClusterListName));
+
+    m_slidingFitWindow = 20;
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+        "SlidingFitWindow", m_slidingFitWindow));
 
     return STATUS_CODE_SUCCESS;
 }
