d29a4a96ea6453b5d16132c54e03f20064080a1e
John Marshall <marshall@hep.phy.cam.ac.uk>
2014-04-28 17:30:05 +0000

    Implementation of TrackSplitting tensor tool. Logic simplification in MissingTrackSegment tool.
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@785 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- include/LArThreeDReco/LArTrackMatching/TrackSplittingTool.h
+++ include/LArThreeDReco/LArTrackMatching/TrackSplittingTool.h
@@ -29,10 +29,32 @@ public:
     };
 
 private:
+    /**
+     *  @brief  Particle class
+     */
+    class Particle
+    {
+    public:
+        /**
+         *  @brief  Constructor
+         * 
+         *  @param  element the tensor element
+         */
+        Particle(const TensorType::Element &element);
+
+        pandora::Cluster   *m_pLongCluster;             ///< Address of the long cluster
+        pandora::Cluster   *m_pCluster1;                ///< Address of short cluster in view 1
+        pandora::Cluster   *m_pCluster2;                ///< Address of short cluster in view 2
+        float               m_longMinX;                 ///< The min x coordinate of the long cluster
+        float               m_longMaxX;                 ///< The max x coordinate of the long cluster
+        float               m_short1MinX;               ///< The min x coordinate of short cluster 1
+        float               m_short1MaxX;               ///< The max x coordinate of short cluster 1
+        float               m_short2MinX;               ///< The min x coordinate of short cluster 2
+        float               m_short2MaxX;               ///< The max x coordinate of short cluster 2
+    };
+
     bool Run(ThreeDTransverseTracksAlgorithm *pAlgorithm, TensorType &overlapTensor);
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
-
-    typedef std::map<pandora::Cluster*, pandora::CartesianPointList> SplitPositionMap;
 
     /**
      *  @brief  Find remaining tracks, hidden by spurious track segments (and maybe other ambiguities) in the tensor
@@ -67,6 +89,11 @@ private:
     unsigned int    m_minMatchedSamplingPoints;         ///< The min number of matched sampling points for particle creation
     float           m_minXOverlapFraction;              ///< The min x overlap fraction (between long clusters and short cluster vs. shared overlap)
     unsigned int    m_minMatchedSamplingPointRatio;     ///< The min ratio between 1st and 2nd highest msps for simple ambiguity resolution
+
+    float           m_maxShortDeltaXFraction;           ///< Max x distance between ends of two short clusters (measured as fraction of long cluster x length)
+    float           m_maxAbsoluteShortDeltaX;           ///< Max x distance between ends of two short clusters (measured as an absolute distance)
+    float           m_minLongDeltaXFraction;            ///< Min x distance between ends of short and long clusters (measured as fraction of long cluster x length)
+    float           m_minAbsoluteLongDeltaX;            ///< Min x distance between ends of short and long clusters (measured as an absolute distance)
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
