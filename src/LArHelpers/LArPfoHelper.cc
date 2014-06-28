70ee1ed48041f053dbad65e8c7dd5ba88b5d811d
Andrew Blake <blake@hep.phy.cam.ac.uk>
2014-06-28 15:33:45 +0000

    Filling LArPfoHelper
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@911 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- src/LArHelpers/LArPfoHelper.cc
+++ src/LArHelpers/LArPfoHelper.cc
@@ -22,7 +22,116 @@ using namespace pandora;
 namespace lar
 {
 
+//------------------------------------------------------------------------------------------------------------------------------------------
+ 
+void LArPfoHelper::GetClusters(const PfoVector &pfoVector, const HitType hitType, ClusterVector &clusterVector)
+{
+    for (PfoVector::const_iterator pIter = pfoVector.begin(), pIterEnd = pfoVector.end(); pIter != pIterEnd; ++pIter)
+    {
+        const ParticleFlowObject *pPfo = *pIter;
+        LArPfoHelper::GetClusters(pPfo, hitType, clusterVector);
+    }
+}
 
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void LArPfoHelper::GetClusters(const ParticleFlowObject *pPfo, const HitType hitType, ClusterVector &clusterVector)
+{
+    const ClusterList &pfoClusterList = pPfo->GetClusterList();
+    for (ClusterList::const_iterator cIter = pfoClusterList.begin(), cIterEnd = pfoClusterList.end(); cIter != cIterEnd; ++cIter)
+    {
+        Cluster *pPfoCluster = *cIter;
+
+        if (hitType != LArClusterHelper::GetClusterHitType(pPfoCluster))
+            continue;
+
+        clusterVector.push_back(pPfoCluster);
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+float LArPfoHelper::GetClosestDistance(const ParticleFlowObject *const pPfo, const Cluster *const pCluster)
+{
+    const HitType hitType(LArClusterHelper::GetClusterHitType(pCluster));
+
+    ClusterVector clusterVector;
+    LArPfoHelper::GetClusters(pPfo, hitType, clusterVector);
+
+    if (clusterVector.empty())
+        throw StatusCodeException(STATUS_CODE_NOT_FOUND);
+
+    float bestDistance(std::numeric_limits<float>::max());
+
+    for (ClusterVector::const_iterator iter = clusterVector.begin(), iterEnd = clusterVector.end(); iter != iterEnd; ++iter)
+    {
+        const Cluster* pPfoCluster = *iter;
+        const float thisDistance(LArClusterHelper::GetClosestDistance(pCluster, pPfoCluster));
+
+        if (thisDistance < bestDistance)
+            bestDistance = thisDistance;
+    }
+
+    return bestDistance;
+}
+  
+    
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+float LArPfoHelper::GetTwoDSeparation(const ParticleFlowObject *const pPfo1, const ParticleFlowObject *const pPfo2)
+{
+    ClusterVector clusterVectorU1, clusterVectorV1, clusterVectorW1;
+    ClusterVector clusterVectorU2, clusterVectorV2, clusterVectorW2;
+
+    LArPfoHelper::GetClusters(pPfo1, TPC_VIEW_U, clusterVectorU1);
+    LArPfoHelper::GetClusters(pPfo1, TPC_VIEW_V, clusterVectorV1);
+    LArPfoHelper::GetClusters(pPfo1, TPC_VIEW_W, clusterVectorW1);    
+
+    LArPfoHelper::GetClusters(pPfo2, TPC_VIEW_U, clusterVectorU2);
+    LArPfoHelper::GetClusters(pPfo2, TPC_VIEW_V, clusterVectorV2);
+    LArPfoHelper::GetClusters(pPfo2, TPC_VIEW_W, clusterVectorW2);
+
+    float numViews(0.f);
+    float distanceSquared(0.f);
+
+    if (!clusterVectorU1.empty() && !clusterVectorU2.empty())
+    {
+        distanceSquared += LArClusterHelper::GetClosestDistance(clusterVectorU1, clusterVectorU2);
+        numViews += 1.f;
+    }
+
+    if (!clusterVectorV1.empty() && !clusterVectorV2.empty())
+    {
+        distanceSquared += LArClusterHelper::GetClosestDistance(clusterVectorV1, clusterVectorV2);
+        numViews += 1.f;
+    }
+
+    if (!clusterVectorW1.empty() && !clusterVectorW2.empty())
+    {
+        distanceSquared += LArClusterHelper::GetClosestDistance(clusterVectorW1, clusterVectorW2);
+        numViews += 1.f;
+    }
+
+    if (numViews < std::numeric_limits<float>::epsilon())
+        throw StatusCodeException(STATUS_CODE_NOT_FOUND);
+
+    return std::sqrt(distanceSquared / numViews);
+}
+ 
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+float LArPfoHelper::GetThreeDSeparation(const ParticleFlowObject *const pPfo1, const ParticleFlowObject *const pPfo2)
+{
+    ClusterVector clusterVector1, clusterVector2;
+
+    LArPfoHelper::GetClusters(pPfo1, TPC_3D, clusterVector1);
+    LArPfoHelper::GetClusters(pPfo2, TPC_3D, clusterVector2);
+
+    if (clusterVector1.empty() || clusterVector2.empty())
+        throw StatusCodeException(STATUS_CODE_NOT_FOUND);
+
+    return LArClusterHelper::GetClosestDistance(clusterVector1, clusterVector2);
+}
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
