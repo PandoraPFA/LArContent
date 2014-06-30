412778c94980ae70cff8504b1cf821dc5633f7b3
Andrew Blake <blake@hep.phy.cam.ac.uk>
2014-06-30 19:32:28 +0000

    Latest version of CosmicRayShowerMatchingAlgorithm
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@923 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- include/LArThreeDReco/LArCosmicRay/CosmicRayShowerMatchingAlgorithm.h
+++ include/LArThreeDReco/LArCosmicRay/CosmicRayShowerMatchingAlgorithm.h
@@ -56,19 +56,17 @@ private:
 
     typedef std::vector<Particle> ParticleList;
 
-    
-
-    typedef std::map<const pandora::Cluster*, const pandora::ParticleFlowObject*> ClusterToPfoMap;
-
     typedef std::map<const pandora::Cluster*, pandora::ClusterList> ClusterAssociationMap;
 
+    /**
+     *  @brief Run cluster merging
+     */
+    void MergeClusters() const;
 
-
-    void MergeClusters();
-
-
-    void MatchClusters();
-
+    /**
+     *  @brief Run cluster matching (which builds new Pfos)
+     */
+    void MatchClusters() const;
 
     /**
      *  @brief  Get a vector of Pfos in the provided input Pfo lists
@@ -86,30 +84,34 @@ private:
      */
     void GetClusters(const std::string inputClusterListName, pandora::ClusterVector &clusterVector) const;
 
-
     /**
-     *  @brief  Generate a mapping between pfos and clusters
+     *  @brief  Select candidate showers and delta rays for matching (and don't select tracks)
      *
-     *  @param  pfoVector the input vector of Pfos
-     *  @param  clusterToPfoMap the output mapping from clusters to pfos
+     *  @param  inputVector the input vector of all Pfos
+     *  @param  outputVector the output vector of selected Pfos
      */
-    void GetPfoClusterMap(const pandora::PfoVector &pfoVector, ClusterToPfoMap &clusterToPfoMap) const;
- 
-
-
     void SelectPfos(const pandora::PfoVector &inputVector, pandora::PfoVector &outputVector) const;
 
-
-
- 
-
     /**
-     *  @brief  Fill list of candidate particles from input vector of Pfos
+     *  @brief Delete Pfos
      *
      *  @param  pfoVector the input vector of Pfos
-     *  @param  particleList the ouput list of candidate particles
      */
-    void MatchViews(const pandora::PfoVector &pfoVector, ParticleList &particleList) const;
+    void DeletePfos(const pandora::PfoVector &pfoVector) const;
+
+    /**
+     *  @brief  Merge clusters together based on results of matching
+     *
+     *  @param  particleList the input list of cluster matches
+     */
+    void MergeClusters(const ParticleList &particleList) const;
+
+    /**
+     *  @brief  Build Pfos based on matched clusters
+     *
+     *  @param  particleList the input list of cluster matches
+     */
+    void MatchClusters(const ParticleList &particleList) const;
 
     /**
      *  @brief  Fill list of candidate particles from input vectors of clusters
@@ -122,7 +124,7 @@ private:
     void MatchViews(const pandora::ClusterVector &clusterVectorU, const pandora::ClusterVector &clusterVectorV, 
         const pandora::ClusterVector &clusterVectorW, ParticleList &particleList) const;
 
-    /**
+   /**
      *  @brief  Match a UVW combination of clusters
      *
      *  @param  pClusterU the address of the cluster in the U view
@@ -135,12 +137,12 @@ private:
         const pandora::Cluster *const pClusterW) const;
 
     /**
-     *  @brief  Build map of cluster associations from list of candidate particles
+     *  @brief Identify good merges from list of matched clusters
      *
-     *  @param  particleList the list of candidate particles
-     *  @param  clusterAssociationMap the output map of cluster associations
-     */
-    void BuildAssociationMap(const ParticleList &particleList, ClusterAssociationMap &clusterAssociationMap) const;
+     *  @param particleList the input list of candidate particles
+     *  @param clusterAssociationMap the output map of merges between clusters
+     */   
+    void IdentifyMerges(const ParticleList &particleList, ClusterAssociationMap &clusterAssociationMap) const;
 
     /**
      *  @brief  Build map of cluster associations from two clusters in the same view
@@ -149,44 +151,65 @@ private:
      *  @param  pCluster2 the address of the second cluster
      *  @param  clusterAssociationMap the output map of cluster associations
      */
-    void BuildAssociationMap(const pandora::Cluster *const pCluster1, const pandora::Cluster *const pCluster2, 
+    void CheckAssociation(const pandora::Cluster *const pCluster1, const pandora::Cluster *const pCluster2, 
         ClusterAssociationMap &clusterAssociationMap) const;
 
     /**
-     *  @brief  Merge the associated clusters
+     *  @brief Identify good matches from a list of possible matches
      *
-     *  @param  clusterToPfoMap the mapping between clusters and current Pfos
-     *  @param  clusterAssociationMap the mapping between clusters to be merged
-     */
-    void MergeClusters(const ClusterToPfoMap &clusterToPfoMap, const ClusterAssociationMap &clusterAssociationMap) const;
+     *  @param inputParticleList the input list of possible matches
+     *  @param outputParticleList the output list of good matches 
+     */   
+    void IdentifyMatches(const ParticleList &inputParticleList, ParticleList &outputParticleList) const;
 
+    /**
+     *  @brief Perform cluster merging
+     *
+     *  @param clusterAssociationMap the input map of cluster merges
+     */   
+    void ApplyMerges(const ClusterAssociationMap &clusterAssociationMap) const;
 
-
+    /**
+     *  @brief Build new Pfos
+     *
+     *  @param particleList the input list of candidate particles 
+     */   
+    void ApplyMatches(const ParticleList &particleList) const;
+ 
+    /**
+     *  @brief Collect up cluster associations
+     *
+     *  @param clusterAssociationMap the input map of cluster associations
+     *  @param clusterMergeMap the ouput map of cluster merges
+     */   
     void CollectAssociatedClusters(const ClusterAssociationMap &clusterAssociationMap, 
         ClusterAssociationMap &clusterMergeMap) const;
 
-
+    /**
+     *  @brief Collect up cluster associations
+     *
+     *  @param  pSeedCluster pointer to the initial cluster
+     *  @param  pCurrentCluster pointer to the current cluster
+     *  @param  clusterAssociationMap the map of cluster associations
+     *  @param  clusterVetoList the list of clusters that have already been merged
+     *  @param  associatedClusterList the output list of associated clusters
+     */   
     void CollectAssociatedClusters(pandora::Cluster *pSeedCluster, pandora::Cluster *pCurrentCluster, 
         const ClusterAssociationMap &clusterAssociationMap, const pandora::ClusterList &clusterVetoList, 
         pandora::ClusterList &associatedClusterList) const;
   
-
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
-
-    std::string  m_inputPfoListName;        ///< The input pfo list name
+    std::string  m_inputPfoListName;          ///< The input pfo list name
     std::string  m_inputClusterListNameU;     ///< The input cluster list name for the u view
     std::string  m_inputClusterListNameV;     ///< The input cluster list name for the v view
     std::string  m_inputClusterListNameW;     ///< The input cluster list name for the w view
 
-
     unsigned int m_minCaloHitsPerCluster;     ///< The min number of calo hits per candidate cluster
 
-    float        m_xOverlapWindow;
-    float        m_distanceForMatching;
-    float        m_pseudoChi2Cut;
-
-   
+    float        m_xOverlapWindow;            ///< The overlap window in x coordinate when matching views
+    float        m_distanceForMatching;       ///< Maximum distance between primary and secondary Pfos
+    float        m_pseudoChi2Cut;             ///< Selection cut on pseudoChi2 when matching views
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
