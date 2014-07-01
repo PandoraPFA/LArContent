15eb73d92091c496b17256693a32923ddb425b69
Andrew Blake <blake@hep.phy.cam.ac.uk>
2014-07-01 21:04:07 +0000

    Removing UID's from ParticleMonitoringAlgorithm - they're driving me mad!
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@931 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- include/LArMonitoring/ParticleMonitoringAlgorithm.h
+++ include/LArMonitoring/ParticleMonitoringAlgorithm.h
@@ -1,8 +1,8 @@
 /**
  *  @file   LArContent/include/LArMonitoring/ParticleMonitoringAlgorithm.h
- * 
+ *
  *  @brief  Header file for the particle monitoring algorithm.
- * 
+ *
  *  $Log: $
  */
 #ifndef LAR_PARTICLE_MONITORING_ALGORITHM_H
@@ -42,58 +42,79 @@ private:
     pandora::StatusCode Run();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
-    typedef std::map<pandora::Uid, int> ContributionMap;
-    typedef std::map<pandora::Uid, pandora::Uid> UidRelationMap;
-    typedef std::map<pandora::Uid, const pandora::MCParticle*> UidToMCParticleMap;
-    typedef std::map<pandora::Uid, const pandora::ParticleFlowObject*> UidToPfoMap;
+    typedef std::map<const pandora::MCParticle*, const pandora::MCParticle*> MCRelationMap;
+    typedef std::map<const pandora::MCParticle*, const pandora::ParticleFlowObject*> MCToPfoMap;
+    typedef std::map<const pandora::ParticleFlowObject*, const pandora::MCParticle*> PfoToMCMap;
+    typedef std::map<const pandora::CaloHit*, const pandora::MCParticle*> CaloHitToMCMap;
+    typedef std::map<const pandora::CaloHit*, const pandora::ParticleFlowObject*> CaloHitToPfoMap;
+    typedef std::map<const pandora::ParticleFlowObject*, int> PfoContributionMap;
+    typedef std::map<const pandora::MCParticle*, int> MCContributionMap;
 
     /**
-     *  @brief  Figure out the primary ancestor of each MC particle and store in a map
-     * 
-     *  @param  pMCParticleList
-     *  @param  uidToPrimaryMap
-     *  @param  uidToMCParticleMap
+     *  @brief  Create map between each MC particle and its primary parent MC particle
+     *
+     *  @param  pMCParticleList the input MC particle list
+     *  @param  mcPrimaryMap the output mapping between particles and their parents
      */
-    void GetMCParticleMaps(const pandora::MCParticleList *const pMCParticleList, UidRelationMap &uidToPrimaryMap,
-        UidToMCParticleMap &uidToMCParticleMap) const;
+    void GetMCParticleMaps(const pandora::MCParticleList *const pMCParticleList, MCRelationMap &mcPrimaryMap) const;
 
     /**
-     *  @brief  Find pfo containing most hits from each primary MC particle, and store properties in a map indexed by particle Uid
-     * 
-     *  @param  pCaloHitList
-     *  @param  pfoList
-     *  @param  uidToPrimaryMap
-     *  @param  uidToPfoMap
-     *  @param  contributionMap
+     *  @brief  Match calo hits to their parent particles
+     *
+     *  @param  pCaloHitList the input list of calo hits
+     *  @param  mcPrimaryMap the input map between particles and their primaries
+     *  @param  mcHitMap the output mapping between calo hits and their main MC particles
+     *  @param  mcContributionMap the output mapping between MC particles and their number of associated hits
      */
-    void GetMCParticleToPfoMatches(const pandora::CaloHitList *const pCaloHitList, const pandora::PfoList &pfoList, 
-        const UidRelationMap &uidToPrimaryMap, UidToPfoMap &uidToPfoMap, ContributionMap &contributionMap) const;
+    void GetMCParticleToCaloHitMatches(const pandora::CaloHitList *const pCaloHitList, const MCRelationMap &mcPrimaryMap,
+        CaloHitToMCMap &mcHitMap, MCContributionMap &mcContributionMap) const;
 
     /**
-     *  @brief  Work out the total number of hits associated with each primary MC particle
-     * 
-     *  @param  pCaloHitList
-     *  @param  uidToPrimaryMap
-     *  @param  contributionMap
+     *  @brief  Match calo hits to their parent Pfos
+     *
+     *  @param  pCaloHitList the input list of calo hits
+     *  @param  pfoList the input list of Pfos
+     *  @param  pfoHitMap the output mapping between calo hits and their parent Pfos
+     *  @param  pfoContributionMap the output mapping between Pfos and their number of associated hits
      */
-    void GetMCParticleToCaloHitMatches(const pandora::CaloHitList *const pCaloHitList, const UidRelationMap &uidToPrimaryMap,
-        ContributionMap &contributionMap) const;
+    void GetPfoToCaloHitMatches(const pandora::CaloHitList *const pCaloHitList, const pandora::PfoList &pfoList,
+        CaloHitToPfoMap &pfoHitMap, PfoContributionMap &pfoContributionMap) const;
 
     /**
-     *  @brief  Get 3D position in best agreement with provided reference point, using combinations of u,v and w inner/outer layer centroids
-     * 
-     *  @param  clusterList
-     *  @param  referencePoint
-     *  @param  useInnerLayer
+     *  @brief  Match MC particle and Pfos
+     *
+     *  @param  pCaloHitList the input list of calo hits
+     *  @param  pfoList the input list of Pfos
+     *  @param  mcHitMap the input mapping between calo hits and their parent Pfos
+     *  @param  outputPrimaryMap the output mapping between MC particles to their best matched Pfo
+     *  @param  outputContributionMap the output mapping between MC particles and their number of matched hits
      */
-    pandora::CartesianVector GetSpacePoint(const pandora::ClusterList &clusterList, const pandora::CartesianVector &referencePoint,
-        const bool useInnerLayer) const;
+    void GetMCParticleToPfoMatches(const pandora::CaloHitList *const pCaloHitList, const pandora::PfoList &pfoList,
+        const CaloHitToMCMap &mcHitMap, MCToPfoMap &outputPrimaryMap, MCContributionMap &outputContributionMap) const;
 
-    std::string     m_caloHitListName;                  ///< 
-    std::string     m_mcParticleListName;               ///< 
-    std::string     m_pfoListName;                      ///< 
-    std::string     m_fileName;                         ///< 
-    std::string     m_treeName;                         ///< 
+    /**
+     *  @brief  Collect up all calo hits associated with a Pfo and its daughters
+     *
+     *  @param  pParentPfo the input Pfo
+     *  @param  caloHitList the output calo hit list
+     */
+    void CollectCaloHits(const pandora::ParticleFlowObject *const pParentPfo, pandora::CaloHitList &caloHitList) const;
+
+    /**
+     *  @brief  Collect up all daughter clusters of a Pfo
+     *
+     *  @param  pParentPfo the input Pfo
+     *  @param  clusterList the output cluster list
+     */
+    void CollectDaughterClusters(const pandora::ParticleFlowObject *const pParentPfo, pandora::ClusterList &clusterList) const;
+
+    std::string     m_caloHitListName;                  ///< Name of input calo hit list
+    std::string     m_mcParticleListName;               ///< Name of input MC particle list
+    std::string     m_pfoListName;                      ///< Name of input Pfo list
+    std::string     m_fileName;                         ///< Name of output file
+    std::string     m_treeName;                         ///< Name of output tree
+
+    bool            m_useDaughterPfos;                  ///< Whether to include daughter Pfos in performance metrics
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
