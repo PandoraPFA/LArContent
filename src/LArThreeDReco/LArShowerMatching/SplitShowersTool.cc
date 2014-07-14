08739e3cf237b6b81401f8baf3ecce0788704140
John Marshall <marshall@hep.phy.cam.ac.uk>
2014-07-14 15:24:22 +0000

    Complete basic tool structures.
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@952 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- src/LArThreeDReco/LArShowerMatching/SplitShowersTool.cc
+++ src/LArThreeDReco/LArShowerMatching/SplitShowersTool.cc
@@ -42,39 +42,104 @@ void SplitShowersTool::FindSplitShowers(const TensorType &overlapTensor, ProtoPa
         TensorType::ElementList elementList;
         overlapTensor.GetConnectedElements(iterU->first, true, elementList, nU, nV, nW);
 
-        IteratorList iteratorList;
-        this->SelectLargeShowerElements(elementList, usedClusters, iteratorList);
+        if (nU * nV * nW < 2)
+            continue;
 
-        // Check that elements are not directly connected and are significantly longer than any other directly connected elements
-        for (IteratorList::const_iterator iIter = iteratorList.begin(), iIterEnd = iteratorList.end(); iIter != iIterEnd; ++iIter)
+        std::sort(elementList.begin(), elementList.end(), ThreeDShowersAlgorithm::SortByNMatchedSamplingPoints);
+ClusterList allClusterListU, allClusterListV, allClusterListW;
+        for (TensorType::ElementList::const_iterator eIter = elementList.begin(); eIter != elementList.end(); ++eIter)
         {
+            if (!this->PassesElementCuts(eIter, usedClusters))
+                continue;
+
+            IteratorList iteratorList;
+            this->SelectTensorElements(eIter, elementList, usedClusters, iteratorList);
+
+            if (iteratorList.size() < 2)
+                continue;
+
+            for (IteratorList::const_iterator iIter = iteratorList.begin(), iIterEnd = iteratorList.end(); iIter != iIterEnd; ++iIter)
+            {
+allClusterListU.insert((*iIter)->GetClusterU());
+allClusterListV.insert((*iIter)->GetClusterV());
+allClusterListW.insert((*iIter)->GetClusterW());
+std::cout << " Selected Element: MatchedFraction " << (*iIter)->GetOverlapResult().GetMatchedFraction()
+<< ", MatchedSamplingPoints " << (*iIter)->GetOverlapResult().GetNMatchedSamplingPoints()
+<< ", xSpanU " << (*iIter)->GetOverlapResult().GetXOverlap().GetXSpanU()
+<< ", xSpanV " << (*iIter)->GetOverlapResult().GetXOverlap().GetXSpanV()
+<< ", xSpanW " << (*iIter)->GetOverlapResult().GetXOverlap().GetXSpanW()
+<< ", xOverlapSpan " << (*iIter)->GetOverlapResult().GetXOverlap().GetXOverlapSpan()
+<< ", Availability (" << (*iIter)->GetClusterU()->IsAvailable() << (*iIter)->GetClusterV()->IsAvailable() << (*iIter)->GetClusterW()->IsAvailable() << ") "
+<< std::endl;
+                usedClusters.insert((*iIter)->GetClusterU());
+                usedClusters.insert((*iIter)->GetClusterV());
+                usedClusters.insert((*iIter)->GetClusterW());
+            }
         }
+if (!allClusterListU.empty() || !allClusterListV.empty() || !allClusterListW.empty())
+{
+std::cout << " All Connected Clusters " << std::endl;
+PANDORA_MONITORING_API(SetEveDisplayParameters(false, DETECTOR_VIEW_XZ));
+PANDORA_MONITORING_API(VisualizeClusters(&allClusterListU, "AllUClusters", RED));
+PANDORA_MONITORING_API(VisualizeClusters(&allClusterListV, "AllVClusters", GREEN));
+PANDORA_MONITORING_API(VisualizeClusters(&allClusterListW, "AllWClusters", BLUE));
+PANDORA_MONITORING_API(ViewEvent());
+}
     }
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-void SplitShowersTool::SelectLargeShowerElements(const TensorType::ElementList &elementList, const pandora::ClusterList &usedClusters,
-    IteratorList &iteratorList) const
+bool SplitShowersTool::PassesElementCuts(TensorType::ElementList::const_iterator eIter, const ClusterList &usedClusters) const
 {
-    for (TensorType::ElementList::const_iterator eIter = elementList.begin(); eIter != elementList.end(); ++eIter)
+    if (usedClusters.count(eIter->GetClusterU()) || usedClusters.count(eIter->GetClusterV()) || usedClusters.count(eIter->GetClusterW()))
+        return false;
+
+    if (eIter->GetOverlapResult().GetMatchedFraction() < m_minMatchedFraction)
+        return false;
+
+    if (eIter->GetOverlapResult().GetNMatchedSamplingPoints() < m_minMatchedSamplingPoints)
+        return false;
+
+    return true;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void SplitShowersTool::SelectTensorElements(TensorType::ElementList::const_iterator eIter, const TensorType::ElementList &elementList,
+    const ClusterList &usedClusters, IteratorList &iteratorList) const
+{
+    iteratorList.push_back(eIter);
+
+    for (TensorType::ElementList::const_iterator eIter2 = elementList.begin(); eIter2 != elementList.end(); ++eIter2)
     {
-        if (usedClusters.count(eIter->GetClusterU()) || usedClusters.count(eIter->GetClusterV()) || usedClusters.count(eIter->GetClusterW()))
+        if (eIter == eIter2)
             continue;
 
-        if (eIter->GetOverlapResult().GetMatchedFraction() < m_minMatchedFraction)
+        if (!this->PassesElementCuts(eIter2, usedClusters))
             continue;
 
-        if (eIter->GetOverlapResult().GetNMatchedSamplingPoints() < m_minMatchedSamplingPoints)
-            continue;
-
-        const XOverlap &xOverlap(eIter->GetOverlapResult().GetXOverlap());
-
-        if ((xOverlap.GetXSpanU() > std::numeric_limits<float>::epsilon()) && (xOverlap.GetXOverlapSpan() / xOverlap.GetXSpanU() > m_minXOverlapFraction) &&
-            (xOverlap.GetXSpanV() > std::numeric_limits<float>::epsilon()) && (xOverlap.GetXOverlapSpan() / xOverlap.GetXSpanV() > m_minXOverlapFraction) &&
-            (xOverlap.GetXSpanW() > std::numeric_limits<float>::epsilon()) && (xOverlap.GetXOverlapSpan() / xOverlap.GetXSpanW() > m_minXOverlapFraction))
+        for (IteratorList::const_iterator iIter = iteratorList.begin(); iIter != iteratorList.end(); ++iIter)
         {
-            iteratorList.push_back(eIter);
+            if ((*iIter) == eIter2)
+                continue;
+
+            unsigned int nMatchedClusters(0);
+
+            if ((*iIter)->GetClusterU() == eIter2->GetClusterU())
+                ++nMatchedClusters;
+
+            if ((*iIter)->GetClusterV() == eIter2->GetClusterV())
+                ++nMatchedClusters;
+
+            if ((*iIter)->GetClusterW() == eIter2->GetClusterW())
+                ++nMatchedClusters;
+
+            if (m_nCommonClusters == nMatchedClusters)
+            {
+                iteratorList.push_back(eIter2);
+                return;
+            }
         }
     }
 }
@@ -83,17 +148,17 @@ void SplitShowersTool::SelectLargeShowerElements(const TensorType::ElementList &
 
 StatusCode SplitShowersTool::ReadSettings(const TiXmlHandle xmlHandle)
 {
+    m_nCommonClusters = 2;
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+        "NCommonClusters", m_nCommonClusters));
+
     m_minMatchedFraction = 0.2f;
     PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
         "MinMatchedFraction", m_minMatchedFraction));
 
-    m_minMatchedSamplingPoints = 20;
+    m_minMatchedSamplingPoints = 40;
     PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
         "MinMatchedSamplingPoints", m_minMatchedSamplingPoints));
-
-    m_minXOverlapFraction = 0.5f;
-    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
-        "MinXOverlapFraction", m_minXOverlapFraction));
 
     return STATUS_CODE_SUCCESS;
 }
