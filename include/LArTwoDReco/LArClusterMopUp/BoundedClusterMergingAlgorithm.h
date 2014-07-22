cd3097928f945e06ed41e87ad1e5fd2ebfded9e2
John Marshall <marshall@hep.phy.cam.ac.uk>
2014-07-22 16:19:54 +0000

    Introduce ClusterMopUp base algorithm and new BoundedClusterMerging algorithm. Tweak shower position maps in ThreeDShowers algorithm (key on int, rather than unsigned int) Alter helper function interface for consistency and clarity regarding sorting.
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@969 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- include/LArTwoDReco/LArClusterMopUp/BoundedClusterMergingAlgorithm.h
+++ include/LArTwoDReco/LArClusterMopUp/BoundedClusterMergingAlgorithm.h
@@ -10,6 +10,8 @@
 
 #include "Pandora/Algorithm.h"
 
+#include "LArTwoDReco/LArClusterMopUp/ClusterMopUpAlgorithm.h"
+
 namespace lar
 {
 
@@ -17,7 +19,7 @@ namespace lar
  *  @brief  BoundedClusterMergingAlgorithm class
  */
 
-class BoundedClusterMergingAlgorithm : public pandora::Algorithm
+class BoundedClusterMergingAlgorithm : public ClusterMopUpAlgorithm
 {
 public:
     /**
@@ -30,14 +32,96 @@ public:
     };
 
 private:
-    pandora::StatusCode Run();
+    /**
+     *  @brief  XSampling class
+     */
+    class XSampling
+    {
+    public:
+        /**
+         *  @brief  Constructor
+         * 
+         *  @param  fitResult the sliding fit result
+         */
+        XSampling(const TwoDSlidingFitResult &fitResult);
+
+        float       m_minX;          ///< The min x value
+        float       m_maxX;          ///< The max x value
+        float       m_xPitch;        ///< The x sampling pitch to be used
+    };
+
+    /**
+     *  @brief  ShowerEdge
+     */
+    class ShowerEdge
+    {
+    public:
+        /**
+         *  @brief  Constructor
+         * 
+         *  @param  xCoordinate the x coordinate
+         *  @param  highEdgeZ the shower high edge z coordinate
+         *  @param  lowEdgeZ the shower low edge z coordinate
+         */
+        ShowerEdge(const float xCoordinate, const float highEdgeZ, const float lowEdgeZ);
+
+        /**
+         *  @param  Get the x coordinate
+         * 
+         *  @return the x coordinate
+         */
+        float GetXCoordinate() const;
+
+        /**
+         *  @param  Get the shower high edge z coordinate
+         * 
+         *  @return the shower high edge z coordinate
+         */
+        float GetHighEdgeZ() const;
+
+        /**
+         *  @param  Get the shower low edge z coordinate
+         * 
+         *  @return the shower low edge z coordinate
+         */
+        float GetLowEdgeZ() const;
+
+    private:
+        float       m_xCoordinate;   ///< The x coordinate
+        float       m_highEdgeZ;     ///< The shower high edge z coordinate
+        float       m_lowEdgeZ;      ///< The shower low edge z coordinate
+    };
+
+    typedef std::map<int, ShowerEdge> ShowerPositionMap;
+
+    void ClusterMopUp(const pandora::ClusterList &pfoClusters, const pandora::ClusterList &remnantClusters, ClusterAssociationMap &clusterAssociationMap) const;
+
+    /**
+     *  @brief  Get the shower position map containing high and low edge z positions in bins of x
+     * 
+     *  @param  negativeEdgeFitResult the negative edge sliding fit result
+     *  @param  positiveEdgeFitResult the positive edge sliding fit result
+     *  @param  xSampling the x sampling details
+     *  @param  showerPositionMap to receive the shower position map
+     */
+    void GetShowerPositionMap(const TwoDSlidingFitResult &negativeEdgeFitResult, const TwoDSlidingFitResult &positiveEdgeFitResult,
+        const XSampling &xSampling, ShowerPositionMap &showerPositionMap) const;
+
+    /**
+     *  @brief  Get the fraction of hits in a cluster bounded by a specified shower position map
+     * 
+     *  @param  pCluster address of the cluster
+     *  @param  xSampling the x sampling details
+     *  @param  showerPositionMap the shower position map
+     * 
+     *  @return the fraction of bounded hits
+     */
+    float GetBoundedFraction(const pandora::Cluster *const pCluster, const XSampling &xSampling, const ShowerPositionMap &showerPositionMap) const;
+
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
-    std::string             m_inputPfoListName;         ///< The pfo list name
-    pandora::StringVector   m_inputClusterListNames;    ///< The list of cluster list names
-
-    float                   m_vertexVetoRadius;         ///< The vertex veto radius
-    unsigned int            m_minCaloHitsInCluster;     ///< The minimum number of calo hits in a cluster for merging
+    unsigned int            m_slidingFitWindow;             ///< The layer window for the sliding linear fits
+    float                   m_minBoundedFraction;           ///< The minimum cluster bounded fraction for merging
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -45,6 +129,37 @@ private:
 inline pandora::Algorithm *BoundedClusterMergingAlgorithm::Factory::CreateAlgorithm() const
 {
     return new BoundedClusterMergingAlgorithm();
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline BoundedClusterMergingAlgorithm::ShowerEdge::ShowerEdge(const float xCoordinate, const float edge1, const float edge2) :
+    m_xCoordinate(xCoordinate),
+    m_highEdgeZ(std::max(edge1, edge2)),
+    m_lowEdgeZ(std::min(edge1, edge2))
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline float BoundedClusterMergingAlgorithm::ShowerEdge::GetXCoordinate() const
+{
+    return m_xCoordinate;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline float BoundedClusterMergingAlgorithm::ShowerEdge::GetHighEdgeZ() const
+{
+    return m_highEdgeZ;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline float BoundedClusterMergingAlgorithm::ShowerEdge::GetLowEdgeZ() const
+{
+    return m_lowEdgeZ;
 }
 
 } // namespace lar
--- src/LArTwoDReco/LArClusterMopUp/BoundedClusterMergingAlgorithm.cc
+++ src/LArTwoDReco/LArClusterMopUp/BoundedClusterMergingAlgorithm.cc
@@ -8,6 +8,8 @@
 
 #include "Pandora/AlgorithmHeaders.h"
 
+#include "LArHelpers/LArClusterHelper.h"
+
 #include "LArTwoDReco/LArClusterMopUp/BoundedClusterMergingAlgorithm.h"
 
 using namespace pandora;
@@ -15,27 +17,149 @@ using namespace pandora;
 namespace lar
 {
 
-StatusCode BoundedClusterMergingAlgorithm::Run()
+void BoundedClusterMergingAlgorithm::ClusterMopUp(const ClusterList &pfoClusters, const ClusterList &remnantClusters,
+    ClusterAssociationMap &clusterAssociationMap) const
 {
-    return STATUS_CODE_SUCCESS;
+    for (ClusterList::const_iterator pIter = pfoClusters.begin(), pIterEnd = pfoClusters.end(); pIter != pIterEnd; ++pIter)
+    {
+        Cluster *pPfoCluster(*pIter);
+
+        TwoDSlidingFitResult showerFitResult, negativeEdgeFitResult, positiveEdgeFitResult;
+        LArClusterHelper::LArTwoDSlidingFit(pPfoCluster, m_slidingFitWindow, showerFitResult);
+        LArClusterHelper::LArTwoDShowerEdgeFit(showerFitResult, NEGATIVE_SHOWER_EDGE, negativeEdgeFitResult);
+        LArClusterHelper::LArTwoDShowerEdgeFit(showerFitResult, POSITIVE_SHOWER_EDGE, positiveEdgeFitResult);
+
+        ShowerPositionMap showerPositionMap;
+        const XSampling xSampling(showerFitResult);
+        this->GetShowerPositionMap(negativeEdgeFitResult, positiveEdgeFitResult, xSampling, showerPositionMap);
+
+        for (ClusterList::const_iterator rIter = remnantClusters.begin(), rIterEnd = remnantClusters.end(); rIter != rIterEnd; ++rIter)
+        {
+            Cluster *pRemnantCluster(*rIter);
+            const float boundedFraction(this->GetBoundedFraction(pRemnantCluster, xSampling, showerPositionMap));
+
+            if (boundedFraction < m_minBoundedFraction)
+                continue;
+
+            AssociationDetails &associationDetails(clusterAssociationMap[pRemnantCluster]);
+
+            if (!associationDetails.insert(AssociationDetails::value_type(pPfoCluster, boundedFraction)).second)
+                throw StatusCodeException(STATUS_CODE_ALREADY_PRESENT);
+        }
+    }
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
+void BoundedClusterMergingAlgorithm::GetShowerPositionMap(const TwoDSlidingFitResult &negativeEdgeFitResult, const TwoDSlidingFitResult &positiveEdgeFitResult,
+    const XSampling &xSampling, ShowerPositionMap &showerPositionMap) const
+{
+    float minXn(0.f), maxXn(0.f), minXp(0.f), maxXp(0.f), minZn(0.f), maxZn(0.f), minZp(0.f), maxZp(0.f);
+    negativeEdgeFitResult.GetMinAndMaxX(minXn, maxXn);
+    positiveEdgeFitResult.GetMinAndMaxX(minXp, maxXp);
+    negativeEdgeFitResult.GetMinAndMaxZ(minZn, maxZn);
+    positiveEdgeFitResult.GetMinAndMaxZ(minZp, maxZp);
+
+    const float minX(std::min(minXn, minXp)), maxX(std::max(maxXn, maxXp));
+    const float minZ(std::min(minZn, minZp)), maxZ(std::max(maxZn, maxZp));
+
+    for (float x = xSampling.m_minX; x < xSampling.m_maxX; x += xSampling.m_xPitch)
+    {
+        CartesianPointList fitPositionList;
+        try {negativeEdgeFitResult.GetGlobalFitPositionListAtX(x, fitPositionList);} catch (StatusCodeException &) {}
+        try {positiveEdgeFitResult.GetGlobalFitPositionListAtX(x, fitPositionList);} catch (StatusCodeException &) {}
+
+        FloatVector edgePositions;
+        for (CartesianPointList::const_iterator iter = fitPositionList.begin(), iterEnd = fitPositionList.end(); iter != iterEnd; ++iter)
+        {
+            edgePositions.push_back(iter->GetZ());
+        }
+
+        if (edgePositions.empty() && (x >= minX) && (x <= maxX))
+        {
+            edgePositions.push_back(minZ);
+            edgePositions.push_back(maxZ);
+        }
+
+        if (edgePositions.size() < 2)
+            continue;
+
+        std::sort(edgePositions.begin(), edgePositions.end());
+        const int xBin((x - xSampling.m_minX) / xSampling.m_xPitch);
+        showerPositionMap.insert(ShowerPositionMap::value_type(xBin, ShowerEdge(x, edgePositions.front(), edgePositions.back())));
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+float BoundedClusterMergingAlgorithm::GetBoundedFraction(const Cluster *const pCluster, const XSampling &xSampling, const ShowerPositionMap &showerPositionMap) const
+{
+    if (((xSampling.m_maxX - xSampling.m_minX) < std::numeric_limits<float>::epsilon()) ||
+        (xSampling.m_xPitch < std::numeric_limits<float>::epsilon()) || (0 == pCluster->GetNCaloHits()))
+    {
+        throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+    }
+
+    unsigned int nMatchedHits(0);
+    const OrderedCaloHitList &orderedCaloHitList(pCluster->GetOrderedCaloHitList());
+
+    for (OrderedCaloHitList::const_iterator iter = orderedCaloHitList.begin(), iterEnd = orderedCaloHitList.end(); iter != iterEnd; ++iter)
+    {
+        for (CaloHitList::const_iterator hIter = iter->second->begin(), hIterEnd = iter->second->end(); hIter != hIterEnd; ++hIter)
+        {
+            CaloHit *pCaloHit = *hIter;
+            const float x(pCaloHit->GetPositionVector().GetX());
+            const float z(pCaloHit->GetPositionVector().GetZ());
+
+            if ((x < xSampling.m_minX) || (x > xSampling.m_maxX))
+                continue;
+
+            const int xBin((x - xSampling.m_minX) / xSampling.m_xPitch);
+
+            ShowerPositionMap::const_iterator positionIter = showerPositionMap.find(xBin);
+
+            if ((showerPositionMap.end() != positionIter) && (z > positionIter->second.GetLowEdgeZ()) && (z < positionIter->second.GetHighEdgeZ()))
+                ++nMatchedHits;
+        }
+    }
+
+    return (static_cast<float>(nMatchedHits) / static_cast<float>(pCluster->GetNCaloHits()));
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+BoundedClusterMergingAlgorithm::XSampling::XSampling(const TwoDSlidingFitResult &fitResult)
+{
+    fitResult.GetMinAndMaxX(m_minX, m_maxX);
+    const int nPoints(fitResult.GetMaxLayer() - fitResult.GetMinLayer());
+
+    if (0 >= nPoints)
+        throw StatusCodeException(STATUS_CODE_NOT_FOUND);
+
+    m_xPitch = (m_maxX - m_minX) / static_cast<float>(nPoints);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------
+
 StatusCode BoundedClusterMergingAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
 {
-    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "InputPfoListName", m_inputPfoListName));
-    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadVectorOfValues(xmlHandle, "InputClusterListNames", m_inputClusterListNames));
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "PfoListName", m_pfoListName));
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadVectorOfValues(xmlHandle, "RemnantClusterListNames", m_remnantClusterListNames));
 
-    m_vertexVetoRadius = 2.5f; // cm
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadVectorOfValues(xmlHandle,
+        "AdditionalClusterListNames", m_additionalClusterListNames));
+
+    m_slidingFitWindow = 20;
     PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, 
-        "VertexVetoRadius", m_vertexVetoRadius));
+        "SlidingFitWindow", m_slidingFitWindow));
 
-    m_minCaloHitsInCluster = 4;
+    m_minBoundedFraction = 0.5f;
     PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, 
-        "MinCaloHitsInCluster", m_minCaloHitsInCluster));
+        "MinBoundedFraction", m_minBoundedFraction));
 
-    return STATUS_CODE_SUCCESS;
+    return ClusterMopUpAlgorithm::ReadSettings(xmlHandle);
 }
 
 } // namespace lar
