b9c37be3748c58ef0aa0b1083fc19110e798a7e9
Andrew Blake <blake@hep.phy.cam.ac.uk>
2014-07-30 21:29:55 +0000

    Committing base tool for longitudinal hit creation, and ClearLongitudinalTrackHitsTool. Also restricting the creation of track hits to Pfos with a track-like particle ID
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@995 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- include/LArThreeDReco/LArHitCreation/ClearLongitudinalTrackHitsTool.h
+++ include/LArThreeDReco/LArHitCreation/ClearLongitudinalTrackHitsTool.h
@@ -1,8 +1,8 @@
 /**
  *  @file   LArContent/include/LArThreeDReco/LArHitCreation/ClearLongitudinalTrackHitsTool.h
- * 
+ *
  *  @brief  Header file for the longitudinal track hit creation tool.
- * 
+ *
  *  $Log: $
  */
 #ifndef CLEAR_LONGITUDINAL_TRACK_HITS_TOOL_H
@@ -31,8 +31,9 @@ public:
 private:
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
-
-
+    void GetThreeDPosition(const pandora::CaloHit *const pCaloHit2D, const MatchedSlidingFitMap &matchedSlidingFitMap,
+        const pandora::CartesianVector &vtx3D, const pandora::CartesianVector &end3D, pandora::CartesianVector &position3D,
+        float &chiSquared) const;
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
--- src/LArThreeDReco/LArHitCreation/ClearLongitudinalTrackHitsTool.cc
+++ src/LArThreeDReco/LArHitCreation/ClearLongitudinalTrackHitsTool.cc
@@ -1,11 +1,13 @@
 /**
  *  @file   LArContent/src/LArThreeDReco/LArHitCreation/ClearLongitudinalTrackHitsTool.cc
- * 
+ *
  *  @brief  Implementation of the longitudinal track hit creation tool.
- * 
+ *
  *  $Log: $
  */
 
+#include "LArHelpers/LArClusterHelper.h"
+#include "LArHelpers/LArGeometryHelper.h"
 
 #include "LArThreeDReco/LArHitCreation/ClearLongitudinalTrackHitsTool.h"
 
@@ -13,7 +15,80 @@ using namespace pandora;
 
 namespace lar
 {
- 
+
+void ClearLongitudinalTrackHitsTool::GetThreeDPosition(const CaloHit *const pCaloHit2D, const MatchedSlidingFitMap &matchedSlidingFitMap,
+    const CartesianVector &vtx3D, const CartesianVector &end3D, CartesianVector &position3D, float &chiSquared) const
+{
+    const HitType hitType(pCaloHit2D->GetHitType());
+    const HitType hitType1((TPC_VIEW_U == hitType) ? TPC_VIEW_V : (TPC_VIEW_V == hitType) ? TPC_VIEW_W : TPC_VIEW_U);
+    const HitType hitType2((TPC_VIEW_U == hitType) ? TPC_VIEW_W : (TPC_VIEW_V == hitType) ? TPC_VIEW_U : TPC_VIEW_V);
+
+    const CartesianVector vtx2D(LArGeometryHelper::ProjectPosition(vtx3D, hitType));
+    const CartesianVector end2D(LArGeometryHelper::ProjectPosition(end3D, hitType));
+
+    if ((end2D - vtx2D).GetMagnitudeSquared() < std::numeric_limits<float>::epsilon())
+        throw StatusCodeException(STATUS_CODE_NOT_FOUND);
+
+    const float frac((end2D - vtx2D).GetDotProduct(pCaloHit2D->GetPositionVector() - vtx2D) / (end2D - vtx2D).GetMagnitudeSquared());
+    const CartesianVector projection3D(vtx3D + (end3D - vtx3D) * frac);
+
+    bool foundPosition1(false), foundPosition2(false);
+    CartesianVector position1(0.f, 0.f, 0.f), position2(0.f, 0.f, 0.f);
+
+    try
+    {
+        MatchedSlidingFitMap::const_iterator fIter1 = matchedSlidingFitMap.find(hitType1);
+        if (matchedSlidingFitMap.end() == fIter1)
+            throw StatusCodeException(STATUS_CODE_NOT_FOUND);
+
+        const TwoDSlidingFitResult &fitResult1 = fIter1->second;
+        const CartesianVector position2D(LArGeometryHelper::ProjectPosition(projection3D, hitType1));
+
+        float rL1(0.f), rT1(0.f);
+        fitResult1.GetLocalPosition(position2D, rL1, rT1);
+        fitResult1.GetTransverseProjection(pCaloHit2D->GetPositionVector().GetX(), fitResult1.GetFitSegment(rL1), position1);
+        foundPosition1 = true;
+    }
+    catch (StatusCodeException &)
+    {
+    }
+
+    try
+    {
+        MatchedSlidingFitMap::const_iterator fIter2 = matchedSlidingFitMap.find(hitType2);
+        if (matchedSlidingFitMap.end() == fIter2)
+            throw StatusCodeException(STATUS_CODE_NOT_FOUND);
+
+        const TwoDSlidingFitResult &fitResult2 = fIter2->second;
+        const CartesianVector position2D(LArGeometryHelper::ProjectPosition(projection3D, hitType2));
+
+        float rL2(0.f), rT2(0.f);
+        fitResult2.GetLocalPosition(position2D, rL2, rT2);
+        fitResult2.GetTransverseProjection(pCaloHit2D->GetPositionVector().GetX(), fitResult2.GetFitSegment(rL2), position2);
+        foundPosition2 = true;
+    }
+    catch (StatusCodeException &)
+    {
+    }
+
+    if (foundPosition1 && foundPosition2)
+    {
+        this->GetPosition3D(pCaloHit2D, hitType1, hitType2, position1, position2, position3D, chiSquared);
+    }
+    else if(foundPosition1)
+    {
+        this->GetPosition3D(pCaloHit2D, hitType1, position1, position3D, chiSquared);
+    }
+    else if(foundPosition2)
+    {
+        this->GetPosition3D(pCaloHit2D, hitType2, position2, position3D, chiSquared);
+    }
+    else
+    {
+        throw StatusCodeException(STATUS_CODE_NOT_FOUND);
+    }
+}
+
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 StatusCode ClearLongitudinalTrackHitsTool::ReadSettings(const TiXmlHandle xmlHandle)
