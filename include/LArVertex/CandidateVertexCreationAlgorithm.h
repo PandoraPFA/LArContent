0ed60c761b500d49f7b7bb66dc24572996b78418
John Marshall <marshall@hep.phy.cam.ac.uk>
2014-10-16 14:56:17 +0000

    Candidate vertex creation and selection framework. TwoDSlidingFitResultMap now keyed on address of a const Cluster.
    git-svn-id: file:///lbne/LArPandora/LArContent/trunk@1095 2b2306d6-deb7-42c6-9a4b-bff6bceb2610

--- include/LArVertex/CandidateVertexCreationAlgorithm.h
+++ include/LArVertex/CandidateVertexCreationAlgorithm.h
@@ -8,6 +8,8 @@
 #ifndef LAR_CANDIDATE_VERTEX_CREATION_ALGORITHM_H
 #define LAR_CANDIDATE_VERTEX_CREATION_ALGORITHM_H 1
 
+#include "LArObjects/LArTwoDSlidingFitResult.h"
+
 #include "Pandora/Algorithm.h"
 
 namespace lar_content
@@ -35,10 +37,75 @@ public:
 
 private:
     pandora::StatusCode Run();
+
+    /**
+     *  @brief  Select a subset of input clusters (contained in the input list names) for processing in this algorithm
+     *
+     *  @param  clusterListU to receive the selected clusters in the u view
+     *  @param  clusterListV to receive the selected clusters in the v view
+     *  @param  clusterListW to receive the selected clusters in the w view
+     */
+    void SelectClusters(pandora::ClusterList &clusterListU, pandora::ClusterList &clusterListV, pandora::ClusterList &clusterListW);
+
+    /**
+     *  @brief  Create candidate vertex positions by comparing pairs of cluster end positions
+     *
+     *  @param  clusterList1 the list of clusters in view 1
+     *  @param  clusterList1 the list of clusters in view 2
+     */
+    void ClusterEndPointComparison(const pandora::ClusterList &clusterList1, const pandora::ClusterList &clusterList2) const;
+
+    /**
+     *  @brief  Create a candidate vertex position, using an end-point position from one cluster and the fit for a second cluster
+     *
+     *  @param  position1 an end-point position for the first cluster
+     *  @param  hitType1 the hit type of the first cluster
+     *  @param  fitResult2 the two dimensional sliding fit result for the second cluster
+     */
+    void CreateVertex(const pandora::CartesianVector &position1, const pandora::HitType hitType1, const TwoDSlidingFitResult &fitResult2) const;
+
+    /**
+     *  @brief  Select a subset of input clusters for processing in this algorithm
+     *
+     *  @param  pInputClusterList address of an input cluster list
+     *  @param  selectedClusterList to receive the selected cluster list
+     */
+    void SelectClusters(const pandora::ClusterList *const pInputClusterList, pandora::ClusterList &selectedClusterList);
+
+    /**
+     *  @brief  Add a new sliding fit result, for the specified cluster, to the algorithm cache
+     * 
+     *  @param  pCluster address of the relevant cluster
+     */
+    void AddToSlidingFitCache(const pandora::Cluster *const pCluster);
+
+    /**
+     *  @brief  Get a sliding fit result from the algorithm cache
+     * 
+     *  @param  pCluster address of the relevant cluster
+     */
+    const TwoDSlidingFitResult &GetCachedSlidingFitResult(const pandora::Cluster *const pCluster) const;
+
+    /**
+     *  @brief  Clear relevant algorithm member variables between events
+     */
+    void TidyUp();
+
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
-    std::string     m_outputVertexListName;         ///< The name under which to save the output vertex list
-    bool            m_replaceCurrentVertexList;     ///< Whether to replace the current vertex list with the output list
+    std::string                 m_inputClusterListNameU;        ///< The name of the view U cluster list
+    std::string                 m_inputClusterListNameV;        ///< The name of the view V cluster list
+    std::string                 m_inputClusterListNameW;        ///< The name of the view W cluster list
+    std::string                 m_outputVertexListName;         ///< The name under which to save the output vertex list
+    bool                        m_replaceCurrentVertexList;     ///< Whether to replace the current vertex list with the output list
+
+    unsigned int                m_slidingFitWindow;             ///< The layer window for the sliding linear fits
+    TwoDSlidingFitResultMap     m_slidingFitResultMap;          ///< The sliding fit result map
+
+    unsigned int                m_minClusterCaloHits;           ///< The min number of hits in base cluster selection method
+    float                       m_minClusterLengthSquared;      ///< The min length (squared) in base cluster selection method
+    float                       m_maxClusterXDiscrepancy;       ///< The max cluster end-point discrepancy
+    float                       m_chiSquaredCut;                ///< The chi squared cut (accept only values below the cut value)
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
--- include/LArVertex/VertexSelectionAlgorithm.h
+++ include/LArVertex/VertexSelectionAlgorithm.h
@@ -35,10 +35,61 @@ public:
 
 private:
     pandora::StatusCode Run();
+
+    /**
+     *  @brief  Use hits in clusters (in the provided named list) to fill a provided histogram with hit-vertex relationship information
+     * 
+     *  @param  pVertex the address of the vertex
+     *  @param  hitType the relevant hit type
+     *  @param  clusterListName the cluster list name
+     *  @param  histogram to receive the populated histogram
+     */
+    void FillHistogram(const pandora::Vertex *const pVertex, const pandora::HitType hitType, const std::string &clusterListName,
+        pandora::Histogram &histogram) const;
+
+    /**
+     *  @brief  Use a provided vertex position and cluster to fill a provided histogram with hit-vertex relationship information
+     * 
+     *  @param  vertexPosition2D the projected vertex position
+     *  @param  pCluster the address of the cluster
+     *  @param  histogram to receive the populated histogram
+     */
+    void FillHistogram(const pandora::CartesianVector &vertexPosition2D, const pandora::Cluster *const pCluster,
+        pandora::Histogram &histogram) const;
+
+    /**
+     *  @brief  Get the figure of merit for a trio of histograms
+     * 
+     *  @param  histogramU the histogram for the u view
+     *  @param  histogramV the histogram for the v view
+     *  @param  histogramW the histogram for the w view
+     * 
+     *  @return the figure of merit
+     */
+    float GetFigureOfMerit(const pandora::Histogram &histogramU, const pandora::Histogram &histogramV, const pandora::Histogram &histogramW) const;
+
+    /**
+     *  @brief  Get the figure of merit contribution for a single histogram
+     * 
+     *  @param  histogram the histogram
+     * 
+     *  @return the figure of merit
+     */
+    float GetFigureOfMerit(const pandora::Histogram &histogram) const;
+
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
-    std::string     m_outputVertexListName;         ///< The name under which to save the output vertex list
-    bool            m_replaceCurrentVertexList;     ///< Whether to replace the current vertex list with the output list
+    std::string             m_inputClusterListNameU;        ///< The name of the view U cluster list
+    std::string             m_inputClusterListNameV;        ///< The name of the view V cluster list
+    std::string             m_inputClusterListNameW;        ///< The name of the view W cluster list
+    std::string             m_outputVertexListName;         ///< The name under which to save the output vertex list
+    bool                    m_replaceCurrentVertexList;     ///< Whether to replace the current vertex list with the output list
+
+    unsigned int            m_histogramNPhiBins;            ///< The number of histogram bins in phi
+    float                   m_histogramPhiMin;              ///< The histogram lower phi bound
+    float                   m_histogramPhiMax;              ///< The histogram upper phi bound
+
+    float                   m_maxHitVertexDisplacement;     ///< Max hit-vertex displacement for contribution to histograms
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
