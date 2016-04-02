0870fc1f1fc831e4c43262e492b1704bae95621d
Andy Blake <ablake@lapj.lancs.ac.uk>
2016-04-02 15:55:50 +0100

    Add a new cluster extension algorithm to join clusters across detector gaps.

---
+++ include/LArTwoDReco/LArClusterAssociation/CrossGapsExtensionAlgorithm.h
@@ -0,0 +1,106 @@
+/**
+ *  @file   LArContent/include/LArTwoDReco/LArClusterAssociation/CrossGapsExtensionAlgorithm.h
+ *
+ *  @brief  Header file for the cross gaps extension algorithm class.
+ *
+ *  $Log: $
+ */
+#ifndef LAR_CROSS_GAPS_EXTENSION_ALGORITHM_H
+#define LAR_GROSS_GAPS_EXTENSION_ALGORITHM_H 1
+
+#include "LArObjects/LArPointingCluster.h"
+
+#include "LArTwoDReco/LArClusterAssociation/ClusterExtensionAlgorithm.h"
+
+namespace lar_content
+{
+
+/**
+ *  @brief  CrossGapsExtensionAlgorithm class
+ */
+class CrossGapsExtensionAlgorithm : public ClusterExtensionAlgorithm
+{
+public:
+    /**
+     *  @brief  Factory class for instantiating algorithm
+     */
+    class Factory : public pandora::AlgorithmFactory
+    {
+    public:
+	pandora::Algorithm *CreateAlgorithm() const;
+    };
+
+    /**
+     *  @brief  Default constructor
+     */
+    CrossGapsExtensionAlgorithm();
+
+private:
+    void GetListOfCleanClusters(const pandora::ClusterList *const pClusterList, pandora::ClusterVector &clusterVector) const;
+    void FillClusterAssociationMatrix(const pandora::ClusterVector &clusterVector, ClusterAssociationMatrix &clusterAssociationMatrix) const;
+    void FillClusterMergeMap(const ClusterAssociationMatrix &clusterAssociationMatrix, ClusterMergeMap &clusterMergeMap) const;
+
+    /**
+     *  @brief Build lists of pointing clusters that are adjacent to a detector gap
+     *
+     *  @param clusterVector the input vector of clusters
+     *  @param innerPointingClusterList the pointing clusters whose inner vertex is close to a detector gap
+     *  @param outerPointingClusterList the pointing clusters whose outer vertex is close to a detector gap
+     */
+    void BuildPointingClusterList(const pandora::ClusterVector &clusterVector, LArPointingClusterList &innerPointingClusterList,
+	LArPointingClusterList &outerPointingClusterList) const;
+
+    /**
+     *  @brief Build a list of pointing clusters that are adjacent to a detector gap
+     *
+     *  @param useInner check the inner vertex
+     *  @param inputPointingClusterList the input list of pointing clusters
+     *  @param outputPointingClusterList the output list of pointing clusters
+     */
+    void BuildPointingClusterList(const bool useInner, const LArPointingClusterList &inputPointingClusterList,
+	LArPointingClusterList &outputPointingClusterList) const;
+
+    /**
+     *  @brief Use pointing information to determine whether two clusters are associated
+     *
+     *  @param pointingVertex1 the first pointing vertex
+     *  @param pointingVertex2 the second pointing vertex
+     */
+    bool IsAssociated(const LArPointingCluster::Vertex &pointingVertex1, const LArPointingCluster::Vertex &pointingVertex2) const;
+
+    /**
+     *  @brief Determine whether a start and end position sit either side of a gap
+     *
+     *  @param minZ the start position
+     *  @param maxZ the end position
+     *  @param hitType the hitType
+     */
+    bool IsAcrossGap(const float minZ, const float maxZ, const pandora::HitType hitType) const;
+
+    /**
+     *  @brief Determine whether a test position lies within a gap
+     *
+     *  @param testPosition the test position
+     *  @param hitType the hit type
+     */
+    bool IsInGap(const pandora::CartesianVector &testPosition, const pandora::HitType hitType) const;
+
+    pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
+
+    float   m_minClusterLength;               ///<
+    float   m_minGapFraction;                 ///<
+    float   m_maxGapTolerance;                ///<
+    float   m_maxTransverseDisplacement;      ///<
+    float   m_maxRelativeAngle;               ///<
+};
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline pandora::Algorithm *CrossGapsExtensionAlgorithm::Factory::CreateAlgorithm() const
+{
+    return new CrossGapsExtensionAlgorithm();
+}
+
+} // namespace lar_content
+
+#endif // #ifndef LAR_CROSS_GAPS_EXTENSION_ALGORITHM_H
---
+++ src/LArTwoDReco/LArClusterAssociation/CrossGapsExtensionAlgorithm.cc
@@ -0,0 +1,366 @@
+/**
+ *  @file   LArContent/src/LArTwoDReco/LArClusterAssociation/CrossGapsExtensionAlgorithm.cc
+ *
+ *  @brief  Implementation of the cross gaps extension algorithm class.
+ *
+ *  $Log: $
+ */
+
+#include "Pandora/AlgorithmHeaders.h"
+
+#include "LArHelpers/LArClusterHelper.h"
+#include "LArHelpers/LArPointingClusterHelper.h"
+
+#include "LArTwoDReco/LArClusterAssociation/CrossGapsExtensionAlgorithm.h"
+
+using namespace pandora;
+
+namespace lar_content
+{
+
+CrossGapsExtensionAlgorithm::CrossGapsExtensionAlgorithm() :
+    m_minClusterLength(5.f),
+    m_minGapFraction(0.5f),
+    m_maxGapTolerance(2.f),
+    m_maxTransverseDisplacement(2.5f),
+    m_maxRelativeAngle(10.f)
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void CrossGapsExtensionAlgorithm::GetListOfCleanClusters(const ClusterList *const pClusterList, ClusterVector &clusterVector) const
+{
+    // ATTN May want to opt-out completely if no gap information available
+    // if (PandoraContentApi::GetGeometry(*this)->GetDetectorGapList().empty())
+    //     return;
+
+    for (ClusterList::const_iterator iter = pClusterList->begin(), iterEnd = pClusterList->end(); iter != iterEnd; ++iter)
+    {
+	const Cluster *const pCluster = *iter;
+
+	if (LArClusterHelper::GetLengthSquared(pCluster) < m_minClusterLength * m_minClusterLength)
+	    continue;
+
+	clusterVector.push_back(pCluster);
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void CrossGapsExtensionAlgorithm::FillClusterAssociationMatrix(const ClusterVector &clusterVector, ClusterAssociationMatrix &clusterAssociationMatrix) const
+{
+    // Build lists of pointing clusters in proximity to gaps
+    LArPointingClusterList innerPointingClusterList, outerPointingClusterList;
+    this->BuildPointingClusterList(clusterVector, innerPointingClusterList, outerPointingClusterList);
+
+    // Form associations between pairs of pointing clusters
+    for (LArPointingClusterList::const_iterator iterInner = innerPointingClusterList.begin(), iterEndInner = innerPointingClusterList.end();
+	iterInner != iterEndInner; ++iterInner)
+    {
+	const LArPointingCluster &pointingClusterInner = *iterInner;
+	const LArPointingCluster::Vertex pointingVertexInner(pointingClusterInner.GetInnerVertex());
+	const float zInner(pointingVertexInner.GetPosition().GetZ());
+
+	for (LArPointingClusterList::const_iterator iterOuter = outerPointingClusterList.begin(), iterEndOuter = outerPointingClusterList.end();
+	    iterOuter != iterEndOuter; ++iterOuter)
+	{
+	    const LArPointingCluster &pointingClusterOuter = *iterOuter;
+	    const LArPointingCluster::Vertex pointingVertexOuter(pointingClusterOuter.GetOuterVertex());
+	    const float zOuter(pointingVertexOuter.GetPosition().GetZ());
+
+	    if (!this->IsAcrossGap(zOuter, zInner, LArClusterHelper::GetClusterHitType(pointingClusterInner.GetCluster())))
+		continue;
+
+	    if (!this->IsAssociated(pointingVertexInner, pointingVertexOuter))
+		continue;
+
+	    const Cluster *const pClusterInner = pointingClusterInner.GetCluster();
+	    const Cluster *const pClusterOuter = pointingClusterOuter.GetCluster();
+/*
+ClusterList clusterList1, clusterList2;
+clusterList1.insert(pClusterInner);
+clusterList2.insert(pClusterOuter);
+PandoraMonitoringApi::SetEveDisplayParameters(this->GetPandora(), true, DETECTOR_VIEW_XZ, -1.f, -1.f, 1.f);
+PandoraMonitoringApi::VisualizeClusters(this->GetPandora(), &clusterList1, "ClusterInner", RED);
+PandoraMonitoringApi::VisualizeClusters(this->GetPandora(), &clusterList2, "ClusterOuter", BLUE);
+PandoraMonitoringApi::ViewEvent(this->GetPandora());
+*/
+	    const float lengthSquaredInner(LArClusterHelper::GetLengthSquared(pClusterInner));
+	    const float lengthSquaredOuter(LArClusterHelper::GetLengthSquared(pClusterOuter));
+
+	    (void) clusterAssociationMatrix[pClusterInner].insert(ClusterAssociationMap::value_type(pClusterOuter,
+		ClusterAssociation(ClusterAssociation::INNER, ClusterAssociation::OUTER, ClusterAssociation::STRONG, lengthSquaredOuter)));
+	    (void) clusterAssociationMatrix[pClusterOuter].insert(ClusterAssociationMap::value_type(pClusterInner,
+		ClusterAssociation(ClusterAssociation::OUTER, ClusterAssociation::INNER, ClusterAssociation::STRONG, lengthSquaredInner)));
+	}
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void CrossGapsExtensionAlgorithm::BuildPointingClusterList(const ClusterVector &clusterVector, LArPointingClusterList &innerPointingClusterList,
+    LArPointingClusterList &outerPointingClusterList) const
+{
+    // Convert each input cluster into a pointing cluster
+    LArPointingClusterList pointingClusterList;
+
+    for (ClusterVector::const_iterator iter = clusterVector.begin(), iterEnd = clusterVector.end(); iter != iterEnd; ++iter)
+    {
+	try
+	{
+	    pointingClusterList.push_back(LArPointingCluster(*iter));
+	}
+	catch (StatusCodeException &)
+	{
+	}
+    }
+
+    // Identify clusters adjacent to detector gaps
+    this->BuildPointingClusterList(true, pointingClusterList, innerPointingClusterList);
+    this->BuildPointingClusterList(false, pointingClusterList, outerPointingClusterList);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void CrossGapsExtensionAlgorithm::BuildPointingClusterList(const bool useInner, const LArPointingClusterList &inputPointingClusterList,
+    LArPointingClusterList &outputPointingClusterList) const
+{
+    for (LArPointingClusterList::const_iterator iter = inputPointingClusterList.begin(), iterEnd = inputPointingClusterList.end();
+	iter != iterEnd; ++iter)
+    {
+	const LArPointingCluster &pointingCluster = *iter;
+	const LArPointingCluster::Vertex pointingVertex(useInner ? pointingCluster.GetInnerVertex() : pointingCluster.GetOuterVertex());
+
+	if (this->IsInGap(pointingVertex.GetPosition(), LArClusterHelper::GetClusterHitType(pointingCluster.GetCluster())))
+	    outputPointingClusterList.push_back(pointingCluster);
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+bool CrossGapsExtensionAlgorithm::IsAssociated(const LArPointingCluster::Vertex &pointingVertex1, const LArPointingCluster::Vertex &pointingVertex2) const
+{
+    const float maxLongitudinalDisplacement((pointingVertex2.GetPosition() - pointingVertex1.GetPosition()).GetMagnitude());
+
+    const bool isAssociated1(LArPointingClusterHelper::IsEmission(pointingVertex1.GetPosition(), pointingVertex2, -1.f,
+	maxLongitudinalDisplacement + 1.f, m_maxTransverseDisplacement, m_maxRelativeAngle));
+    const bool isAssociated2(LArPointingClusterHelper::IsEmission(pointingVertex2.GetPosition(), pointingVertex1, -1.f,
+	maxLongitudinalDisplacement + 1.f, m_maxTransverseDisplacement, m_maxRelativeAngle));
+
+    return (isAssociated1 && isAssociated2);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+bool CrossGapsExtensionAlgorithm::IsAcrossGap(const float minZ, const float maxZ, const HitType hitType) const
+{
+    if (maxZ - minZ < std::numeric_limits<float>::epsilon())
+	return false;
+
+    float deltaZ(0.f);
+
+    for (const DetectorGap *const pDetectorGap : PandoraContentApi::GetGeometry(*this)->GetDetectorGapList())
+    {
+	const LineGap *const pLineGap = dynamic_cast<const LineGap*>(pDetectorGap);
+
+        if (NULL == pLineGap)
+            throw StatusCodeException(STATUS_CODE_INVALID_PARAMETER);
+
+	if (pLineGap->GetHitType() == hitType && pLineGap->GetLineStartZ() > minZ && pLineGap->GetLineEndZ() < maxZ)
+	    deltaZ += (pLineGap->GetLineEndZ() - pLineGap->GetLineStartZ());
+    }
+
+    if (deltaZ / (maxZ - minZ) < m_minGapFraction)
+	return false;
+
+    return true;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+bool CrossGapsExtensionAlgorithm::IsInGap(const CartesianVector &testPosition, const HitType hitType) const
+{
+    for (const DetectorGap *const pDetectorGap : PandoraContentApi::GetGeometry(*this)->GetDetectorGapList())
+    {
+	if (pDetectorGap->IsInGap(testPosition, hitType, m_maxGapTolerance))
+	    return true;
+    }
+
+    return false;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void CrossGapsExtensionAlgorithm::FillClusterMergeMap(const ClusterAssociationMatrix &inputAssociationMatrix, ClusterMergeMap &clusterMergeMap) const
+{
+    // Decide which associations will become merges
+    // To make the merge A <-> B, both A -> B and B -> A must be strong associations
+    // with the largest figures of merit of all the A -> X and B -> Y associations
+
+    // First step: remove double-counting from the map of associations
+    // i.e. if the map has A <-> B, B <-> C, A <-> C, then remove A <-> C
+    ClusterAssociationMatrix clusterAssociationMatrix;
+
+    for (ClusterAssociationMatrix::const_iterator iter1 = inputAssociationMatrix.begin(), iterEnd1 = inputAssociationMatrix.end(); iter1 != iterEnd1; ++iter1)
+    {
+	const Cluster *const pCluster1(iter1->first);
+	const ClusterAssociationMap &associationMap1(iter1->second);
+
+	for (ClusterAssociationMatrix::const_iterator iter2 = iter1, iterEnd2 = inputAssociationMatrix.end(); iter2 != iterEnd2; ++iter2)
+	{
+	    const Cluster *const pCluster2(iter2->first);
+	    const ClusterAssociationMap &associationMap2(iter2->second);
+
+	    if (pCluster1 == pCluster2)
+		continue;
+
+	    ClusterAssociationMap::const_iterator iter12 = associationMap1.find(pCluster2);
+	    if (associationMap1.end() == iter12)
+		continue;
+
+	    ClusterAssociationMap::const_iterator iter21 = associationMap2.find(pCluster1);
+	    if (associationMap2.end() == iter21)
+		continue;
+
+	    const ClusterAssociation &association12(iter12->second);
+	    const ClusterAssociation &association21(iter21->second);
+
+	    bool isAssociated(true);
+
+	    for (ClusterAssociationMap::const_iterator iter13 = associationMap1.begin(), iterEnd13 = associationMap1.end(); iter13 != iterEnd13; ++iter13)
+	    {
+		const Cluster *const pCluster3(iter13->first);
+
+		ClusterAssociationMap::const_iterator iter23 = associationMap2.find(pCluster3);
+		if (associationMap2.end() == iter23)
+		    continue;
+
+		const ClusterAssociation &association13(iter13->second);
+		const ClusterAssociation &association23(iter23->second);
+
+		if (association12.GetParent() == association13.GetParent() &&
+		    association23.GetParent() == association21.GetParent() &&
+		    association13.GetDaughter() != association23.GetDaughter())
+		{
+		    isAssociated = false;
+		    break;
+		}
+	    }
+
+	    if (isAssociated)
+	    {
+		(void) clusterAssociationMatrix[pCluster1].insert(ClusterAssociationMap::value_type(pCluster2, association12));
+		(void) clusterAssociationMatrix[pCluster2].insert(ClusterAssociationMap::value_type(pCluster1, association21));
+	    }
+	}
+    }
+
+
+    // Second step: find the best associations A -> X and B -> Y
+    ClusterAssociationMatrix intermediateAssociationMatrix;
+
+    for (ClusterAssociationMatrix::const_iterator iter1 = clusterAssociationMatrix.begin(), iterEnd1 = clusterAssociationMatrix.end(); iter1 != iterEnd1; ++iter1)
+    {
+	const Cluster *const pParentCluster(iter1->first);
+	const ClusterAssociationMap &clusterAssociationMap(iter1->second);
+
+	const Cluster *pBestClusterInner = NULL;
+	ClusterAssociation bestAssociationInner(ClusterAssociation::UNDEFINED, ClusterAssociation::UNDEFINED, ClusterAssociation::NONE, 0.f);
+
+	const Cluster *pBestClusterOuter = NULL;
+	ClusterAssociation bestAssociationOuter(ClusterAssociation::UNDEFINED, ClusterAssociation::UNDEFINED, ClusterAssociation::NONE, 0.f);
+
+	for (ClusterAssociationMap::const_iterator iter2 = clusterAssociationMap.begin(), iterEnd2 = clusterAssociationMap.end(); iter2 != iterEnd2; ++iter2)
+	{
+	    const Cluster *const pDaughterCluster(iter2->first);
+	    const ClusterAssociation &clusterAssociation(iter2->second);
+
+	    // Inner associations
+	    if (clusterAssociation.GetParent() == ClusterAssociation::INNER)
+	    {
+		if (clusterAssociation.GetFigureOfMerit() > bestAssociationInner.GetFigureOfMerit())
+		{
+		    bestAssociationInner = clusterAssociation;
+		    pBestClusterInner = pDaughterCluster;
+		}
+	    }
+
+	    // Outer associations
+	    if (clusterAssociation.GetParent() == ClusterAssociation::OUTER)
+	    {
+		if (clusterAssociation.GetFigureOfMerit() > bestAssociationOuter.GetFigureOfMerit())
+		{
+		    bestAssociationOuter = clusterAssociation;
+		    pBestClusterOuter = pDaughterCluster;
+		}
+	    }
+	}
+
+	if (pBestClusterInner)
+	    (void) intermediateAssociationMatrix[pParentCluster].insert(ClusterAssociationMap::value_type(pBestClusterInner, bestAssociationInner));
+
+	if (pBestClusterOuter)
+	    (void) intermediateAssociationMatrix[pParentCluster].insert(ClusterAssociationMap::value_type(pBestClusterOuter, bestAssociationOuter));
+    }
+
+
+    // Third step: make the merge if A -> X and B -> Y is in fact A -> B and B -> A
+    for (ClusterAssociationMatrix::const_iterator iter3 = intermediateAssociationMatrix.begin(), iterEnd3 = intermediateAssociationMatrix.end(); iter3 != iterEnd3; ++iter3)
+    {
+	const Cluster *const pParentCluster(iter3->first);
+	const ClusterAssociationMap &parentAssociationMap(iter3->second);
+
+	for (ClusterAssociationMap::const_iterator iter4 = parentAssociationMap.begin(), iterEnd4 = parentAssociationMap.end(); iter4 != iterEnd4; ++iter4)
+	{
+	    const Cluster *const pDaughterCluster(iter4->first);
+	    const ClusterAssociation &parentToDaughterAssociation(iter4->second);
+
+	    ClusterAssociationMatrix::const_iterator iter5 = intermediateAssociationMatrix.find(pDaughterCluster);
+
+	    if (intermediateAssociationMatrix.end() == iter5)
+		continue;
+
+	    const ClusterAssociationMap &daughterAssociationMap(iter5->second);
+
+	    ClusterAssociationMap::const_iterator iter6 = daughterAssociationMap.find(pParentCluster);
+
+	    if (daughterAssociationMap.end() == iter6)
+		continue;
+
+	    const ClusterAssociation &daughterToParentAssociation(iter6->second);
+
+	    if (parentToDaughterAssociation.GetParent() == daughterToParentAssociation.GetDaughter() &&
+		parentToDaughterAssociation.GetDaughter() == daughterToParentAssociation.GetParent())
+	    {
+		clusterMergeMap[pParentCluster].insert(pDaughterCluster);
+	    }
+	}
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode CrossGapsExtensionAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
+{
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+	"MinClusterLength", m_minClusterLength));
+
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+	"MinGapFraction", m_minGapFraction));
+
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+	"MaxGapTolerance", m_maxGapTolerance));
+
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+	"MaxTransverseDisplacement", m_maxTransverseDisplacement));
+
+    float maxCosRelativeAngle(std::cos(m_maxRelativeAngle * M_PI / 180.0));
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+	"MaxCosRelativeAngle", maxCosRelativeAngle));
+    m_maxRelativeAngle = (180.0 / M_PI) * std::acos(maxCosRelativeAngle);
+
+    return ClusterExtensionAlgorithm::ReadSettings(xmlHandle);
+}
+
+} // namespace lar_content
