6c4d24ec1e5be646274350a4960800b12b417700
John Marshall <marshall@hep.phy.cam.ac.uk>
2017-05-31 13:26:56 +0100

    Updated track/shower characterisation variables.

--- larpandoracontent/LArTrackShowerId/TrackShowerIdFeatureTool.h
+++ larpandoracontent/LArTrackShowerId/TrackShowerIdFeatureTool.h
@@ -34,14 +34,14 @@ private:
    /**
     *  @brief  Calculation of the shower fit width variable
     *
-    *  @param  pAlgorithm, address of the calling algorithm
-    *  @param  pCluster, the cluster we are characterizing
+    *  @param  pAlgorithm address of the calling algorithm
+    *  @param  pCluster the cluster we are characterizing
     *
     *  @return shower fit width
     */
     float CalculateShowerFitWidth(const pandora::Algorithm *const pAlgorithm, const pandora::Cluster *const pCluster) const;
 
-    float   m_slidingShowerFitWindow;       ///< The sliding shower fit window
+    unsigned int    m_slidingShowerFitWindow;        ///< The sliding shower fit window
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -79,16 +79,26 @@ private:
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
     /**
-    *  @brief  Calculation of several variables related to sliding linear fit
-    *
-    *  @param  pCluster, the cluster we are characterizing
-    */
-    void CalculateVariablesSlidingLinearFit(const pandora::Cluster *const pCluster, float &straightLineLengthLarge, float &diffWithStraigthLine, float &dTdLWidth, float &maxFitGapLength) const;
+     *  @brief  Calculation of several variables related to sliding linear fit
+     *
+     *  @param  pCluster the cluster we are characterizing
+     *  @param  straightLineLengthLarge to receive to length reported by the straight line fit
+     *  @param  diffWithStraigthLineMean to receive the difference with straight line mean variable
+     *  @param  diffWithStraightLineSigma to receive the difference with straight line sigma variable
+     *  @param  dTdLWidth to receive the dTdL width variable
+     *  @param  maxFitGapLength to receive the max fit gap length variable
+     *  @param  rmsSlidingLinearFit to receive the RMS from the linear fit
+     */
+    void CalculateVariablesSlidingLinearFit(const pandora::Cluster *const pCluster, float &straightLineLengthLarge, float &diffWithStraigthLineMean,
+        float &diffWithStraightLineSigma, float &dTdLWidth, float &maxFitGapLength, float &rmsSlidingLinearFit) const;
 
-    float   m_slidingLinearFitWindow;       ///< The sliding linear fit window
-    bool    m_addDiffWithStraightLine;      ///< Decide whether to add the difference with straight line variable to the feature list
-    bool    m_addDTDLWidth;                 ///< Decide whether to add the dTdL variable to the feature list
-    bool    m_addMaxFitGapLength;           ///< Decide whether to add the max fit gap length variable to the feature list
+    unsigned int    m_slidingLinearFitWindow;       ///< The sliding linear fit window
+    unsigned int    m_slidingLinearFitWindowLarge;  ///< The sliding linear fit window - should be large, providing a simple linear fit
+    bool            m_addDiffWithStraightLineMean;  ///< Decide whether to add the difference with straight line mean variable to the feature list
+    bool            m_addDiffWithStraightLineSigma; ///< Decide whether to add the difference with straight line sigma variable to the feature list
+    bool            m_addDTDLWidth;                 ///< Decide whether to add the dTdL width variable to the feature list
+    bool            m_addMaxFitGapLength;           ///< Decide whether to add the max fit gap length variable to the feature list
+    bool            m_addRMSLinearFit;              ///< Decide whether to add the RMS from the linear fit
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -113,7 +123,7 @@ private:
     /**
      *  @brief  Calculation of number of points of contact, i.e. number of clusters nearby
      *
-     *  @param  pCluster, the cluster we are characterizing
+     *  @param  pCluster the cluster we are characterizing
      *  @param  pAlgorithm address of the calling algorithm
      *
      *  @return number of clusters nearby pCluster
@@ -123,8 +133,8 @@ private:
     /**
      *  @brief  Address whether pCandidateCluster can be considered nearby pCluster
      *
-     *  @param  pCluster, the cluster we are characterizing
-     *  @param  pCandidateCluster, the cluster we are asking whether it is nearby pCluster
+     *  @param  pCluster the cluster we are characterizing
+     *  @param  pCandidateCluster the cluster we are asking whether it is nearby pCluster
      *
      *  @return boolean
      */
@@ -157,7 +167,7 @@ private:
     /**
      *  @brief  Calculate the mip energy of the cluster
      *
-     *  @param  pCluster, the cluster we are characterizing
+     *  @param  pCluster the cluster we are characterizing
      *
      *  @return the mip energy of the cluster
      */
@@ -183,7 +193,7 @@ private:
     /**
      *  @brief  Calculation of vertex distance
      *
-     *  @param  pCluster, the cluster we are characterizing
+     *  @param  pCluster the cluster we are characterizing
      *
      *  @return distance to neutrino vertex
      */
