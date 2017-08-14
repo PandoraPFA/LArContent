6d217478fac35132f4d8bcc1503be3328b7a66b5
loressa <escudero@hep.phy.cam.ac.uk>
2017-08-14 17:50:01 +0100

    Added the possibility to work with probabilities instead of binary classification

--- larpandoracontent/LArTrackShowerId/SvmPfoCharacterisationAlgorithm.h
+++ larpandoracontent/LArTrackShowerId/SvmPfoCharacterisationAlgorithm.h
@@ -34,6 +34,7 @@ protected:
 
     bool                    m_trainingSetMode;              ///< Whether to train
     bool                    m_ratioVariables;               ///< Whether to divide all variables by the straight line length
+    bool                    m_enableProbability;            ///< Whether to use probabilities instead of binary classification
     unsigned int            m_minCaloHitsCut;               ///< The minimum number of calo hits to qualify as a track
 
     std::string             m_trainingOutputFile;           ///< The training output file
