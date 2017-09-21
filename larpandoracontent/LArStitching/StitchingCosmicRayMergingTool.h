8a926c95fd210c8ec8f2bad139b338f141e0a929
John Marshall <marshall@hep.phy.cam.ac.uk>
2017-09-21 12:34:28 +0100

    Update StitchingCosmicRayMerging tool to use LArTPC geometry description.

--- larpandoracontent/LArStitching/StitchingCosmicRayMergingTool.h
+++ larpandoracontent/LArStitching/StitchingCosmicRayMergingTool.h
@@ -26,15 +26,6 @@ class StitchingCosmicRayMergingTool : public StitchingTool
 {
 public:
     /**
-     *  @brief  Factory class for instantiating algorithm tool
-     */
-    class Factory : public pandora::AlgorithmToolFactory
-    {
-    public:
-        pandora::AlgorithmTool *CreateAlgorithmTool() const;
-    };
-
-    /**
      *  @brief  Default constructor
      */
     StitchingCosmicRayMergingTool();
@@ -93,34 +84,18 @@ public:
         float           m_fom;              ///<
     };
 
-    typedef StitchingAlgorithm::PfoToVolumeIdMap PfoToVolumeIdMap;
-    typedef std::unordered_map<int, pandora::PfoList> VolumeIdToPfoMap;
-    typedef std::unordered_map<const pandora::ParticleFlowObject*, pandora::PfoList> PfoMergeMap;
-    typedef std::unordered_map<const pandora::ParticleFlowObject*, PfoAssociation> PfoAssociationMap;
-    typedef std::unordered_map<const pandora::ParticleFlowObject*, PfoAssociationMap> PfoAssociationMatrix;
-    typedef std::unordered_map<const pandora::ParticleFlowObject*, LArPointingCluster> ThreeDPointingClusterMap;
-
 private:
-
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
     /**
      *  @brief  Select primary Pfos from the input list of Pfos
      *
-     *  @param  pInputPfoList  the input list of Pfos
-     *  @param  outputPfoList  the output list of Pfos
+     *  @param  pInputPfoList the input list of Pfos
+     *  @param  outputPfoList the output list of Pfos
      */
     void SelectPrimaryPfos(const pandora::PfoList *pInputPfoList, pandora::PfoList &outputPfoList) const;
 
-    /**
-     *  @brief  Build a list of Pfos for each drift volume
-     *
-     *  @param  inputPfoList  the input list of Pfos
-     *  @param  pfoToVolumeIdMap  the input mapping between Pfos and drift volumes
-     *  @param  volumeIdToPfoMap  the output mapping between drift volumes and Pfos
-     */
-    void BuildVolumeMaps(const pandora::PfoList &inputPfoList, const PfoToVolumeIdMap &pfoToVolumeIdMap,
-        VolumeIdToPfoMap &volumeIdToPfoMap) const;
+    typedef std::unordered_map<const pandora::ParticleFlowObject*, LArPointingCluster> ThreeDPointingClusterMap;
 
     /**
      *  @brief  Build a 3D pointing cluster for each Pfo
@@ -130,76 +105,92 @@ private:
      */
     void BuildPointingClusterMaps(const pandora::PfoList &inputPfoList, ThreeDPointingClusterMap &pointingClusterMap) const;
 
+    typedef StitchingAlgorithm::PfoToLArTPCMap PfoToLArTPCMap;
+    typedef std::unordered_map<const pandora::LArTPC*, pandora::PfoList> LArTPCToPfoMap;
+
+    /**
+     *  @brief  Build a list of Pfos for each tpc
+     *
+     *  @param  inputPfoList the input list of Pfos
+     *  @param  pfoToLArTPCMap the input mapping between Pfos and tpc
+     *  @param  larTPCToPfoMap the output mapping between tpc and Pfos
+     */
+    void BuildTPCMaps(const pandora::PfoList &inputPfoList, const PfoToLArTPCMap &pfoToLArTPCMap, LArTPCToPfoMap &larTPCToPfoMap) const;
+
+    typedef std::unordered_map<const pandora::ParticleFlowObject*, PfoAssociation> PfoAssociationMap;
+    typedef std::unordered_map<const pandora::ParticleFlowObject*, PfoAssociationMap> PfoAssociationMatrix;
+
     /**
      *  @brief  Create associations between Pfos using 3D pointing clusters
      *
-     *  @param  volumeIdToPfoMap  the input mapping between Pfos and drift volumes
-     *  @param  pointingClusterMap  the mapping between Pfos and their corresponding 3D pointing clusters
-     *  @param  pfoAssociationMatrix  the output matrix of associations between Pfos
+     *  @param  larTPCToPfoMap the input mapping between tpc and Pfos
+     *  @param  pointingClusterMap the mapping between Pfos and their corresponding 3D pointing clusters
+     *  @param  pfoAssociationMatrix the output matrix of associations between Pfos
      */
-    void CreatePfoMatches(const VolumeIdToPfoMap &volumeIdToPfoMap, const ThreeDPointingClusterMap &pointingClusterMap,
+    void CreatePfoMatches(const LArTPCToPfoMap &larTPCToPfoMap, const ThreeDPointingClusterMap &pointingClusterMap,
         PfoAssociationMatrix &pfoAssociationMatrix) const;
 
     /**
      *  @brief  Create associations between Pfos using 3D pointing clusters
      *
-     *  @param  pfoVolume1  the drift volume of the first Pfo
-     *  @param  pfoVolume2  the drift volume of the second Pfo
-     *  @param  pPfo1  the first Pfo
-     *  @param  pPfo2  the second Pfo
-     *  @param  pointingClusterMap  the mapping between Pfos and their corresponding 3D pointing clusters
-     *  @param  pfoAssociationMatrix  the output matrix of associations between Pfos
+     *  @param  larTPC1 the tpc description for the first Pfo
+     *  @param  larTPC2 the tpc description for the second Pfo
+     *  @param  pPfo1 the first Pfo
+     *  @param  pPfo2 the second Pfo
+     *  @param  pointingClusterMap the mapping between Pfos and their corresponding 3D pointing clusters
+     *  @param  pfoAssociationMatrix the output matrix of associations between Pfos
      */
-    void CreatePfoMatches(const VolumeInfo &pfoVolume1, const VolumeInfo &pfoVolume2,
-        const pandora::ParticleFlowObject *const pPfo1, const pandora::ParticleFlowObject *const pPfo2,
-        const ThreeDPointingClusterMap &pointingClusterMap, PfoAssociationMatrix &pfoAssociationMatrix) const;
+    void CreatePfoMatches(const pandora::LArTPC &larTPC1, const pandora::LArTPC &larTPC2, const pandora::ParticleFlowObject *const pPfo1,
+        const pandora::ParticleFlowObject *const pPfo2, const ThreeDPointingClusterMap &pointingClusterMap, PfoAssociationMatrix &pfoAssociationMatrix) const;
+
+    typedef std::unordered_map<const pandora::ParticleFlowObject*, pandora::PfoList> PfoMergeMap;
 
     /**
-     *  @brief  Create mapping between associated Pfos, handling any ambiguities
+     *  @brief  Select the best associations between Pfos; create a mapping between associated Pfos, handling any ambiguities
      *
-     *  @param  pfoAssociationMatrix  the input list of all associations between Pfos
-     *  @param  pfoSelectedMatches  the output list of good associations between pfos (candidates for merging)
+     *  @param  pfoAssociationMatrix the input list of all associations between Pfos
+     *  @param  pfoSelectedMatches the output list of good associations between pfos (candidates for merging)
      */
     void SelectPfoMatches(const PfoAssociationMatrix &pfoAssociationMatrix, PfoMergeMap &pfoSelectedMatches) const;
 
     /**
-     *  @brief  Create mapping between Pfos to enlarge and Pfos to delete
+     *  @brief  Create an initial map of Pfo merges to be made
      *
-     *  @param  pfoMatches  the input list of associated Pfos
-     *  @param  pfoMerges  the output mapping between Pfos to enlarge and Pfos to delete
+     *  @param  pfoMatches the input list of associated Pfos
+     *  @param  pfoMerges the output mapping between Pfos to enlarge and Pfos to delete
      */
     void SelectPfoMerges(const PfoMergeMap &pfoMatches, PfoMergeMap &pfoMerges) const;
 
     /**
      *  @brief  Collect up associations between Pfos
      *
-     *  @param  pSeedPfo  the seed Pfo (will be enlarged after merging)
-     *  @param  pCurrentPfo  the target Pfo (will be deleted after merging)
-     *  @param  pfoMerges  the list of associations between Pfos
-     *  @param  vetoList  the list of associated Pfos that have already been considered
-     *  @param  associatedList  the output list of associated Pfos
+     *  @param  pSeedPfo the seed Pfo (will be enlarged after merging)
+     *  @param  pCurrentPfo the target Pfo (will be deleted after merging)
+     *  @param  pfoMerges the list of associations between Pfos
+     *  @param  vetoSet the set of associated Pfos that have already been considered
+     *  @param  associatedList the output list of associated Pfos
      */
     void CollectAssociatedPfos(const pandora::ParticleFlowObject *const pSeedPfo, const pandora::ParticleFlowObject *const pCurrentPfo,
-        const PfoMergeMap &pfoMerges, const pandora::PfoList &vetoList, pandora::PfoList &associatedList) const;
+        const PfoMergeMap &pfoMerges, const pandora::PfoSet &vetoSet, pandora::PfoList &associatedList) const;
 
     /**
      *  @brief  Identify the vertex Pfo and then re-order the map of merges so that the vertex Pfo will be enlarged
      *
-     *  @param  pfoToVolumeIdMap  the mapping between pfos and drift volume IDs
-     *  @param  pointingClusterMap  the mapping between Pfos and their corresponding 3D pointing clusters
-     *  @param  inputPfoMerges  the input map of Pfo merges
-     *  @param  outputPfoMerges  the re-ordered map of Pfo merges
+     *  @param  pfoToLArTPCMap the mapping between pfos and tpc
+     *  @param  pointingClusterMap the mapping between Pfos and their corresponding 3D pointing clusters
+     *  @param  inputPfoMerges the input map of Pfo merges
+     *  @param  outputPfoMerges the re-ordered map of Pfo merges
      */
-    void OrderPfoMerges(const PfoToVolumeIdMap &pfoToVolumeIdMap, const ThreeDPointingClusterMap &pointingClusterMap,
+    void OrderPfoMerges(const PfoToLArTPCMap &pfoToLArTPCMap, const ThreeDPointingClusterMap &pointingClusterMap,
         const PfoMergeMap &inputPfoMerges, PfoMergeMap &outputPfoMerges) const;
 
     /**
-     *  @brief  Stitch together the associated Pfos
+     *  @brief  Apply X0 corrections, and then stitch together Pfos
      *
-     *  @param  pAlgorithm  the address of the parent stitching algorithm
+     *  @param  pAlgorithm the address of the parent stitching algorithm
      *  @param  pointingClusterMap  the mapping between Pfos and their corresponding 3D pointing clusters
-     *  @param  pfoMerges  the input map of Pfo merges
-     *  @param  stitchingInfo  the stitching information block
+     *  @param  pfoMerges the input map of Pfo merges
+     *  @param  stitchingInfo the stitching information block
      */
     void StitchPfos(const StitchingAlgorithm *const pAlgorithm, const ThreeDPointingClusterMap &pointingClusterMap,
         const PfoMergeMap &pfoMerges, StitchingAlgorithm::StitchingInfo &stitchingInfo) const;
@@ -207,12 +198,12 @@ private:
     /**
      *  @brief  Calculate x0 shift for a group of associated Pfos
      *
-     *  @param  pfoToVolumeIdMap  the mapping between pfos and drift volume IDs
-     *  @param  pointingClusterMap  the mapping between Pfos and their corresponding 3D pointing clusters
-     *  @param  pfoVector  the vector of parent Pfos to stitch together
-     *  @param  x0  the output x0 value
+     *  @param  pfoToLArTPCMap the mapping between pfos and tpc
+     *  @param  pointingClusterMap the mapping between Pfos and their corresponding 3D pointing clusters
+     *  @param  pfoVector the vector of parent Pfos to stitch together
+     *  @param  x0 the output x0 value
      */
-    void CalculateX0(const PfoToVolumeIdMap &pfoToVolumeIdMap, const ThreeDPointingClusterMap &pointingClusterMap,
+    void CalculateX0(const PfoToLArTPCMap &pfoToLArTPCMap, const ThreeDPointingClusterMap &pointingClusterMap,
         const pandora::PfoVector &pfoVector, float &x0) const;
 
     bool  m_useXcoordinate;
@@ -224,13 +215,6 @@ private:
     float m_relaxCosRelativeAngle;
     float m_relaxTransverseDisplacement;
 };
-
-//------------------------------------------------------------------------------------------------------------------------------------------
-
-inline pandora::AlgorithmTool *StitchingCosmicRayMergingTool::Factory::CreateAlgorithmTool() const
-{
-    return new StitchingCosmicRayMergingTool();
-}
 
 } // namespace lar_content
 
