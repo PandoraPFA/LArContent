99345609cc807191ce7c31a68fec0a8d4a2c5eac
Steven Green <sg1sg2sg3@hotmail.co.uk>
2018-03-27 13:58:02 +0100

    Added CheatingRemovingCosmicRays algorithm.

---
+++ larpandoracontent/LArCheating/CheatingRemovingCosmicRays.cc
@@ -0,0 +1,72 @@
+/**
+ *  @file   larpandoracontent/LArCheating/CheatingRemovingCosmicRays.cc
+ * 
+ *  @brief  Implementation of the cheating removing cosmic rays algorithm class.
+ * 
+ *  $Log: $
+ */
+
+#include "Pandora/AlgorithmHeaders.h"
+#include "Helpers/MCParticleHelper.h"
+
+#include "larpandoracontent/LArHelpers/LArMCParticleHelper.h"
+
+#include "larpandoracontent/LArCheating/CheatingRemovingCosmicRays.h"
+
+using namespace pandora;
+
+namespace lar_content
+{
+
+CheatingRemovingCosmicRays::CheatingRemovingCosmicRays() :
+    m_inputCaloHitListName(""),
+    m_mcParticleListName("")
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode CheatingRemovingCosmicRays::Run()
+{
+    const MCParticleList *pMCParticleList(nullptr);
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_mcParticleListName, pMCParticleList));
+
+    const CaloHitList *pCaloHitList(nullptr);
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*this, m_inputCaloHitListName, pCaloHitList));
+
+    CaloHitList outputCaloHitList;
+
+    for (const CaloHit *pCaloHit : *pCaloHitList)
+    {
+        try
+        {
+            const MCParticle *const pHitParticle(MCParticleHelper::GetMainMCParticle(pCaloHit));
+
+            if (!LArMCParticleHelper::IsCosmicRay(pHitParticle))
+                outputCaloHitList.push_back(pCaloHit);
+        }
+        catch (...)
+        {
+            std::cout << "CheatingRemovingCosmicRays::Run - Calo hit has no available MainMCParticle." << std::endl;
+            continue;
+        }
+    }
+
+    PANDORA_THROW_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::SaveList(*this, outputCaloHitList, m_outputCaloHitListName));
+    PANDORA_THROW_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::ReplaceCurrentList<CaloHit>(*this, m_outputCaloHitListName));
+
+    return STATUS_CODE_SUCCESS;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode CheatingRemovingCosmicRays::ReadSettings(const TiXmlHandle xmlHandle)
+{
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "InputCaloHitListName", m_inputCaloHitListName));
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "MCParticleListName", m_mcParticleListName));
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "OutputCaloHitListName", m_outputCaloHitListName));
+
+    return STATUS_CODE_SUCCESS;
+}
+
+} // namespace lar_content
---
+++ larpandoracontent/LArCheating/CheatingRemovingCosmicRays.h
@@ -0,0 +1,39 @@
+/**
+ *  @file   larpandoracontent/LArCheating/CheatingRemovingCosmicRays.h
+ * 
+ *  @brief  Header file for the cheating removing cosmic rays algorithm class.
+ * 
+ *  $Log: $
+ */
+#ifndef LAR_CHEATING_REMOVING_COSMIC_RAYS_H
+#define LAR_CHEATING_REMOVING_COSMIC_RAYS_H 1
+
+#include "Pandora/Algorithm.h"
+
+namespace lar_content
+{
+
+/**
+ *  @brief  CheatingRemovingCosmicRays::Algorithm class
+ */
+class CheatingRemovingCosmicRays : public pandora::Algorithm
+{
+public:
+    /**
+     *  @brief  Default constructor
+     */
+    CheatingRemovingCosmicRays();
+
+private:
+    pandora::StatusCode Run();
+
+    pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
+
+    std::string m_inputCaloHitListName;     ///< Input calo hit list name
+    std::string m_outputCaloHitListName;    ///< Output calo hit list name
+    std::string m_mcParticleListName;       ///< MC Particle list name
+};
+
+} // namespace lar_content
+
+#endif // #ifndef LAR_CHEATING_REMOVING_COSMIC_RAYS_H
