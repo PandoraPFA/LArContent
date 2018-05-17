4768652c2b16e8a574f72c382010fc0b26b5dbc2
John Marshall <marshall@hep.phy.cam.ac.uk>
2018-05-17 14:59:39 +0100

    Updated lar rotational transformation plugin implementation to handle a non-zero w wire angle. Sign convention for v wire angle also changed.

--- larpandoracontent/LArPlugins/LArRotationalTransformationPlugin.h
+++ larpandoracontent/LArPlugins/LArRotationalTransformationPlugin.h
@@ -27,31 +27,43 @@ public:
     virtual double UVtoW(const double u, const double v) const;
     virtual double VWtoU(const double v, const double w) const;
     virtual double WUtoV(const double w, const double u) const;
+
     virtual double UVtoY(const double u, const double v) const;
     virtual double UVtoZ(const double u, const double v) const;
+    virtual double UWtoY(const double u, const double w) const;
+    virtual double UWtoZ(const double u, const double w) const;
+    virtual double VWtoY(const double v, const double w) const;
+    virtual double VWtoZ(const double v, const double w) const;
+
     virtual double YZtoU(const double y, const double z) const;
     virtual double YZtoV(const double y, const double z) const;
+    virtual double YZtoW(const double y, const double z) const;
+
     virtual void GetMinChiSquaredYZ(const double u, const double v, const double w, const double sigmaU, const double sigmaV, const double sigmaW,
         double &y, double &z, double &chiSquared) const;
     virtual void GetMinChiSquaredYZ(const double u, const double v, const double w, const double sigmaU, const double sigmaV, const double sigmaW,
         const double uFit, const double vFit, const double wFit, const double sigmaFit, double &y, double &z, double &chiSquared) const;
-    virtual void GetProjectedYZ(const PositionAndType &hitPositionAndType, const PositionAndType &fitPositionAndType1,
-        const PositionAndType &fitPositionAndType2, const double sigmaHit, const double sigmaFit, double &y, double &z, double &chiSquared) const;
 
 private:
     pandora::StatusCode Initialize();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
-    double    m_thetaU;          ///< inclination of U wires (radians)
-    double    m_thetaV;          ///< inclination of V wires (radians)
-    double    m_sinUplusV;       ///< sin(thetaU+thetaV)
-    double    m_sinU;            ///< sin(thetaU)
-    double    m_sinV;            ///< sin(thetaV)
-    double    m_cosU;            ///< cos(thetaU)
-    double    m_cosV;            ///< cos(thetaV)
+    double    m_thetaU;                 ///< inclination of U wires (radians)
+    double    m_thetaV;                 ///< inclination of V wires (radians)
+    double    m_thetaW;                 ///< inclination of W wires (radians)
 
-    double    m_maxAngularDiscrepancy;      ///< Maximum allowed difference between like wire (u and v) angles between LArTPCs
-    double    m_maxSigmaDiscrepancy;        ///< Maximum allowed difference between like wire sigma values between LArTPCs
+    double    m_sinU;                   ///< sin(thetaU)
+    double    m_sinV;                   ///< sin(thetaV)
+    double    m_sinW;                   ///< sin(thetaW)
+    double    m_cosU;                   ///< cos(thetaU)
+    double    m_cosV;                   ///< cos(thetaV)
+    double    m_cosW;                   ///< cos(thetaW)
+    double    m_sinVminusU;             ///< sin(thetaV - thetaU)
+    double    m_sinWminusV;             ///< sin(thetaW - thetaV)
+    double    m_sinUminusW;             ///< sin(thetaU - thetaW)
+
+    double    m_maxAngularDiscrepancy;  ///< Maximum allowed difference between like wire (u and v) angles between LArTPCs
+    double    m_maxSigmaDiscrepancy;    ///< Maximum allowed difference between like wire sigma values between LArTPCs
 };
 
 } // namespace lar_content
