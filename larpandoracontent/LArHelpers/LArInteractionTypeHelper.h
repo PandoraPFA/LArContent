57c3e400efd09ff4d91413831e57745bc011386f
StevenGreen1 <sg1sg2sg3@hotmail.co.uk>
2019-07-01 15:00:23 +0100

    Using preprocessor to define interaction types.

--- larpandoracontent/LArHelpers/LArInteractionTypeHelper.h
+++ larpandoracontent/LArHelpers/LArInteractionTypeHelper.h
@@ -15,6 +15,223 @@
 namespace lar_content
 {
 
+// Specify (name)
+#define INTERACTION_TYPE_TABLE(d)                         \
+    d(CCQEL_MU                                          ) \
+    d(CCQEL_MU_P                                        ) \
+    d(CCQEL_MU_P_P                                      ) \
+    d(CCQEL_MU_P_P_P                                    ) \
+    d(CCQEL_MU_P_P_P_P                                  ) \
+    d(CCQEL_MU_P_P_P_P_P                                ) \
+    d(CCQEL_E                                           ) \
+    d(CCQEL_E_P                                         ) \
+    d(CCQEL_E_P_P                                       ) \
+    d(CCQEL_E_P_P_P                                     ) \
+    d(CCQEL_E_P_P_P_P                                   ) \
+    d(CCQEL_E_P_P_P_P_P                                 ) \
+    d(NCQEL_P                                           ) \
+    d(NCQEL_P_P                                         ) \
+    d(NCQEL_P_P_P                                       ) \
+    d(NCQEL_P_P_P_P                                     ) \
+    d(NCQEL_P_P_P_P_P                                   ) \
+    d(CCRES_MU                                          ) \
+    d(CCRES_MU_P                                        ) \
+    d(CCRES_MU_P_P                                      ) \
+    d(CCRES_MU_P_P_P                                    ) \
+    d(CCRES_MU_P_P_P_P                                  ) \
+    d(CCRES_MU_P_P_P_P_P                                ) \
+    d(CCRES_MU_PIPLUS                                   ) \
+    d(CCRES_MU_P_PIPLUS                                 ) \
+    d(CCRES_MU_P_P_PIPLUS                               ) \
+    d(CCRES_MU_P_P_P_PIPLUS                             ) \
+    d(CCRES_MU_P_P_P_P_PIPLUS                           ) \
+    d(CCRES_MU_P_P_P_P_P_PIPLUS                         ) \
+    d(CCRES_MU_PHOTON                                   ) \
+    d(CCRES_MU_P_PHOTON                                 ) \
+    d(CCRES_MU_P_P_PHOTON                               ) \
+    d(CCRES_MU_P_P_P_PHOTON                             ) \
+    d(CCRES_MU_P_P_P_P_PHOTON                           ) \
+    d(CCRES_MU_P_P_P_P_P_PHOTON                         ) \
+    d(CCRES_MU_PIZERO                                   ) \
+    d(CCRES_MU_P_PIZERO                                 ) \
+    d(CCRES_MU_P_P_PIZERO                               ) \
+    d(CCRES_MU_P_P_P_PIZERO                             ) \
+    d(CCRES_MU_P_P_P_P_PIZERO                           ) \
+    d(CCRES_MU_P_P_P_P_P_PIZERO                         ) \
+    d(CCRES_E                                           ) \
+    d(CCRES_E_P                                         ) \
+    d(CCRES_E_P_P                                       ) \
+    d(CCRES_E_P_P_P                                     ) \
+    d(CCRES_E_P_P_P_P                                   ) \
+    d(CCRES_E_P_P_P_P_P                                 ) \
+    d(CCRES_E_PIPLUS                                    ) \
+    d(CCRES_E_P_PIPLUS                                  ) \
+    d(CCRES_E_P_P_PIPLUS                                ) \
+    d(CCRES_E_P_P_P_PIPLUS                              ) \
+    d(CCRES_E_P_P_P_P_PIPLUS                            ) \
+    d(CCRES_E_P_P_P_P_P_PIPLUS                          ) \
+    d(CCRES_E_PHOTON                                    ) \
+    d(CCRES_E_P_PHOTON                                  ) \
+    d(CCRES_E_P_P_PHOTON                                ) \
+    d(CCRES_E_P_P_P_PHOTON                              ) \
+    d(CCRES_E_P_P_P_P_PHOTON                            ) \
+    d(CCRES_E_P_P_P_P_P_PHOTON                          ) \
+    d(CCRES_E_PIZERO                                    ) \
+    d(CCRES_E_P_PIZERO                                  ) \
+    d(CCRES_E_P_P_PIZERO                                ) \
+    d(CCRES_E_P_P_P_PIZERO                              ) \
+    d(CCRES_E_P_P_P_P_PIZERO                            ) \
+    d(CCRES_E_P_P_P_P_P_PIZERO                          ) \
+    d(NCRES_P                                           ) \
+    d(NCRES_P_P                                         ) \
+    d(NCRES_P_P_P                                       ) \
+    d(NCRES_P_P_P_P                                     ) \
+    d(NCRES_P_P_P_P_P                                   ) \
+    d(NCRES_PIPLUS                                      ) \
+    d(NCRES_P_PIPLUS                                    ) \
+    d(NCRES_P_P_PIPLUS                                  ) \
+    d(NCRES_P_P_P_PIPLUS                                ) \
+    d(NCRES_P_P_P_P_PIPLUS                              ) \
+    d(NCRES_P_P_P_P_P_PIPLUS                            ) \
+    d(NCRES_PIMINUS                                     ) \
+    d(NCRES_P_PIMINUS                                   ) \
+    d(NCRES_P_P_PIMINUS                                 ) \
+    d(NCRES_P_P_P_PIMINUS                               ) \
+    d(NCRES_P_P_P_P_PIMINUS                             ) \
+    d(NCRES_P_P_P_P_P_PIMINUS                           ) \
+    d(NCRES_PHOTON                                      ) \
+    d(NCRES_P_PHOTON                                    ) \
+    d(NCRES_P_P_PHOTON                                  ) \
+    d(NCRES_P_P_P_PHOTON                                ) \
+    d(NCRES_P_P_P_P_PHOTON                              ) \
+    d(NCRES_P_P_P_P_P_PHOTON                            ) \
+    d(NCRES_PIZERO                                      ) \
+    d(NCRES_P_PIZERO                                    ) \
+    d(NCRES_P_P_PIZERO                                  ) \
+    d(NCRES_P_P_P_PIZERO                                ) \
+    d(NCRES_P_P_P_P_PIZERO                              ) \
+    d(NCRES_P_P_P_P_P_PIZERO                            ) \
+    d(CCDIS_MU                                          ) \
+    d(CCDIS_MU_P                                        ) \
+    d(CCDIS_MU_P_P                                      ) \
+    d(CCDIS_MU_P_P_P                                    ) \
+    d(CCDIS_MU_P_P_P_P                                  ) \
+    d(CCDIS_MU_P_P_P_P_P                                ) \
+    d(CCDIS_MU_PIPLUS                                   ) \
+    d(CCDIS_MU_P_PIPLUS                                 ) \
+    d(CCDIS_MU_P_P_PIPLUS                               ) \
+    d(CCDIS_MU_P_P_P_PIPLUS                             ) \
+    d(CCDIS_MU_P_P_P_P_PIPLUS                           ) \
+    d(CCDIS_MU_P_P_P_P_P_PIPLUS                         ) \
+    d(CCDIS_MU_PHOTON                                   ) \
+    d(CCDIS_MU_P_PHOTON                                 ) \
+    d(CCDIS_MU_P_P_PHOTON                               ) \
+    d(CCDIS_MU_P_P_P_PHOTON                             ) \
+    d(CCDIS_MU_P_P_P_P_PHOTON                           ) \
+    d(CCDIS_MU_P_P_P_P_P_PHOTON                         ) \
+    d(CCDIS_MU_PIZERO                                   ) \
+    d(CCDIS_MU_P_PIZERO                                 ) \
+    d(CCDIS_MU_P_P_PIZERO                               ) \
+    d(CCDIS_MU_P_P_P_PIZERO                             ) \
+    d(CCDIS_MU_P_P_P_P_PIZERO                           ) \
+    d(CCDIS_MU_P_P_P_P_P_PIZERO                         ) \
+    d(NCDIS_P                                           ) \
+    d(NCDIS_P_P                                         ) \
+    d(NCDIS_P_P_P                                       ) \
+    d(NCDIS_P_P_P_P                                     ) \
+    d(NCDIS_P_P_P_P_P                                   ) \
+    d(NCDIS_PIPLUS                                      ) \
+    d(NCDIS_P_PIPLUS                                    ) \
+    d(NCDIS_P_P_PIPLUS                                  ) \
+    d(NCDIS_P_P_P_PIPLUS                                ) \
+    d(NCDIS_P_P_P_P_PIPLUS                              ) \
+    d(NCDIS_P_P_P_P_P_PIPLUS                            ) \
+    d(NCDIS_PIMINUS                                     ) \
+    d(NCDIS_P_PIMINUS                                   ) \
+    d(NCDIS_P_P_PIMINUS                                 ) \
+    d(NCDIS_P_P_P_PIMINUS                               ) \
+    d(NCDIS_P_P_P_P_PIMINUS                             ) \
+    d(NCDIS_P_P_P_P_P_PIMINUS                           ) \
+    d(NCDIS_PHOTON                                      ) \
+    d(NCDIS_P_PHOTON                                    ) \
+    d(NCDIS_P_P_PHOTON                                  ) \
+    d(NCDIS_P_P_P_PHOTON                                ) \
+    d(NCDIS_P_P_P_P_PHOTON                              ) \
+    d(NCDIS_P_P_P_P_P_PHOTON                            ) \
+    d(NCDIS_PIZERO                                      ) \
+    d(NCDIS_P_PIZERO                                    ) \
+    d(NCDIS_P_P_PIZERO                                  ) \
+    d(NCDIS_P_P_P_PIZERO                                ) \
+    d(NCDIS_P_P_P_P_PIZERO                              ) \
+    d(NCDIS_P_P_P_P_P_PIZERO                            ) \
+    d(CCCOH                                             ) \
+    d(NCCOH                                             ) \
+    d(COSMIC_RAY_MU                                     ) \
+    d(COSMIC_RAY_P                                      ) \
+    d(COSMIC_RAY_E                                      ) \
+    d(COSMIC_RAY_PHOTON                                 ) \
+    d(COSMIC_RAY_OTHER                                  ) \
+    d(BEAM_PARTICLE_MU                                  ) \
+    d(BEAM_PARTICLE_P                                   ) \
+    d(BEAM_PARTICLE_E                                   ) \
+    d(BEAM_PARTICLE_PHOTON                              ) \
+    d(BEAM_PARTICLE_PI_PLUS                             ) \
+    d(BEAM_PARTICLE_PI_MINUS                            ) \
+    d(BEAM_PARTICLE_KAON_PLUS                           ) \
+    d(BEAM_PARTICLE_KAON_MINUS                          ) \
+    d(BEAM_PARTICLE_OTHER                               ) \
+    d(BEAM_PARTICLE_PI_PLUS_PI_PLUS                     ) \
+    d(BEAM_PARTICLE_PI_PLUS_PI_PLUS_PHOTON              ) \
+    d(BEAM_PARTICLE_PI_PLUS_PI_PLUS_PIZERO              ) \
+    d(BEAM_PARTICLE_PI_PLUS_COMPLEX                     ) \
+    d(BEAM_PARTICLE_PI_MINUS_PI_MINUS                   ) \
+    d(BEAM_PARTICLE_PI_MINUS_PI_MINUS_PHOTON            ) \
+    d(BEAM_PARTICLE_PI_MINUS_PI_MINUS_PIZERO            ) \
+    d(BEAM_PARTICLE_PI_MINUS_COMPLEX                    ) \
+    d(BEAM_PARTICLE_P_P                                 ) \
+    d(BEAM_PARTICLE_P_P_PHOTON                          ) \
+    d(BEAM_PARTICLE_P_P_PHOTON_PHOTON                   ) \
+    d(BEAM_PARTICLE_P_P_PHOTON_PHOTON_PHOTON            ) \
+    d(BEAM_PARTICLE_P_P_PHOTON_PHOTON_PHOTON_PHOTON     ) \
+    d(BEAM_PARTICLE_P_P_P                               ) \
+    d(BEAM_PARTICLE_P_P_P_PHOTON                        ) \
+    d(BEAM_PARTICLE_P_P_P_PHOTON_PHOTON                 ) \
+    d(BEAM_PARTICLE_P_P_P_PHOTON_PHOTON_PHOTON          ) \
+    d(BEAM_PARTICLE_P_P_P_P                             ) \
+    d(BEAM_PARTICLE_P_P_P_P_PHOTON                      ) \
+    d(BEAM_PARTICLE_P_P_P_P_PHOTON_PHOTON               ) \
+    d(BEAM_PARTICLE_P_P_P_P_P                           ) \
+    d(BEAM_PARTICLE_P_P_P_P_P_PHOTON                    ) \
+    d(BEAM_PARTICLE_P_P_P_P_P_P                         ) \
+    d(BEAM_PARTICLE_P_COMPLEX                           ) \
+    d(BEAM_PARTICLE_MU_E                                ) \
+    d(BEAM_PARTICLE_MU_COMPLEX                          ) \
+    d(BEAM_PARTICLE_KAON_PLUS_MU                        ) \
+    d(BEAM_PARTICLE_KAON_PLUS_KAON_PLUS_KAON0L_COMPLEX  ) \
+    d(BEAM_PARTICLE_KAON_PLUS_KAON_PLUS_COMPLEX         ) \
+    d(BEAM_PARTICLE_KAON_PLUS_COMPLEX                   ) \
+    d(BEAM_PARTICLE_KAON_MINUS_MU                       ) \
+    d(BEAM_PARTICLE_KAON_MINUS_KAON_MINUS_KAON0L_COMPLEX) \
+    d(BEAM_PARTICLE_KAON_MINUS_KAON_MINUS_COMPLEX       ) \
+    d(BEAM_PARTICLE_KAON_MINUS_COMPLEX                  ) \
+    d(BEAM_PARTICLE_E_COMPLEX                           ) \
+    d(BEAM_PARTICLE_COMPLEX_HIERARCHY                   ) \
+    d(BEAM_PARTICLE_UNKNOWN_HIERARCHY                   ) \
+    d(OTHER_INTERACTION                                 ) \
+    d(ALL_INTERACTIONS                                  )
+
+/**
+ *  @brief  The particle type enum macro
+ */
+#define GET_INTERACTION_TYPE_ENTRY(a)                     \
+    a,
+
+/**
+ *  @brief  The name switch statement macro
+ */
+#define GET_INTERACTION_TYPE_NAME_SWITCH(a)               \
+    case a : return std::string(#a);
+
 /**
  *  @brief  LArInteractionTypeHelper class
  */
@@ -26,208 +243,8 @@ public:
      */
     enum InteractionType : unsigned int
     {
-        CCQEL_MU,
-        CCQEL_MU_P,
-        CCQEL_MU_P_P,
-        CCQEL_MU_P_P_P,
-        CCQEL_MU_P_P_P_P,
-        CCQEL_MU_P_P_P_P_P,
-        CCQEL_E,
-        CCQEL_E_P,
-        CCQEL_E_P_P,
-        CCQEL_E_P_P_P,
-        CCQEL_E_P_P_P_P,
-        CCQEL_E_P_P_P_P_P,
-        NCQEL_P,
-        NCQEL_P_P,
-        NCQEL_P_P_P,
-        NCQEL_P_P_P_P,
-        NCQEL_P_P_P_P_P,
-        CCRES_MU,
-        CCRES_MU_P,
-        CCRES_MU_P_P,
-        CCRES_MU_P_P_P,
-        CCRES_MU_P_P_P_P,
-        CCRES_MU_P_P_P_P_P,
-        CCRES_MU_PIPLUS,
-        CCRES_MU_P_PIPLUS,
-        CCRES_MU_P_P_PIPLUS,
-        CCRES_MU_P_P_P_PIPLUS,
-        CCRES_MU_P_P_P_P_PIPLUS,
-        CCRES_MU_P_P_P_P_P_PIPLUS,
-        CCRES_MU_PHOTON,
-        CCRES_MU_P_PHOTON,
-        CCRES_MU_P_P_PHOTON,
-        CCRES_MU_P_P_P_PHOTON,
-        CCRES_MU_P_P_P_P_PHOTON,
-        CCRES_MU_P_P_P_P_P_PHOTON,
-        CCRES_MU_PIZERO,
-        CCRES_MU_P_PIZERO,
-        CCRES_MU_P_P_PIZERO,
-        CCRES_MU_P_P_P_PIZERO,
-        CCRES_MU_P_P_P_P_PIZERO,
-        CCRES_MU_P_P_P_P_P_PIZERO,
-        CCRES_E,
-        CCRES_E_P,
-        CCRES_E_P_P,
-        CCRES_E_P_P_P,
-        CCRES_E_P_P_P_P,
-        CCRES_E_P_P_P_P_P,
-        CCRES_E_PIPLUS,
-        CCRES_E_P_PIPLUS,
-        CCRES_E_P_P_PIPLUS,
-        CCRES_E_P_P_P_PIPLUS,
-        CCRES_E_P_P_P_P_PIPLUS,
-        CCRES_E_P_P_P_P_P_PIPLUS,
-        CCRES_E_PHOTON,
-        CCRES_E_P_PHOTON,
-        CCRES_E_P_P_PHOTON,
-        CCRES_E_P_P_P_PHOTON,
-        CCRES_E_P_P_P_P_PHOTON,
-        CCRES_E_P_P_P_P_P_PHOTON,
-        CCRES_E_PIZERO,
-        CCRES_E_P_PIZERO,
-        CCRES_E_P_P_PIZERO,
-        CCRES_E_P_P_P_PIZERO,
-        CCRES_E_P_P_P_P_PIZERO,
-        CCRES_E_P_P_P_P_P_PIZERO,
-        NCRES_P,
-        NCRES_P_P,
-        NCRES_P_P_P,
-        NCRES_P_P_P_P,
-        NCRES_P_P_P_P_P,
-        NCRES_PIPLUS,
-        NCRES_P_PIPLUS,
-        NCRES_P_P_PIPLUS,
-        NCRES_P_P_P_PIPLUS,
-        NCRES_P_P_P_P_PIPLUS,
-        NCRES_P_P_P_P_P_PIPLUS,
-        NCRES_PIMINUS,
-        NCRES_P_PIMINUS,
-        NCRES_P_P_PIMINUS,
-        NCRES_P_P_P_PIMINUS,
-        NCRES_P_P_P_P_PIMINUS,
-        NCRES_P_P_P_P_P_PIMINUS,
-        NCRES_PHOTON,
-        NCRES_P_PHOTON,
-        NCRES_P_P_PHOTON,
-        NCRES_P_P_P_PHOTON,
-        NCRES_P_P_P_P_PHOTON,
-        NCRES_P_P_P_P_P_PHOTON,
-        NCRES_PIZERO,
-        NCRES_P_PIZERO,
-        NCRES_P_P_PIZERO,
-        NCRES_P_P_P_PIZERO,
-        NCRES_P_P_P_P_PIZERO,
-        NCRES_P_P_P_P_P_PIZERO,
-        CCDIS_MU,
-        CCDIS_MU_P,
-        CCDIS_MU_P_P,
-        CCDIS_MU_P_P_P,
-        CCDIS_MU_P_P_P_P,
-        CCDIS_MU_P_P_P_P_P,
-        CCDIS_MU_PIPLUS,
-        CCDIS_MU_P_PIPLUS,
-        CCDIS_MU_P_P_PIPLUS,
-        CCDIS_MU_P_P_P_PIPLUS,
-        CCDIS_MU_P_P_P_P_PIPLUS,
-        CCDIS_MU_P_P_P_P_P_PIPLUS,
-        CCDIS_MU_PHOTON,
-        CCDIS_MU_P_PHOTON,
-        CCDIS_MU_P_P_PHOTON,
-        CCDIS_MU_P_P_P_PHOTON,
-        CCDIS_MU_P_P_P_P_PHOTON,
-        CCDIS_MU_P_P_P_P_P_PHOTON,
-        CCDIS_MU_PIZERO,
-        CCDIS_MU_P_PIZERO,
-        CCDIS_MU_P_P_PIZERO,
-        CCDIS_MU_P_P_P_PIZERO,
-        CCDIS_MU_P_P_P_P_PIZERO,
-        CCDIS_MU_P_P_P_P_P_PIZERO,
-        NCDIS_P,
-        NCDIS_P_P,
-        NCDIS_P_P_P,
-        NCDIS_P_P_P_P,
-        NCDIS_P_P_P_P_P,
-        NCDIS_PIPLUS,
-        NCDIS_P_PIPLUS,
-        NCDIS_P_P_PIPLUS,
-        NCDIS_P_P_P_PIPLUS,
-        NCDIS_P_P_P_P_PIPLUS,
-        NCDIS_P_P_P_P_P_PIPLUS,
-        NCDIS_PIMINUS,
-        NCDIS_P_PIMINUS,
-        NCDIS_P_P_PIMINUS,
-        NCDIS_P_P_P_PIMINUS,
-        NCDIS_P_P_P_P_PIMINUS,
-        NCDIS_P_P_P_P_P_PIMINUS,
-        NCDIS_PHOTON,
-        NCDIS_P_PHOTON,
-        NCDIS_P_P_PHOTON,
-        NCDIS_P_P_P_PHOTON,
-        NCDIS_P_P_P_P_PHOTON,
-        NCDIS_P_P_P_P_P_PHOTON,
-        NCDIS_PIZERO,
-        NCDIS_P_PIZERO,
-        NCDIS_P_P_PIZERO,
-        NCDIS_P_P_P_PIZERO,
-        NCDIS_P_P_P_P_PIZERO,
-        NCDIS_P_P_P_P_P_PIZERO,
-        CCCOH,
-        NCCOH,
-        COSMIC_RAY_MU,
-        COSMIC_RAY_P,
-        COSMIC_RAY_E,
-        COSMIC_RAY_PHOTON,
-        COSMIC_RAY_OTHER,
-        BEAM_PARTICLE_MU,
-        BEAM_PARTICLE_P,
-        BEAM_PARTICLE_E,
-        BEAM_PARTICLE_PHOTON,
-        BEAM_PARTICLE_PI_PLUS,
-        BEAM_PARTICLE_PI_MINUS,
-        BEAM_PARTICLE_KAON_PLUS,
-        BEAM_PARTICLE_KAON_MINUS,
-        BEAM_PARTICLE_OTHER,
-        BEAM_PARTICLE_PI_PLUS_PI_PLUS,
-        BEAM_PARTICLE_PI_PLUS_PI_PLUS_PHOTON,
-        BEAM_PARTICLE_PI_PLUS_PI_PLUS_PIZERO,
-        BEAM_PARTICLE_PI_PLUS_COMPLEX,
-        BEAM_PARTICLE_PI_MINUS_PI_MINUS,
-        BEAM_PARTICLE_PI_MINUS_PI_MINUS_PHOTON,
-        BEAM_PARTICLE_PI_MINUS_PI_MINUS_PIZERO,
-        BEAM_PARTICLE_PI_MINUS_COMPLEX,
-        BEAM_PARTICLE_P_P,
-        BEAM_PARTICLE_P_P_PHOTON,
-        BEAM_PARTICLE_P_P_PHOTON_PHOTON,
-        BEAM_PARTICLE_P_P_PHOTON_PHOTON_PHOTON,
-        BEAM_PARTICLE_P_P_PHOTON_PHOTON_PHOTON_PHOTON,
-        BEAM_PARTICLE_P_P_P,
-        BEAM_PARTICLE_P_P_P_PHOTON,
-        BEAM_PARTICLE_P_P_P_PHOTON_PHOTON,
-        BEAM_PARTICLE_P_P_P_PHOTON_PHOTON_PHOTON,
-        BEAM_PARTICLE_P_P_P_P,
-        BEAM_PARTICLE_P_P_P_P_PHOTON,
-        BEAM_PARTICLE_P_P_P_P_PHOTON_PHOTON,
-        BEAM_PARTICLE_P_P_P_P_P,
-        BEAM_PARTICLE_P_P_P_P_P_PHOTON,
-        BEAM_PARTICLE_P_P_P_P_P_P,
-        BEAM_PARTICLE_P_COMPLEX,
-        BEAM_PARTICLE_MU_E,
-        BEAM_PARTICLE_MU_COMPLEX,
-        BEAM_PARTICLE_KAON_PLUS_MU,
-        BEAM_PARTICLE_KAON_PLUS_KAON_PLUS_KAON0L_COMPLEX,
-        BEAM_PARTICLE_KAON_PLUS_KAON_PLUS_COMPLEX,
-        BEAM_PARTICLE_KAON_PLUS_COMPLEX,
-        BEAM_PARTICLE_KAON_MINUS_MU,
-        BEAM_PARTICLE_KAON_MINUS_KAON_MINUS_KAON0L_COMPLEX,
-        BEAM_PARTICLE_KAON_MINUS_KAON_MINUS_COMPLEX,
-        BEAM_PARTICLE_KAON_MINUS_COMPLEX,
-        BEAM_PARTICLE_E_COMPLEX,
-        BEAM_PARTICLE_COMPLEX_HIERARCHY,
-        BEAM_PARTICLE_UNKNOWN_HIERARCHY,
-        OTHER_INTERACTION,
-        ALL_INTERACTIONS
+        INTERACTION_TYPE_TABLE(GET_INTERACTION_TYPE_ENTRY)
+        UNKNOWN_INTERACTION_TYPE = 0
     };
 
     /**
