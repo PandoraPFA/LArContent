b29bd12ff11f610ed0d46778346c11bf2fdb661d
loressa <escudero@hep.phy.cam.ac.uk>
2019-08-30 18:21:37 +0100

    Applying SCE corrections as of August 2019, following uboone transformations. Changes in CMakeLists.txt to work both in standalone Pandora and LArSoft. Example usage given in CheatingVertexCreationAlgorithm. Note1: using only forward transformation of the true position for this example (others can be added to do the opposite and correct a reconstructed position, for example in EventValidationAlgorithm). Note2: tested and used in uboone, with a hard-coded path for the SCE file, changed here (not tested) to read this path with ReadSettings function

--- larpandoracontent/LArCheating/CheatingVertexCreationAlgorithm.cc
+++ larpandoracontent/LArCheating/CheatingVertexCreationAlgorithm.cc
@@ -13,6 +13,9 @@
 
 #include "larpandoracontent/LArCheating/CheatingVertexCreationAlgorithm.h"
 
+#include "TH3F.h"
+#include "TFile.h"
+
 using namespace pandora;
 
 namespace lar_content
@@ -34,13 +37,38 @@ StatusCode CheatingVertexCreationAlgorithm::Run()
     const VertexList *pVertexList(nullptr); std::string temporaryListName;
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::CreateTemporaryListAndSetCurrent(*this, pVertexList, temporaryListName));
 
+    TFile infile(m_pathToSCEFile.c_str(),"READ");
+
+    if(!infile.IsOpen())
+    {
+        std::cout << "Could not find the space charge effect file !\n";
+        return STATUS_CODE_NOT_FOUND;
+    }
+
+    TH3F* m_hDx = (TH3F*)infile.Get("hDx");
+    TH3F* m_hDy = (TH3F*)infile.Get("hDy");
+    TH3F* m_hDz = (TH3F*)infile.Get("hDz");
+
     for (const MCParticle *const pMCParticle : *pMCParticleList)
     {
         if (!LArMCParticleHelper::IsNeutrino(pMCParticle))
             continue;
 
+        // SCE 2019: First step is to transform position of the neutrino vertex to match SCE maps provided
+        const CartesianVector transformedPosition(TransformX(pMCParticle->GetEndpoint().GetX()), TransformY(pMCParticle->GetEndpoint().GetY()),TransformZ(pMCParticle->GetEndpoint().GetZ()) );
+
+        // Then, calculate offsets with the TH3F provided, in the transformed position
+        const CartesianVector positionOffset(m_hDx->Interpolate(transformedPosition.GetX(),transformedPosition.GetY(),transformedPosition.GetZ()),
+                                             m_hDy->Interpolate(transformedPosition.GetX(),transformedPosition.GetY(),transformedPosition.GetZ()),
+                                             m_hDz->Interpolate(transformedPosition.GetX(),transformedPosition.GetY(),transformedPosition.GetZ()));
+
+        // Correct neutrino vertex with the offsets calculated
+        const CartesianVector targetVertexSCE(pMCParticle->GetEndpoint().GetX() - positionOffset.GetX(),
+                                              pMCParticle->GetEndpoint().GetY() + positionOffset.GetY(),
+                                              pMCParticle->GetEndpoint().GetZ() + positionOffset.GetZ());
+
         PandoraContentApi::Vertex::Parameters parameters;
-        parameters.m_position = CartesianVector(pMCParticle->GetEndpoint().GetX() + m_vertexXCorrection, pMCParticle->GetEndpoint().GetY(), pMCParticle->GetEndpoint().GetZ());
+        parameters.m_position = CartesianVector(targetVertexSCE.GetX() + m_vertexXCorrection, targetVertexSCE.GetY(), targetVertexSCE.GetZ());
         parameters.m_vertexLabel = VERTEX_INTERACTION;
         parameters.m_vertexType = VERTEX_3D;
 
@@ -60,11 +88,41 @@ StatusCode CheatingVertexCreationAlgorithm::Run()
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
+/// Transformation coordinates for the maps provided for MicroBooNE as of August 2019
+
+float CheatingVertexCreationAlgorithm::TransformX(float xPosition) const
+{
+  const float newPos(2.50f - (2.50f/2.56f)*(xPosition/100.0f));
+  return newPos;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+/// Transform Y to SCE Y coordinate:  [0.0,6.08] --> [0.0,6.0]
+
+float CheatingVertexCreationAlgorithm::TransformY(float yPosition) const
+{
+    const float newPos((2.50f/2.33f)*((yPosition/100.0f)+1.165f));
+    return newPos;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+/// Transform Z to SCE Z coordinate:  [0.0,6.97] --> [0.0,7.2]
+
+float CheatingVertexCreationAlgorithm::TransformZ(float zPosition) const
+{
+    const float newPos((10.0f/10.37f)*(zPosition/100.0f));
+    return newPos;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
 
 StatusCode CheatingVertexCreationAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
 {
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle,
         "OutputVertexListName", m_outputVertexListName));
+
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle,
+        "PathToSCEFile", m_pathToSCEFile));
 
     PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
         "ReplaceCurrentVertexList", m_replaceCurrentVertexList));
--- larpandoracontent/LArCheating/CheatingVertexCreationAlgorithm.h
+++ larpandoracontent/LArCheating/CheatingVertexCreationAlgorithm.h
@@ -28,7 +28,19 @@ private:
     pandora::StatusCode Run();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
+    /**
+     *  @brief Functions for coordinate transformations to match SCE maps
+     *
+     *  @param xPosition the initial position to transformate, in each coordinate separately
+     *
+     *  @return the transformed position
+     */
+    float TransformX(float xPosition) const;
+    float TransformY(float yPosition) const;
+    float TransformZ(float zPosition) const;
+
     std::string     m_outputVertexListName;         ///< The name under which to save the output vertex list
+    std::string     m_pathToSCEFile;                ///< The path to the file with the SCE map to use for corrections
     bool            m_replaceCurrentVertexList;     ///< Whether to replace the current vertex list with the output list
     float           m_vertexXCorrection;            ///< The vertex x correction, added to reported mc neutrino endpoint x value, in cm
 };
