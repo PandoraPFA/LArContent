4b9c6b8b2852ad16aa2e948dd79700e2f1fbf420
StevenGreen1 <sg1sg2sg3@hotmail.co.uk>
2019-10-23 11:39:27 +0100

    Adding X0 to output validation root trees. rebase

--- larpandoracontent/LArMonitoring/TestBeamEventValidationAlgorithm.cc
+++ larpandoracontent/LArMonitoring/TestBeamEventValidationAlgorithm.cc
@@ -205,7 +205,8 @@ void TestBeamEventValidationAlgorithm::ProcessOutput(const ValidationInfo &valid
                 bestMatchPfoNSharedHitsU.push_back(LArMonitoringHelper::CountHitsByType(TPC_VIEW_U, sharedHitList));
                 bestMatchPfoNSharedHitsV.push_back(LArMonitoringHelper::CountHitsByType(TPC_VIEW_V, sharedHitList));
                 bestMatchPfoNSharedHitsW.push_back(LArMonitoringHelper::CountHitsByType(TPC_VIEW_W, sharedHitList));
-                bestMatchPfoX0.push_back(pfoToSharedHits.first->GetPropertiesMap().count("X0") ? pfoToSharedHits.first->GetPropertiesMap().at("X0") : std::numeric_limits<float>::max());
+
+                bestMatchPfoX0.push_back(pfoToSharedHits.first->GetPropertiesMap().count("X0") ? pfoToSharedHits.first->GetPropertiesMap().at("X0") : 0.f);
 
 #ifdef MONITORING
                 try
@@ -246,7 +247,7 @@ void TestBeamEventValidationAlgorithm::ProcessOutput(const ValidationInfo &valid
             bestMatchPfoNHitsTotal.push_back(0); bestMatchPfoNHitsU.push_back(0);
             bestMatchPfoNHitsV.push_back(0); bestMatchPfoNHitsW.push_back(0); bestMatchPfoNSharedHitsTotal.push_back(0);
             bestMatchPfoNSharedHitsU.push_back(0); bestMatchPfoNSharedHitsV.push_back(0); bestMatchPfoNSharedHitsW.push_back(0);
-            bestMatchPfoX0.push_back(std::numeric_limits<float>::max());
+            bestMatchPfoX0.push_back(0.f);
 
         }
 
