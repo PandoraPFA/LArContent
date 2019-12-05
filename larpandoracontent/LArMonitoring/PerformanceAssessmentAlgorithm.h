2bf7ad7bb44d2b121b64dc85d731072484843fae
Isobel Mawby <Isobel.Mawby@warwick.ac.uk>
2019-12-05 18:56:34 +0000

    Added new ParticleEfficiency and PullData algorithms

--- larpandoracontent/LArMonitoring/PerformanceAssessmentAlgorithm.h
+++ larpandoracontent/LArMonitoring/PerformanceAssessmentAlgorithm.h
@@ -32,9 +32,21 @@ namespace lar_content {
   private:
     pandora::StatusCode Run();
 
+    void FillUpstreamContainers(const pandora::MCParticleList *const pMCParticleList, pandora::MCParticleList &neutronUpstreamMCParticles, pandora::MCParticleList &photonUpstreamMCParticles, std::map<const pandora::MCParticle*, float> &photonUpstreamMCParticlesToPropagationMap);
+
+    void FillPhotonTree(const LArMCParticleHelper::MCContributionMap &photonUpstreamMCParticleHitMap, const pandora::MCParticleList &neutronUpstreamMCParticles, const std::map<const pandora::MCParticle*, float> &photonUpstreamMCParticlesToPropagationMap);
+
+    void FillNeutronTree(const LArMCParticleHelper::MCContributionMap &neutronUpstreamMCParticleHitMap);
+
+    void PerformPhotonNeutronTest(const pandora::MCParticleList *const pMCParticleList, const pandora::CaloHitList *const pCaloHitList);
+
     void GetMatchedMCParticlePfoCompleteness(const LArMCParticleHelper::MCParticleToPfoCompletenessPurityMap &mcParticleToPfoCompletenessMap, const pandora::MCParticle *const pMCParticle, const pandora::ParticleFlowObject *const pPfo, double &completeness);
 
     void GetMatchedMCParticlePfoPurity(const LArMCParticleHelper::MCParticleToPfoCompletenessPurityMap &mcParticleToPfoPurityMap, const pandora::MCParticle *const pMCParticle, const pandora::ParticleFlowObject *const pPfo, double &purity);
+
+    void GetMCParticleChainVector(const pandora::MCParticle *const pMCParticle, pandora::MCParticleVector &mcParticleChainList);
+
+    bool IsUpstream(const pandora::MCParticle *const pChainMCParticleDaughter, const pandora::MCParticle *const pEndOfChainMCParticle);
 
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
@@ -47,6 +59,9 @@ namespace lar_content {
     std::string  m_eventTreeName;
     std::string  m_targetMCParticleTreeName;
     std::string  m_fileName;
+
+    std::string  m_neutronFileName;
+    std::string  m_photonFileName;
 
     int m_eventNumber;
 
