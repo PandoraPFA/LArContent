ea3b36418f4055695f29d29b3d0570d0d9b7752d
phuncr <43882062+phuncr@users.noreply.github.com>
2020-01-18 18:32:34 +0000

    Adding Deep Learning vertexing algorithm and Deep Learning vertexing feature tool.

---
+++ larpandoracontent/LArVertex/MyDlVtxFeatureTool.cc
@@ -0,0 +1,80 @@
+/**
+ *  @file   larpandoracontent/MyDlVtxFeatureTool.cc
+ *
+ *  @brief  Implementation of the myDlVtx feature tool class.
+ *
+ *  $Log: $
+ */
+
+#include "Pandora/AlgorithmHeaders.h"
+
+#include "larpandoracontent/LArHelpers/LArGeometryHelper.h"
+#include "larpandoracontent/LArHelpers/LArClusterHelper.h"
+
+#include "larpandoracontent/LArVertex/MyDlVtxFeatureTool.h"
+
+using namespace pandora;
+
+namespace lar_content
+{
+
+MyDlVtxFeatureTool::MyDlVtxFeatureTool() :
+    m_myDlVtxConstant(17)
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void MyDlVtxFeatureTool::Run(LArMvaHelper::MvaFeatureVector &featureVector, const VertexSelectionBaseAlgorithm *const pAlgorithm, const Vertex * const pVertex,
+    const VertexSelectionBaseAlgorithm::SlidingFitDataListMap &/*slidingFitDataListMap*/, const VertexSelectionBaseAlgorithm::ClusterListMap &clusterListMap,
+    const VertexSelectionBaseAlgorithm::KDTreeMap &, const VertexSelectionBaseAlgorithm::ShowerClusterListMap &, const float, float &)
+{
+    if (PandoraContentApi::GetSettings(*pAlgorithm)->ShouldDisplayAlgorithmInfo())
+       std::cout << "----> Running Algorithm Tool: " << this->GetInstanceName() << ", " << this->GetType() << std::endl;
+
+    const VertexList *pInputVertexList(NULL);
+    std::string vertexListName("DlVertices3D");
+    PANDORA_THROW_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetList(*pAlgorithm, vertexListName.c_str(), pInputVertexList));
+    const Vertex * const pDlVertex(pInputVertexList->front());
+
+    CartesianVector DlVertexPosition(pDlVertex->GetPosition());
+
+    if(DlVertexPosition.GetX()==0 && DlVertexPosition.GetY()==0 && DlVertexPosition.GetZ()==0)
+        featureVector.push_back(0.f);
+    else
+    {
+        float score((pVertex->GetPosition()-pDlVertex->GetPosition()).GetMagnitude());
+
+        ClusterList clustersW(clusterListMap.at(TPC_VIEW_W));
+        ClusterList sortedLongClusters;
+        for (const Cluster *const pCluster : clustersW)
+            if (pCluster->GetNCaloHits() > 4) 
+                sortedLongClusters.push_back(pCluster);
+
+        if(sortedLongClusters.empty())
+            featureVector.push_back(0.f);
+        else
+        {
+            CartesianVector inner(0.f, 0.f, 0.f), outer(0.f, 0.f, 0.f);
+            LArClusterHelper::GetExtremalCoordinates(sortedLongClusters, inner, outer);
+            double rdist = std::max(fabs(outer.GetZ()-inner.GetZ()), fabs(outer.GetX()-inner.GetX()));
+            rdist = std::max(rdist,(1e-9));
+
+            float MyDlVtxFeature(-m_myDlVtxConstant*score/rdist);
+
+            featureVector.push_back(MyDlVtxFeature);
+        }
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+StatusCode MyDlVtxFeatureTool::ReadSettings(const TiXmlHandle xmlHandle)
+{
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle,
+        "MyDlVtxConstant", m_myDlVtxConstant));
+
+    return STATUS_CODE_SUCCESS;
+}
+
+} // namespace lar_content
---
+++ larpandoracontent/LArVertex/MyDlVtxFeatureTool.h
@@ -0,0 +1,47 @@
+/**
+ *  @file   larpandoracontent/MyDlVtxFeatureTool.h
+ *
+ *  @brief  Header file for the myDlVtx feature tool class.
+ *
+ *  $Log: $
+ */
+#ifndef LAR_MYDLVTX_FEATURE_TOOL_H
+#define LAR_MYDLVTX_FEATURE_TOOL_H 1
+
+#include "larpandoracontent/LArVertex/VertexSelectionBaseAlgorithm.h"
+
+namespace lar_content
+{
+
+/**
+ *  @brief  MyDlVtxFeatureTool class
+ */
+class MyDlVtxFeatureTool : public VertexSelectionBaseAlgorithm::VertexFeatureTool
+{
+public:
+    /**
+     *  @brief  Default constructor
+     */
+    MyDlVtxFeatureTool();
+
+    /**
+     *  @brief  Run the tool
+     *
+     *  @param  featureVector containing the MyDlVtx feature
+     *  @param  pAlgorithm address of the calling algorithm
+     *  @param  pVertex address of the vertex
+     *
+     */
+    void Run(LArMvaHelper::MvaFeatureVector &featureVector, const VertexSelectionBaseAlgorithm * const pAlgorithm, const pandora::Vertex * const pVertex,
+        const VertexSelectionBaseAlgorithm::SlidingFitDataListMap &/*slidingFitDataListMap*/,const VertexSelectionBaseAlgorithm::ClusterListMap &,
+        const VertexSelectionBaseAlgorithm::KDTreeMap &, const VertexSelectionBaseAlgorithm::ShowerClusterListMap &, const float, float &);
+
+private:
+    pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
+
+    float           m_myDlVtxConstant ;          ///< The DlVtx constant
+};
+
+} // namespace lar_content
+
+#endif // #ifndef LAR_MYDLVTX_FEATURE_TOOL_H
