02acd55f7b11764e534e4edd8c34b5127017f57a
Isobel Mawby <isobelmawby@isobels-air.lan>
2020-05-05 11:53:03 +0100

    changes - need to sort out clusterVector issue

--- larpandoracontent/LArHelpers/LArHitWidthHelper.cc
+++ larpandoracontent/LArHelpers/LArHitWidthHelper.cc
@@ -60,15 +60,6 @@ LArHitWidthHelper::ClusterParameters::ClusterParameters(const Cluster *const pCl
 //------------------------------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-LArHitWidthHelper::SortByHigherXExtrema::SortByHigherXExtrema(const ClusterToParametersMap &clusterToParametersMap) :
-    m_clusterToParametersMap(clusterToParametersMap)
-{
-    if (m_clusterToParametersMap.empty())
-        throw StatusCodeException(STATUS_CODE_NOT_INITIALIZED);
-}
-
-//------------------------------------------------------------------------------------------------------------------------------------------
-
 bool LArHitWidthHelper::SortByHigherXExtrema::operator() (const Cluster *const pLhs, const Cluster *const pRhs)
 {
     const LArHitWidthHelper::ClusterParameters& lhsClusterParameters(LArHitWidthHelper::GetClusterParameters(pLhs, m_clusterToParametersMap));
