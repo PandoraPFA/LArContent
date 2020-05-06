8012eb66f74da59f1a1c87016ce1bec093efa7dd
Isobel Mawby <isobelmawby@isobels-air.lan>
2020-05-06 14:26:41 +0100

    PR changes

--- larpandoracontent/LArMonitoring/TestBeamEventValidationAlgorithm.cc
+++ larpandoracontent/LArMonitoring/TestBeamEventValidationAlgorithm.cc
@@ -206,7 +206,6 @@ void TestBeamEventValidationAlgorithm::ProcessOutput(const ValidationInfo &valid
                 bestMatchPfoNSharedHitsV.push_back(LArMonitoringHelper::CountHitsByType(TPC_VIEW_V, sharedHitList));
                 bestMatchPfoNSharedHitsW.push_back(LArMonitoringHelper::CountHitsByType(TPC_VIEW_W, sharedHitList));
                 bestMatchPfoX0.push_back(pfoToSharedHits.first->GetPropertiesMap().count("X0") ? pfoToSharedHits.first->GetPropertiesMap().at("X0") : std::numeric_limits<float>::max());
-
 #ifdef MONITORING
                 try
                 {
@@ -247,7 +246,6 @@ void TestBeamEventValidationAlgorithm::ProcessOutput(const ValidationInfo &valid
             bestMatchPfoNHitsV.push_back(0); bestMatchPfoNHitsW.push_back(0); bestMatchPfoNSharedHitsTotal.push_back(0);
             bestMatchPfoNSharedHitsU.push_back(0); bestMatchPfoNSharedHitsV.push_back(0); bestMatchPfoNSharedHitsW.push_back(0);
             bestMatchPfoX0.push_back(std::numeric_limits<float>::max());
-
         }
 
         nPrimaryMatchedPfos.push_back(nPrimaryMatches);
