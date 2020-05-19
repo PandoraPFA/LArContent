6a348b770562cf814d171efdeeb723d59124c34b
Dom Brailsford <dominicbrailsford@gmail.com>
2020-05-19 10:59:36 -0500

    Move template implementation back to the header to keep the maintenance easier

--- larpandoracontent/LArHelpers/LArDiscreteProbabilityHelper.h
+++ larpandoracontent/LArHelpers/LArDiscreteProbabilityHelper.h
@@ -27,8 +27,9 @@ public:
     template <typename T>
     static float CalculateMean(const T &t);
 
-
 private:
+
+
 
     template <typename T>
     static size_t GetSize(const T &t);
@@ -57,6 +58,55 @@ private:
     */
    //static float CumulDistLinearInterpolation(const float &xPos, const DiscreteCumulativeDistribution &distribution);
 };
+
+
+template <typename T>
+float LArDiscreteProbabilityHelper::CalculateCorrelationCoefficient(const T &t1, const T &t2)
+{
+    if (GetSize(t1) != (GetSize(t2)))
+        throw pandora::StatusCodeException(pandora::STATUS_CODE_INVALID_PARAMETER);
+
+    if (2 > GetSize(t1))
+        throw pandora::StatusCodeException(pandora::STATUS_CODE_INVALID_PARAMETER);
+
+    float mean1(CalculateMean(t1)); 
+    float mean2(CalculateMean(t2));
+
+    float variance1(0.f);
+    float variance2(0.f);
+    float covariance(0.f);
+
+    for (size_t iElement = 0; iElement < GetSize(t1); iElement++)
+    {
+        float element1(GetElement(t1,iElement));
+        float element2(GetElement(t2,iElement));
+
+        variance1 += (element1-mean1)*(element1-mean1);
+        variance2 += (element2-mean2)*(element2-mean2);
+        covariance += (element1-mean1)*(element2-mean2);
+    }
+
+    if(variance1*variance2 < std::numeric_limits<float>::epsilon())
+        throw pandora::StatusCodeException(pandora::STATUS_CODE_FAILURE);
+
+    return covariance /= std::sqrt(variance1*variance2);
+}
+
+template <typename T>
+float LArDiscreteProbabilityHelper::CalculateMean(const T &t)
+{
+    if (0 == GetSize(t))
+        throw pandora::StatusCodeException(pandora::STATUS_CODE_NOT_INITIALIZED);
+
+    float mean(0.f);
+    for (size_t iElement = 0; iElement < GetSize(t); ++iElement)
+    {
+        mean+=GetElement(t,iElement);
+    }
+    mean /= static_cast<float>(GetSize(t));
+
+    return mean;
+}
 
 template <typename T>
 inline size_t LArDiscreteProbabilityHelper::GetSize(const T &t)
