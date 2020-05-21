f310f5fee624bc30f3c80b9eead27bd196e2e841
Dom Brailsford <dominicbrailsford@gmail.com>
2020-05-21 06:20:59 -0500

    Wrapper function for CalculateOverlapResult so that a statuscode can be returned.  Easier for debugging and copies the three view case. Some minor moving of checks from within the CalculateOverlapResult function.  Still under construction

--- larpandoracontent/LArThreeDReco/LArTwoViewMatching/TwoViewTransverseTracksAlgorithm.h
+++ larpandoracontent/LArThreeDReco/LArTwoViewMatching/TwoViewTransverseTracksAlgorithm.h
@@ -12,12 +12,9 @@
 #include "Pandora/AlgorithmTool.h"
 
 #include "larpandoracontent/LArObjects/LArTrackTwoViewOverlapResult.h"
-#include "larpandoracontent/LArObjects/LArDiscreteCumulativeDistribution.h"
 
 #include "larpandoracontent/LArThreeDReco/LArThreeDBase/NViewTrackMatchingAlgorithm.h"
 #include "larpandoracontent/LArThreeDReco/LArThreeDBase/TwoViewMatchingControl.h"
-
-#include <unsupported/Eigen/Splines>
 
 namespace lar_content
 {
@@ -40,11 +37,10 @@ public:
     TwoViewTransverseTracksAlgorithm();
     virtual ~TwoViewTransverseTracksAlgorithm();
 
-    typedef Eigen::Spline<float, 1, 1> Spline1f;
-    typedef std::vector<std::pair<float,float> > ChargeProfile;
-    typedef std::vector<std::pair<float,float> > ScoreProfile;
 private:
     void CalculateOverlapResult(const pandora::Cluster *const pCluster1, const pandora::Cluster *const pCluster2, const pandora::Cluster *const);
+    pandora::StatusCode CalculateOverlapResult(const pandora::Cluster *const pCluster1, const pandora::Cluster *const pCluster2, TwoViewTransverseOverlapResult &overlapResult);
+
     void ExamineOverlapContainer();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
@@ -52,16 +48,8 @@ private:
     MatrixToolVector            m_algorithmToolVector;      ///< The algorithm tool vector
 
     unsigned int                m_nMaxMatrixToolRepeats;    ///< The maximum number of repeat loops over matrix tools
-
-    Spline1f CreateSplineFromCumulativeDistribution(const DiscreteCumulativeDistribution &cumulativeDistribution);
-    ChargeProfile CreateProfileFromCumulativeDistribution(const DiscreteCumulativeDistribution &cumulativeDistribution);
-    float CalculateCorrelationCoefficient(const ChargeProfile &profile1, const ChargeProfile &profile2);
-    float CalculateTTestValue(const float x, const float coefficient, const float dof);
-    float CalculateTTestPValue(const ChargeProfile &profile1, const ChargeProfile &profile2);
-    ScoreProfile SlidingWindowMatchingScore(const size_t &sizeWindowInBins, const ChargeProfile &profile1, const ChargeProfile &profile2, float &fracGoodScore);
-    ChargeProfile   GetWindow(size_t &i, const size_t &sizeWindowInBins, const ChargeProfile &profile);
-    ScoreProfile SlidingWindowMatchingScore(const size_t &sizeWindowInBins, const DiscreteCumulativeDistribution &disCumulDist1, const DiscreteCumulativeDistribution &disCumulDist2, float &fracGoodScore);
-    ChargeProfile   GetWindow(size_t &i, const size_t &sizeWindowInBins, const DiscreteCumulativeDistribution &disCumulDist);
+    unsigned int                m_downsampleFactor;         ///< The downsampling (hit merging) applied to hits in the overlap region
+    unsigned int                m_minSamples;               ///< The minimum number of samples needed for comparing charges
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
