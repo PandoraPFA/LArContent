6e5532ba4f329d4b16849888aa66a592ec5c963e
Maria Brigida Brunetti <mariabrigida@Marias-MacBook-Air.local>
2020-07-07 16:02:22 +0100

    Fill TwoViewSimpleTracksTool. Small cosmetic changes to TwoViewLongTracksTool

--- larpandoracontent/LArThreeDReco/LArTwoViewMatching/TwoViewLongTracksTool.h
+++ larpandoracontent/LArThreeDReco/LArTwoViewMatching/TwoViewLongTracksTool.h
@@ -38,7 +38,7 @@ public:
      *  @brief  Whether a long element is significantly longer that other elements with which it shares a cluster
      *
      *  @param  iIter specifies the long element under consideration
-     *  @param  elementList the full list of connected tensor elements
+     *  @param  elementList the full list of connected matrix elements
      *  @param  minMatchedSamplingPointRatio the min ratio between 1st and 2nd highest msps for simple ambiguity resolution
      *  @param  usedClusters the list of clusters already marked as to be added to a pfo
      */
@@ -51,17 +51,17 @@ private:
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
     /**
-     *  @brief  Find long tracks, hidden by simple ambiguities in the tensor
+     *  @brief  Find long tracks, hidden by simple ambiguities in the matrix 
      *
-     *  @param  overlapTensor the overlap tensor
+     *  @param  overlapMatrix the overlap matrix 
      *  @param  protoParticleVector to receive the list of proto particles
      */
     void FindLongTracks(const MatrixType &overlapMatrix, ProtoParticleVector &protoParticleVector) const;
 
     /**
-     *  @brief  Select a list of long track-like elements from a set of connected tensor elements
+     *  @brief  Select a list of long track-like elements from a set of connected matrix elements
      *
-     *  @param  elementList the full list of connected tensor elements
+     *  @param  elementList the full list of connected matrix elements
      *  @param  usedClusters the list of clusters already marked as to be added to a pfo
      *  @param  iteratorList to receive a list of iterators to long track-like elements
      */
@@ -69,7 +69,7 @@ private:
         IteratorList &iteratorList) const;
 
     float           m_minMatchedFraction;               ///< The min matched sampling point fraction for particle creation
-    float           m_minMatchingScore;                 ///< The min global matcing score for particle creation
+    float           m_minMatchingScore;                 ///< The min global matching score for particle creation
     unsigned int    m_minMatchedSamplingPoints;         ///< The min number of matched sampling points for particle creation
     float           m_minXOverlapFraction;              ///< The min x overlap fraction (in each view) for particle creation
     unsigned int    m_minMatchedSamplingPointRatio;     ///< The min ratio between 1st and 2nd highest msps for simple ambiguity resolution
