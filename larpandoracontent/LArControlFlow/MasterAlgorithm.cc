79680b64c5618431cf353613dd4975a0987d2d99
Etienne Chardonnet <echardon@dunegpvm04.fnal.gov>
2020-08-14 10:19:34 -0500

    Added in TwoViewTransverseTracksAlgorithm and CosmicRayTrackRecoveryAlgorithm a check if compared clusters belong to the same daughter volume, this commit contains also the implementation of the check

--- larpandoracontent/LArControlFlow/MasterAlgorithm.cc
+++ larpandoracontent/LArControlFlow/MasterAlgorithm.cc
@@ -670,6 +670,7 @@ StatusCode MasterAlgorithm::Copy(const Pandora *const pPandora, const CaloHit *c
     // ATTN Parent of calo hit in worker is corresponding calo hit in master
     parameters.m_pParentAddress = static_cast<const void*>(pCaloHit);
     parameters.m_larTPCVolumeId = pLArCaloHit->GetLArTPCVolumeId();
+    parameters.m_daughterVolumeId = pLArCaloHit->GetDaughterVolumeId();
 
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraApi::CaloHit::Create(*pPandora, parameters, m_larCaloHitFactory));
 
