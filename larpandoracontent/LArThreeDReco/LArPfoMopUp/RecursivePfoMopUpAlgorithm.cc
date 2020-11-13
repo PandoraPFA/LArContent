5e2a991de29fb224881da39e14851c74c0b26fdf
Edward Tyley <e.tyley@sheffield.ac.uk>
2020-11-13 10:30:25 -0600

    Apply clang-format

--- larpandoracontent/LArThreeDReco/LArPfoMopUp/RecursivePfoMopUpAlgorithm.cc
+++ larpandoracontent/LArThreeDReco/LArPfoMopUp/RecursivePfoMopUpAlgorithm.cc
@@ -13,75 +13,74 @@
 
 using namespace pandora;
 
-namespace lar_content {
+namespace lar_content
+{
 
 StatusCode RecursivePfoMopUpAlgorithm::Run()
 {
-  std::vector<RecursivePfoMopUpAlgorithm::pfoMergeStats> mergeStatsVecBefore(GetPfoMergeStats());
+    std::vector<RecursivePfoMopUpAlgorithm::pfoMergeStats> mergeStatsVecBefore(GetPfoMergeStats());
 
-  for (unsigned int iter = 0; iter < m_maxIterations; ++iter) {
+    for (unsigned int iter = 0; iter < m_maxIterations; ++iter)
+    {
 
-    for (auto const& mopUpAlg : m_mopUpAlgorithms) {
-      PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::RunDaughterAlgorithm(*this, mopUpAlg));
-    }
+        for (auto const &mopUpAlg : m_mopUpAlgorithms)
+            PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::RunDaughterAlgorithm(*this, mopUpAlg));
 
-    std::vector<RecursivePfoMopUpAlgorithm::pfoMergeStats> mergeStatsVecAfter(GetPfoMergeStats());
+        std::vector<RecursivePfoMopUpAlgorithm::pfoMergeStats> mergeStatsVecAfter(GetPfoMergeStats());
 
-    if (std::equal(mergeStatsVecBefore.cbegin(), mergeStatsVecBefore.cend(), mergeStatsVecAfter.cbegin(), mergeStatsVecAfter.cend(),
-            RecursivePfoMopUpAlgorithm::pfoMergeStatsComp))
-      break;
+        if (std::equal(mergeStatsVecBefore.cbegin(), mergeStatsVecBefore.cend(), mergeStatsVecAfter.cbegin(), mergeStatsVecAfter.cend(),
+                RecursivePfoMopUpAlgorithm::pfoMergeStatsComp))
+            break;
 
-    mergeStatsVecBefore = std::move(mergeStatsVecAfter);
-  } // while !unchanged
+        mergeStatsVecBefore = std::move(mergeStatsVecAfter);
+    } // iter
 
-  return STATUS_CODE_SUCCESS;
+    return STATUS_CODE_SUCCESS;
 } // Run
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 std::vector<RecursivePfoMopUpAlgorithm::pfoMergeStats> RecursivePfoMopUpAlgorithm::GetPfoMergeStats() const
 {
-  std::vector<RecursivePfoMopUpAlgorithm::pfoMergeStats> pfoMergeStatsVec;
+    std::vector<RecursivePfoMopUpAlgorithm::pfoMergeStats> pfoMergeStatsVec;
 
-  for (auto const& pfoListName : m_pfoListNames) {
+    for (auto const &pfoListName : m_pfoListNames)
+    {
+        const PfoList *pPfoList = NULL;
+        if (STATUS_CODE_SUCCESS != PandoraContentApi::GetList(*this, pfoListName, pPfoList))
+            continue;
 
-    const PfoList* pPfoList = NULL;
-    if (STATUS_CODE_SUCCESS != PandoraContentApi::GetList(*this, pfoListName, pPfoList))
-      continue;
+        for (const ParticleFlowObject *const pPfo : *pPfoList)
+        {
+            std::vector<unsigned int> pfoHits;
+            ClusterList clusterList;
+            LArPfoHelper::GetTwoDClusterList(pPfo, clusterList);
+            for (auto const &cluster : clusterList)
+            {
+                pfoHits.push_back(cluster->GetNCaloHits());
+            }
 
-    for (const ParticleFlowObject* const pPfo : *pPfoList) {
+            const PropertiesMap &pfoMeta(pPfo->GetPropertiesMap());
+            const auto &trackScoreIter(pfoMeta.find("TrackScore"));
+            const float trackScore(trackScoreIter != pfoMeta.end() ? trackScoreIter->second : -1.f);
 
-      std::vector<unsigned int> pfoHits;
-      ClusterList clusterList;
-      LArPfoHelper::GetTwoDClusterList(pPfo, clusterList);
-      for (auto const& cluster : clusterList) {
-        pfoHits.push_back(cluster->GetNCaloHits());
-      }
-
-      const PropertiesMap& pfoMeta(pPfo->GetPropertiesMap());
-      const auto& trackScoreIter(pfoMeta.find("TrackScore"));
-      const float trackScore(trackScoreIter != pfoMeta.end() ? trackScoreIter->second : -1.f);
-
-      pfoMergeStatsVec.emplace_back(RecursivePfoMopUpAlgorithm::pfoMergeStats{ pfoHits, trackScore });
-    } // pPfo : pPfoList
-  }   // pfoListName : m_pfoListNames
-  return pfoMergeStatsVec;
+            pfoMergeStatsVec.emplace_back(RecursivePfoMopUpAlgorithm::pfoMergeStats{pfoHits, trackScore});
+        } // pPfo : pPfoList
+    }     // pfoListName : m_pfoListNames
+    return pfoMergeStatsVec;
 } // GetPfoMergeStats
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
 StatusCode RecursivePfoMopUpAlgorithm::ReadSettings(const pandora::TiXmlHandle xmlHandle)
 {
-  PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=,
-      XmlHelper::ProcessAlgorithmList(*this, xmlHandle, "MopUpAlgorithms", m_mopUpAlgorithms));
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ProcessAlgorithmList(*this, xmlHandle, "MopUpAlgorithms", m_mopUpAlgorithms));
 
-  PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=,
-      XmlHelper::ReadVectorOfValues(xmlHandle, "PfoListNames", m_pfoListNames));
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadVectorOfValues(xmlHandle, "PfoListNames", m_pfoListNames));
 
-  PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=,
-      XmlHelper::ReadValue(xmlHandle, "MaxIterations", m_maxIterations));
+    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "MaxIterations", m_maxIterations));
 
-  return STATUS_CODE_SUCCESS;
+    return STATUS_CODE_SUCCESS;
 } // ReadSettings
 
 } // namespace lar_content
--- larpandoracontent/LArThreeDReco/LArPfoMopUp/RecursivePfoMopUpAlgorithm.h
+++ larpandoracontent/LArThreeDReco/LArPfoMopUp/RecursivePfoMopUpAlgorithm.h
@@ -11,30 +11,33 @@
 
 #include "Pandora/Algorithm.h"
 
-namespace lar_content {
+namespace lar_content
+{
 
 /**
  *  @brief  RecursivePfoMopUpAlgorithm class
  */
-class RecursivePfoMopUpAlgorithm : public pandora::Algorithm {
-  private:
-  struct pfoMergeStats {                      ///< Object to compare PFO before/after merging algs have run to see if anything changed
-    const std::vector<unsigned int> mNumHits; ///< Vector filled with number of hits in each of the PFO's clusters
-    const float mTrackScore;                  ///< MVA "Track Score" for the PFO
-  };
+class RecursivePfoMopUpAlgorithm : public pandora::Algorithm
+{
+private:
+    struct pfoMergeStats
+    {                                             ///< Object to compare PFO before/after merging algs have run to see if anything changed
+        const std::vector<unsigned int> mNumHits; ///< Vector filled with number of hits in each of the PFO's clusters
+        const float mTrackScore;                  ///< MVA "Track Score" for the PFO
+    };
 
-  static bool pfoMergeStatsComp(const pfoMergeStats& lhs, const pfoMergeStats& rhs) ///< Comparitor  for pfoMergeStats
-  {
-    return ((lhs.mNumHits == rhs.mNumHits) && (std::abs(lhs.mTrackScore - rhs.mTrackScore) < std::numeric_limits<float>::epsilon()));
-  };
+    static bool pfoMergeStatsComp(const pfoMergeStats &lhs, const pfoMergeStats &rhs) ///< Comparitor  for pfoMergeStats
+    {
+        return ((lhs.mNumHits == rhs.mNumHits) && (std::abs(lhs.mTrackScore - rhs.mTrackScore) < std::numeric_limits<float>::epsilon()));
+    };
 
-  pandora::StatusCode Run();
-  std::vector<pfoMergeStats> GetPfoMergeStats() const; ///< Vector filled with pfoMergeStats for each PFP in m_pfoListNames
-  pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
+    pandora::StatusCode Run();
+    std::vector<pfoMergeStats> GetPfoMergeStats() const; ///< Vector filled with pfoMergeStats for each PFP in m_pfoListNames
+    pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
-  unsigned int m_maxIterations;            ///< Maximum number of iterations
-  pandora::StringVector m_pfoListNames;    ///< The list of pfo list names
-  pandora::StringVector m_mopUpAlgorithms; ///< Ordered list of mop up algorithms to run
+    unsigned int m_maxIterations;            ///< Maximum number of iterations
+    pandora::StringVector m_pfoListNames;    ///< The list of pfo list names
+    pandora::StringVector m_mopUpAlgorithms; ///< Ordered list of mop up algorithms to run
 };
 
 } // namespace lar_content
