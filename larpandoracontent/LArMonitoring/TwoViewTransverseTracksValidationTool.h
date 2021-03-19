3cf2acf0bda6fec962498f1b7083f5cdcd1f0e80
Dom Brailsford <dominicbrailsford@gmail.com>
2021-03-19 05:06:57 -0500

    Use make_unique to construct the ptr

--- larpandoracontent/LArMonitoring/TwoViewTransverseTracksValidationTool.h
+++ larpandoracontent/LArMonitoring/TwoViewTransverseTracksValidationTool.h
@@ -111,7 +111,7 @@ void TwoViewTransverseTracksValidationTool::TreeDatum<T>::emplace_back_impl(cons
 template <typename T>
 void TwoViewTransverseTracksValidationTool::StoreAndRegisterDatum(const T &t, const std::string datumName, TreeDataBox &treeDataBox)
 {
-    auto [iterator, inserted] = treeDataBox.try_emplace(datumName, (new TreeDatum<T>(t, this, datumName)));
+    auto [iterator, inserted] = treeDataBox.try_emplace(datumName, std::make_unique<TreeDatum<T> >(t, this, datumName));
     if (!inserted)
         throw pandora::StatusCodeException(pandora::STATUS_CODE_NOT_FOUND);
     return;
