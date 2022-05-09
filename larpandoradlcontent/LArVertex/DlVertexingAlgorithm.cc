ee6c6c737decde73ae419264ec62b291c511f044
Andrew Chappell <A.Chappell@warwick.ac.uk>
2022-05-09 13:15:35 +0100

    Update validation outputs

--- larpandoradlcontent/LArVertex/DlVertexingAlgorithm.cc
+++ larpandoradlcontent/LArVertex/DlVertexingAlgorithm.cc
@@ -301,7 +301,8 @@ StatusCode DlVertexingAlgorithm::Infer()
 //                                ")" << std::endl;
                             if (LArVertexHelper::IsInFiducialVolume(trueVertex, "dune_fd_hd"))
                             {
-                                const CartesianVector &recoVertex{vertexTuples.back().GetPosition()};
+                                //std::cout << " - In FV" << std::endl;
+                                const CartesianVector &recoVertex{vertexTuples.front().GetPosition()};
                                 const float tx{trueVertex.GetX()};
                                 const float tu{static_cast<float>(transform->YZtoU(trueVertex.GetY(), trueVertex.GetZ()))};
                                 const float tv{static_cast<float>(transform->YZtoV(trueVertex.GetY(), trueVertex.GetZ()))};
@@ -315,16 +316,23 @@ StatusCode DlVertexingAlgorithm::Infer()
                                 const float dr_u{std::sqrt((rx_u - tx) * (rx_u - tx) + (ru - tu) * (ru - tu))};
                                 const float dr_v{std::sqrt((rx_v - tx) * (rx_v - tx) + (rv - tv) * (rv - tv))};
                                 const float dr_w{std::sqrt((rx_w - tx) * (rx_w - tx) + (rw - tw) * (rw - tw))};
-                                const float dr{(recoVertex - trueVertex).GetMagnitude()};
-    /*                            std::cout << "Truth: " << tx << " " << tu << " " << tv << " " << tw << std::endl;
+                                const CartesianVector &dv{recoVertex - trueVertex};
+                                const float dr{dv.GetMagnitude()};
+                                const float dx{dv.GetX()}, dy{dv.GetY()}, dz{dv.GetZ()};
+                                //std::cout << "Truth: " << trueVertex.GetX() << " " << trueVertex.GetY() << " " << trueVertex.GetZ() << std::endl;
+                                /*std::cout << "Truth: " << tx << " " << tu << " " << tv << " " << tw << std::endl;
                                 std::cout << "U: " << rx_u << " " << ru << " " << dr_u << std::endl;
                                 std::cout << "V: " << rx_v << " " << rv << " " << dr_v << std::endl;
                                 std::cout << "W: " << rx_w << " " << rw << " " << dr_w << std::endl;*/
+                                PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), "vertex", "event", event));
                                 PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), "vertex", "pass", m_pass));
                                 PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), "vertex", "dr_u", dr_u));
                                 PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), "vertex", "dr_v", dr_v));
                                 PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), "vertex", "dr_w", dr_w));
                                 PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), "vertex", "dr", dr));
+                                PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), "vertex", "dx", dx));
+                                PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), "vertex", "dy", dy));
+                                PANDORA_MONITORING_API(SetTreeVariable(this->GetPandora(), "vertex", "dz", dz));
                                 PANDORA_MONITORING_API(FillTree(this->GetPandora(), "vertex"));
                             }
                         }
