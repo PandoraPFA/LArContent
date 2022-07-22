061db141cd9f03b18bdaa1e25ddc42bc119dcf36
Bruce Howard <howard@fnal.gov>
2022-07-22 11:07:18 -0500

    MvaFeatureMapDbl -> DoubleMap as name

--- larpandoracontent/LArHelpers/LArMvaHelper.h
+++ larpandoracontent/LArHelpers/LArMvaHelper.h
@@ -67,7 +67,7 @@ class LArMvaHelper
 public:
     typedef MvaTypes::MvaFeature MvaFeature;
     typedef MvaTypes::MvaFeatureVector MvaFeatureVector;
-    typedef std::map<std::string, double> MvaFeatureMapDbl; // an MvaFeature map that has double as the value and not an MvaFeature specifically (used currently in NeutrinoID tool)
+    typedef std::map<std::string, double> DoubleMap;
 
     typedef pandora::StringVector StringVector;
     typedef MvaTypes::MvaFeatureMap MvaFeatureMap;
