07e5b259cd434645dbc6608544201010c8b06eef
Isobel Mawby <imawby@dunebuild02.fnal.gov>
2023-04-25 04:50:35 -0500

    removing the cheating algorithms

--- larpandoracontent/LArCheating/CheatingVertexCreationAlgorithm.cc
+++ larpandoracontent/LArCheating/CheatingVertexCreationAlgorithm.cc
@@ -29,16 +29,6 @@ StatusCode CheatingVertexCreationAlgorithm::Run()
     const MCParticleList *pMCParticleList(nullptr);
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::GetCurrentList(*this, pMCParticleList));
 
-    bool cheatVertex(false);
-    for (const int nuPdg : m_nuToCheatList)
-    {
-        if (LArMCParticleHelper::IsCCNuEvent(pMCParticleList, nuPdg))
-            cheatVertex = true;
-    }
-
-    if (!cheatVertex && !m_nuToCheatList.empty())
-        return STATUS_CODE_SUCCESS;
-
     const VertexList *pVertexList(nullptr);
     std::string temporaryListName;
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::CreateTemporaryListAndSetCurrent(*this, pVertexList, temporaryListName));
@@ -53,9 +43,6 @@ StatusCode CheatingVertexCreationAlgorithm::Run()
             pMCParticle->GetEndpoint().GetX() + m_vertexXCorrection, pMCParticle->GetEndpoint().GetY(), pMCParticle->GetEndpoint().GetZ());
         parameters.m_vertexLabel = VERTEX_INTERACTION;
         parameters.m_vertexType = VERTEX_3D;
-
-        CartesianVector position( pMCParticle->GetEndpoint().GetX() + m_vertexXCorrection, pMCParticle->GetEndpoint().GetY(), pMCParticle->GetEndpoint().GetZ());
-        CartesianVector origin(0,0,0);
 
         const Vertex *pVertex(nullptr);
         PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, PandoraContentApi::Vertex::Create(*this, parameters, pVertex));
@@ -77,9 +64,6 @@ StatusCode CheatingVertexCreationAlgorithm::Run()
 StatusCode CheatingVertexCreationAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
 {
     PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "OutputVertexListName", m_outputVertexListName));
-
-    PANDORA_RETURN_RESULT_IF_AND_IF(
-        STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadVectorOfValues(xmlHandle, "NuToCheatList", m_nuToCheatList));
 
     PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=,
         XmlHelper::ReadValue(xmlHandle, "ReplaceCurrentVertexList", m_replaceCurrentVertexList));
--- larpandoracontent/LArCheating/CheatingVertexCreationAlgorithm.h
+++ larpandoracontent/LArCheating/CheatingVertexCreationAlgorithm.h
@@ -29,7 +29,6 @@ private:
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
     std::string m_outputVertexListName; ///< The name under which to save the output vertex list
-    pandora::IntVector m_nuToCheatList; ///< list of particle ids of MCPFOs to be selected
     bool m_replaceCurrentVertexList;    ///< Whether to replace the current vertex list with the output list
     float m_vertexXCorrection;          ///< The vertex x correction, added to reported mc neutrino endpoint x value, in cm
 };
