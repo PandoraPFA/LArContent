7a45e50265ea4d6bb911b6126b8fd2c663ff41ea
Andrew Chappell <A.Chappell@warwick.ac.uk>
2024-03-13 16:13:10 +0000

    Very provisional test of alternative graph building using Eigen

--- larpandoradlcontent/LArTwoDReco/DlClusterAlgorithm.cc
+++ larpandoradlcontent/LArTwoDReco/DlClusterAlgorithm.cc
@@ -16,6 +16,8 @@
 #include "larpandoracontent/LArObjects/LArCaloHit.h"
 #include "larpandoracontent/LArUtility/KDTreeLinkerAlgoT.h"
 
+#include <Eigen/Dense>
+
 #include <random>
 
 using namespace pandora;
@@ -34,6 +36,56 @@ StatusCode DlClusterAlgorithm::Run()
         if (pCaloHitList->empty())
             continue;
         std::cout << "Num hits: " << pCaloHitList->size() << std::endl;
+        Eigen::MatrixXf hits(pCaloHitList->size(), 2);
+        int i{0};
+        for (const CaloHit *const pCaloHit : *pCaloHitList)
+        {
+            const CartesianVector &pos{pCaloHit->GetPositionVector()};
+            hits(i, 0) = pos.GetX();
+            hits(i, 1) = pos.GetZ();
+            ++i;
+        }
+        // Edges can be double-counted, so use map of maps to avoid this
+        std::map<const CaloHit *, std::map<const CaloHit *, bool>> edges;
+        for (int r = 0; r < hits.rows(); ++r)
+        {
+            Eigen::RowVectorXf row(2);
+            row << hits(r,0), hits(r,1);
+            Eigen::MatrixXf norms((hits.rowwise() - row).array().pow(2).rowwise().sum());
+            norms(r, 0) = std::numeric_limits<float>::max();
+            Eigen::Index index1, index2;
+            norms.col(0).minCoeff(&index1);
+            auto iter0{pCaloHitList->begin()};
+            std::advance(iter0, r);
+            auto iter1{pCaloHitList->begin()};
+            std::advance(iter1, index1);
+            edges[*iter0][*iter1] = true;
+            edges[*iter1][*iter0] = true;
+            norms(index1, 0) = std::numeric_limits<float>::max();
+            auto val2{norms.col(0).minCoeff(&index2)};
+            auto val3{(hits.row(index1) - hits.row(index2)).array().pow(2).rowwise().sum()};
+            if (val2 < val3(0))
+            {
+                auto iter2{pCaloHitList->begin()};
+                std::advance(iter2, index2);
+                edges[*iter0][*iter2] = true;
+                edges[*iter2][*iter0] = true;
+            }
+        }
+
+        for (const auto &[pCaloHit1, map] : edges)
+        {
+            const CartesianVector &pos1{pCaloHit1->GetPositionVector()};
+            for (const auto &[pCaloHit2, dummy] : map)
+            {
+                const CartesianVector &pos2{pCaloHit2->GetPositionVector()};
+                CartesianVector start(pos1.GetX(), 0, pos1.GetZ());
+                CartesianVector end(pos2.GetX(), 0, pos2.GetZ());
+                PANDORA_MONITORING_API(AddLineToVisualization(this->GetPandora(), &start, &end, "e", BLUE, 2, 1));
+            }
+        }
+        PANDORA_MONITORING_API(ViewEvent(this->GetPandora()));
+    }
 
     return STATUS_CODE_SUCCESS;
 }
