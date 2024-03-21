e9065b78a6bceff3ecb281e822f7ff8bc13befb1
Matthew Osbiston <phuvhw@stelvio.epp.warwick.ac.uk>
2024-03-21 16:08:21 +0000

    modifications to DlSignalAlg for SCaloHitListX creation

--- larpandoradlcontent/LArSignalId/DlSignalAlgorithm.h
+++ larpandoradlcontent/LArSignalId/DlSignalAlgorithm.h
@@ -39,7 +39,7 @@ public:
 private:
 
     typedef std::pair<int, int> Pixel; // A Pixel is a row, column pair
-    typedef std::vector<Pixel> PixelVector;
+    typedef std::map<const pandora::CaloHit*, Pixel> PixelMap; //A mapping between a CaloHit and a Pixel in the canvas
 
     pandora::StatusCode Run();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
@@ -61,43 +61,7 @@ private:
      *  @return The StatusCode resulting from the function
      **/
     pandora::StatusCode MakeNetworkInputFromHits(const pandora::CaloHitList &caloHits, const pandora::HitType view, const float xMin,
-        const float xMax, const float zMin, const float zMax, LArDLHelper::TorchInput &networkInput, PixelVector &pixelVector) const;
-
-    /*
-     *  @brief  Create a list of wire plane-space coordinates from a canvas
-     *
-     *  @param  canvas The input canvas
-     *  @param  canvasWidth The width of the canvas
-     *  @param  canvasHeight The height of the canvas
-     *  @param  columnOffset The column offset used when populating the canvas
-     *  @param  rowOffset The row offset used when populating the canvas
-     *  @param  xMin The minimum x coordinate for the hits
-     *  @param  xMax The maximum x coordinate for the hits
-     *  @param  zMin The minimum x coordinate for the hits
-     *  @param  zMax The maximum x coordinate for the hits
-     *  @param  positionVector The output vector of wire plane positions
-     *
-     *  @return The StatusCode resulting from the function
-     **/
-    pandora::StatusCode MakeWirePlaneCoordinatesFromCanvas(float **canvas, const int canvasWidth, const int canvasHeight,
-        const int columnOffset, const int rowOffset, const pandora::HitType view, const float xMin, const float xMax, const float zMin,
-        const float zMax, pandora::CartesianPointVector &positionVector) const;
-
-    /**
-     *  @brief  Determines the parameters of the canvas for extracting the vertex location.
-     *          The network predicts the distance that each pixel associated with a hit is located from the vertex, but says nothing about
-     *          the direction. As a result, the ring describing the potential vertices associated with that hit can extend beyond the
-     *          original canvas size. This function returns the size of the required canvas and the offset for the bottom left corner.
-     *
-     *  @param  networkOutput The TorchOutput object populated by the network inference step
-     *  @param  pixelVector The vector of populated pixels
-     *  @param  columnOffset The output column offset for the canvas
-     *  @param  rowOffset The output row offset for the canvas
-     *  @param  width The output width for the canvas
-     *  @param  height The output height for the canvas
-     */
-    void GetCanvasParameters(const LArDLHelper::TorchOutput &networkOutput, const PixelVector &pixelVector, int &columnOffset,
-        int &rowOffset, int &width, int &height) const;
+        const float xMax, const float zMin, const float zMax, LArDLHelper::TorchInput &networkInput, PixelMap &pixelMap) const;
 
     /*
      *  @brief  Retrieve the map from MC to calo hits for reconstructable particles
@@ -156,7 +120,7 @@ private:
     std::string m_signalListNameU;            ///< Output signal CaloHitListU name
     std::string m_signalListNameV;            ///< Output signal CaloHitListV name
     std::string m_signalListNameW;            ///< Output signal CaloHitListW name
-
+    std::string m_signalListName2D;            ///< Output signal CaloHitList2D name
 };
 
 } // namespace lar_dl_content
