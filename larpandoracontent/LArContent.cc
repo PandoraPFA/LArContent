895e1f43525d010cba8449b0b2cfb9dd185c53f0
Matthew Osbiston <phuvhw@stelvio.epp.warwick.ac.uk>
2024-08-06 20:08:39 +0100

    removed LowEClusterAlg (obsolete alg) from LArContent as still existed locally

--- larpandoracontent/LArContent.cc
+++ larpandoracontent/LArContent.cc
@@ -176,7 +176,6 @@
 #include "larpandoracontent/LArTwoDReco/LArClusterAssociation/HitWidthClusterMergingAlgorithm.h"
 #include "larpandoracontent/LArTwoDReco/LArClusterAssociation/LongitudinalAssociationAlgorithm.h"
 #include "larpandoracontent/LArTwoDReco/LArClusterAssociation/LongitudinalExtensionAlgorithm.h"
-#include "larpandoracontent/LArTwoDReco/LArClusterAssociation/LowEClusterMergingAlgorithm.h"
 #include "larpandoracontent/LArTwoDReco/LArClusterAssociation/MvaLowEClusterMergingAlgorithm.h"
 #include "larpandoracontent/LArTwoDReco/LArClusterAssociation/SimpleClusterGrowingAlgorithm.h"
 #include "larpandoracontent/LArTwoDReco/LArClusterAssociation/SimpleClusterMergingAlgorithm.h"
@@ -301,7 +300,6 @@
     d("LArHitWidthClusterMerging",              HitWidthClusterMergingAlgorithm)                                                \
     d("LArLongitudinalAssociation",             LongitudinalAssociationAlgorithm)                                               \
     d("LArLongitudinalExtension",               LongitudinalExtensionAlgorithm)                                                 \
-    d("LArLowEClusterMerging",                  LowEClusterMergingAlgorithm)                                                    \
     d("LArBdtLowEClusterMerging",               BdtLowEClusterMergingAlgorithm)                                                 \
     d("LArSimpleClusterGrowing",                SimpleClusterGrowingAlgorithm)                                                  \
     d("LArSimpleClusterMerging",                SimpleClusterMergingAlgorithm)                                                  \
