16660f0ede12dbd654e5658e21f1cdf5ae7c40c1
Isobel Mawby <imawby@dunegpvm16.fnal.gov>
2024-12-04 12:12:00 -0600

    Implementing hierarchy building mechanics, and upstream/downstream vertex/directions for tracks

--- larpandoradlcontent/LArThreeDReco/LArEventBuilding/MLPNeutrinoHierarchyAlgorithm.h
+++ larpandoradlcontent/LArThreeDReco/LArEventBuilding/MLPNeutrinoHierarchyAlgorithm.h
@@ -10,6 +10,11 @@
 
 #include "Pandora/Algorithm.h"
 
+#include "larpandoracontent/LArHelpers/LArPfoHelper.h"
+#include "larpandoradlcontent/LArHelpers/LArDLHelper.h"
+
+using namespace lar_content;
+
 namespace lar_dl_content
 {
 
@@ -38,16 +43,30 @@ private:
      */
     HierarchyPfo();
 
-    HierarchyPfo(const pandora::ParticleFlowObject *pPfo);
+    HierarchyPfo(const pandora::ParticleFlowObject *pPfo, const pandora::CartesianVector &upstreamVertex,
+        const pandora::CartesianVector &upstreamDirection, const pandora::CartesianVector &downstreamVertex, 
+        const pandora::CartesianVector &downstreamDirection);
 
     bool operator== (const HierarchyPfo &otherHierarchyPfo) const;
 
     const pandora::ParticleFlowObject* GetPfo() const;
     void SetPfo(const pandora::ParticleFlowObject *pPfo);
-    const pandora::ParticleFlowObject* GetParentPfo() const;
-    void SetParentPfo(const pandora::ParticleFlowObject *pParentPfo);
     const pandora::ParticleFlowObject* GetPredictedParentPfo() const;
     void SetPredictedParentPfo(const pandora::ParticleFlowObject *pPredictedParentPfo);
+    const pandora::ParticleFlowObject* GetParentPfo() const;
+    void SetParentPfo(const pandora::ParticleFlowObject *pParentPfo);
+    const pandora::PfoVector& GetSortedChildPfoVector();
+    void AddChildPfo(const pandora::ParticleFlowObject *const pChildPfo);
+
+    const pandora::CartesianVector& GetUpstreamVertex() const;
+    void SetUpstreamVertex(const pandora::CartesianVector &upstreamVertex);
+    const pandora::CartesianVector& GetUpstreamDirection() const;
+    void SetUpstreamDirection(const pandora::CartesianVector &upstreamDirection);
+    const pandora::CartesianVector& GetDownstreamVertex() const;
+    void SetDownstreamVertex(const pandora::CartesianVector &downstreamVertex);
+    const pandora::CartesianVector& GetDownstreamDirection() const;
+    void SetDownstreamDirection(const pandora::CartesianVector &downstreamDirection);
+
     float GetPrimaryScore() const;
     void SetPrimaryScore(const float primaryScore);
     float GetLaterTierScore() const;
@@ -56,24 +75,40 @@ private:
     void SetParentOrientation(const int parentOrientation);
     int GetChildOrientation() const;
     void SetChildOrientation(const int childOrientation);
-    bool GetIsSet() const;
-    void SetIsSet(const bool isSet);
+    bool GetIsInHierarchy() const;
+    void SetIsInHierarchy(const bool isInHierarchy);
 
     private:
 
     const pandora::ParticleFlowObject *m_pPfo;
-    const pandora::ParticleFlowObject *m_pParentPfo;
     const pandora::ParticleFlowObject *m_pPredictedParentPfo;
+    const pandora::ParticleFlowObject *m_pParentPfo;
+    pandora::PfoVector m_childPfoVector;
+
+    pandora::CartesianVector m_upstreamVertex; // extremal point closest to vertex
+    pandora::CartesianVector m_upstreamDirection; // direction at extremal point closest to vertex
+    pandora::CartesianVector m_downstreamVertex; // extremal point furthest from vertex
+    pandora::CartesianVector m_downstreamDirection; // direction at extremal point furthest from vertex
+
     float m_primaryScore;
     float m_laterTierScore;
     int m_parentOrientation; // 0 - start is not closest to neutrino vertex
     int m_childOrientation; // 0 - start is not closest to neutrino vertex
-    bool m_isSet;
+    bool m_isInHierarchy;
     };
 
     pandora::StatusCode Run();
 
+    void DetermineIsobelID();
+
+    void PrintHierarchy();
+
+    bool GetNeutrinoPfo();
+
     void FillTrackShowerVectors();
+
+    bool GetExtremalVerticesAndDirections(const pandora::ParticleFlowObject *const pPfo, pandora::CartesianVector &upstreamVertex, 
+        pandora::CartesianVector &upstreamDirection, pandora::CartesianVector &downstreamVertex, pandora::CartesianVector &downstreamDirection);
 
     void SetPrimaryScores();
 
@@ -92,22 +127,42 @@ private:
     float GetLaterTierScoreTrackToShower(const HierarchyPfo &parentPfo, const HierarchyPfo &childPfo,
         int &parentOrientation, int &childOrientation) const;
 
+    void BuildPandoraHierarchy();
+
+    void PrintPandoraHierarchy();
+
+    void CheckForOrphans();
+
     float GetRandomNumber() const;
 
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
     //typedef std::vector<PfoRelationTool *> PfoRelationToolVector;
 
+    std::string m_neutrinoPfoListName;
     pandora::StringVector m_pfoListNames;
+
+    std::string m_primaryTrackBranchModelName;
+    std::string m_primaryTrackClassifierModelName;
+    std::string m_primaryShowerClassifierModelName;
+
+    LArDLHelper::TorchModel m_primaryTrackBranchModel;
+    LArDLHelper::TorchModel m_primaryTrackClassifierModel;
+    LArDLHelper::TorchModel m_primaryShowerClassifierModel;
 
     float m_primaryThresholdTrackPass1;
     float m_primaryThresholdShowerPass1;
     float m_laterTierThresholdTrackPass1;
     float m_laterTierThresholdShowerPass1;
 
+    ///////////////////////////
+    std::map<const pandora::ParticleFlowObject*, int> m_isobelID;
+    ///////////////////////////
+
+    const pandora::ParticleFlowObject* m_pNeutrinoPfo;
     std::map<const pandora::ParticleFlowObject*, HierarchyPfo> m_trackPfos;
     std::map<const pandora::ParticleFlowObject*, HierarchyPfo> m_showerPfos;
-    std::vector<std::vector<const pandora::ParticleFlowObject*>> m_hierarchy;
+    std::vector<pandora::PfoVector> m_hierarchy;
 };
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -126,6 +181,20 @@ inline void MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::SetPfo(const pandora::P
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
+inline const pandora::ParticleFlowObject* MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::GetPredictedParentPfo() const
+{
+    return m_pPredictedParentPfo;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline void MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::SetPredictedParentPfo(const pandora::ParticleFlowObject* pPredictedParentPfo)
+{
+    m_pPredictedParentPfo = pPredictedParentPfo;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
 inline const pandora::ParticleFlowObject* MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::GetParentPfo() const
 {
     return m_pParentPfo;
@@ -140,16 +209,74 @@ inline void MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::SetParentPfo(const pand
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-inline const pandora::ParticleFlowObject* MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::GetPredictedParentPfo() const
+inline const pandora::PfoVector& MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::GetSortedChildPfoVector()
 {
-    return m_pPredictedParentPfo;
+    std::sort(m_childPfoVector.begin(), m_childPfoVector.end(), LArPfoHelper::SortByNHits);
+
+    return m_childPfoVector;
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-inline void MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::SetPredictedParentPfo(const pandora::ParticleFlowObject* pPredictedParentPfo)
+inline void MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::AddChildPfo(const pandora::ParticleFlowObject *const pChildPfo)
 {
-    m_pPredictedParentPfo = pPredictedParentPfo;
+    m_childPfoVector.push_back(pChildPfo);
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline const pandora::CartesianVector& MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::GetUpstreamVertex() const
+{
+    return m_upstreamVertex;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline void MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::SetUpstreamVertex(const pandora::CartesianVector &upstreamVertex)
+{
+    m_upstreamVertex = upstreamVertex;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline const pandora::CartesianVector& MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::GetUpstreamDirection() const
+{
+    return m_upstreamDirection;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline void MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::SetUpstreamDirection(const pandora::CartesianVector &upstreamDirection)
+{
+    m_upstreamDirection = upstreamDirection;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline const pandora::CartesianVector& MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::GetDownstreamVertex() const
+{
+    return m_downstreamVertex;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline void MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::SetDownstreamVertex(const pandora::CartesianVector &downstreamVertex)
+{
+    m_downstreamVertex = downstreamVertex;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline const pandora::CartesianVector& MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::GetDownstreamDirection() const
+{
+    return m_downstreamDirection;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+inline void MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::SetDownstreamDirection(const pandora::CartesianVector &downstreamDirection)
+{
+    m_downstreamDirection = downstreamDirection;
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
@@ -210,16 +337,16 @@ inline void MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::SetChildOrientation(con
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-inline bool MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::GetIsSet() const
+inline bool MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::GetIsInHierarchy() const
 {
-    return m_isSet;
+    return m_isInHierarchy;
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-inline void MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::SetIsSet(const bool isSet)
+inline void MLPNeutrinoHierarchyAlgorithm::HierarchyPfo::SetIsInHierarchy(const bool isInHierarchy)
 {
-    m_isSet = isSet;
+    m_isInHierarchy = isInHierarchy;
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
