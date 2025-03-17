d801875636cd862c1b203d29fd8266da87517547
Andrew Chappell <A.Chappell@warwick.ac.uk>
2025-03-17 11:47:41 +0000

    Add kalman filter utility class

---
+++ larpandoracontent/LArUtility/KalmanFilter.cc
@@ -0,0 +1,149 @@
+/**
+ *  @file   larpandoracontent/LArUtility/KalmanFilter.cc
+ *
+ *  @brief  Kalman filter for predicting 3D trajectories.
+ *
+ *  $Log: $
+ */
+
+#include "larpandoracontent/LArUtility/KalmanFilter.h"
+
+using namespace Eigen;
+
+namespace lar_content
+{
+
+KalmanFilter3D::KalmanFilter3D(double dt, double processVariance, double measurementVariance, const VectorXd &x) :
+    m_dt{dt}
+{
+    m_x = VectorXd::Zero(6);
+    m_x.head(3) = x;
+    m_P = MatrixXd::Identity(6, 6);
+    m_F = MatrixXd::Identity(6, 6);
+    for (int i = 0; i < 3; ++i)
+    {
+        m_F(i, i + 3) = dt;
+    }
+    m_H = MatrixXd::Zero(3, 6);
+    m_H.block<3,3>(0,0) = MatrixXd::Identity(3,3);
+    m_Q = MatrixXd::Zero(6, 6);
+    for (int i = 0; i < 3; ++i)
+    {
+        m_Q(i, i) = (0.25 * dt * dt * dt * dt) * processVariance;
+        m_Q(i, i + 3) = (0.5 * dt * dt * dt) * processVariance;
+        m_Q(i + 3, i) = (0.5 * dt * dt * dt) * processVariance;
+        m_Q(i + 3, i + 3) = (dt * dt) * processVariance;
+    }
+    m_R = MatrixXd::Identity(3, 3) * measurementVariance;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+KalmanFilter3D::~KalmanFilter3D()
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void KalmanFilter3D::Predict()
+{
+    m_xTemp = m_F * m_x;
+    m_PTemp = m_F * m_P * m_F.transpose() + m_Q;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void KalmanFilter3D::Update(const VectorXd &z)
+{
+    m_x = m_xTemp;
+    m_P = m_PTemp;
+    VectorXd y = z - m_H * m_x;
+    MatrixXd S = m_H * m_P * m_H.transpose() + m_R;
+    MatrixXd K = m_P * m_H.transpose() * S.inverse();
+    m_x += K * y;
+    m_P = (MatrixXd::Identity(6, 6) - K * m_H) * m_P;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+const VectorXd &KalmanFilter3D::GetState() const
+{
+    return m_x;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+const VectorXd &KalmanFilter3D::GetTemporaryState() const
+{
+    return m_xTemp;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+KalmanFilter2D::KalmanFilter2D(double dt, double processVariance, double measurementVariance, const VectorXd &x) :
+    m_dt{dt}
+{
+    m_x = VectorXd::Zero(4);
+    m_x.head(2) = x;
+    m_P = MatrixXd::Identity(4, 4);
+    m_F = MatrixXd::Identity(4, 4);
+    for (int i = 0; i < 2; ++i)
+    {
+        m_F(i, i + 2) = dt;
+    }
+    m_H = MatrixXd::Zero(2, 4);
+    m_H.block<2,2>(0,0) = MatrixXd::Identity(3,3);
+    m_Q = MatrixXd::Zero(4, 4);
+    for (int i = 0; i < 2; ++i)
+    {
+        m_Q(i, i) = (0.25 * dt * dt * dt * dt) * processVariance;
+        m_Q(i, i + 2) = (0.5 * dt * dt * dt) * processVariance;
+        m_Q(i + 2, i) = (0.5 * dt * dt * dt) * processVariance;
+        m_Q(i + 2, i + 2) = (dt * dt) * processVariance;
+    }
+    m_R = MatrixXd::Identity(2, 2) * measurementVariance;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+KalmanFilter2D::~KalmanFilter2D()
+{
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void KalmanFilter2D::Predict()
+{
+    m_xTemp = m_F * m_x;
+    m_PTemp = m_F * m_P * m_F.transpose() + m_Q;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void KalmanFilter2D::Update(const VectorXd &z)
+{
+    m_x = m_xTemp;
+    m_P = m_PTemp;
+    VectorXd y{z - m_H * m_x};
+    MatrixXd S{m_H * m_P * m_H.transpose() + m_R};
+    MatrixXd K{m_P * m_H.transpose() * S.inverse()};
+    m_x += K * y;
+    m_P = (MatrixXd::Identity(4, 4) - K * m_H) * m_P;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+const VectorXd &KalmanFilter2D::GetState() const
+{
+    return m_x;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+const VectorXd &KalmanFilter2D::GetTemporaryState() const
+{
+    return m_xTemp;
+}
+
+} // namespace lar_content
---
+++ larpandoracontent/LArUtility/KalmanFilter.h
@@ -0,0 +1,137 @@
+/**
+ *  @file   larpandoracontent/LArUtility/KalmanFilter.h
+ *
+ *  @brief  Kalman filter for predicting 3D trajectories.
+ *
+ *  $Log: $
+ */
+#ifndef LAR_KALMAN_FILTER_H
+#define LAR_KALMAN_FILTER_H 1
+
+#include "Eigen/Dense"
+
+namespace lar_content
+{
+
+/**
+ *  @brief  KalmanFilter3D class
+ */
+class KalmanFilter3D
+{
+public:
+    /**
+     *  @brief  Default constructor
+     *
+     *  @param  dt Time step
+     *  @param  processVariance Process variance
+     *  @param  measurementVariance Measurement variance
+     *  @param  x Initial position
+     */
+    KalmanFilter3D(double dt, double processVariance, double measurementVariance, const Eigen::VectorXd &x);
+
+    /**
+     *  @brief  Destructor
+     */
+    ~KalmanFilter3D();
+
+    /**
+     * @brief  Uses the state transition matrix to predict the next state
+     */
+    void Predict();
+
+    /**
+     * @brief  Uses the measurement matrix to update the state estimate
+     *
+     * @param  z The position to use for updating the state estimate
+     */
+    void Update(const Eigen::VectorXd &z);
+
+    /**
+     * @brief  Get the current state vector
+     *
+     * @return The current state vector
+     */
+    const Eigen::VectorXd &GetState() const;
+
+    /**
+     * @brief  Get the temporary state vector
+     *
+     * @return The temporary state vector
+     */
+    const Eigen::VectorXd &GetTemporaryState() const;
+
+private:
+    double m_dt; ///< Time step
+    Eigen::VectorXd m_x; ///< State vector (tracks position and 'velocity')
+    Eigen::MatrixXd m_P; ///< Covariance matrix
+    Eigen::MatrixXd m_F; ///< State transition matrix
+    Eigen::MatrixXd m_H; ///< Measurement matrix
+    Eigen::MatrixXd m_R; ///< Measurement covariance matrix
+    Eigen::MatrixXd m_Q; ///< Process covariance matrix
+    Eigen::VectorXd m_xTemp; ///< Temporary state vector
+    Eigen::MatrixXd m_PTemp; ///< Temporary covariance matrix
+};
+
+/**
+ *  @brief  KalmanFilter2D class
+ */
+class KalmanFilter2D
+{
+public:
+    /**
+     *  @brief  Default constructor
+     *
+     *  @param  dt Time step
+     *  @param  processVariance Process variance
+     *  @param  measurementVariance Measurement variance
+     *  @param  x Initial position
+     */
+    KalmanFilter2D(double dt, double processVariance, double measurementVariance, const Eigen::VectorXd &x);
+
+    /**
+     *  @brief  Destructor
+     */
+    ~KalmanFilter2D();
+
+    /**
+     * @brief  Uses the state transition matrix to predict the next state
+     */
+    void Predict();
+
+    /**
+     * @brief  Uses the measurement matrix to update the state estimate
+     *
+     * @param  z The position to use for updating the state estimate
+     */
+    void Update(const Eigen::VectorXd &z);
+
+    /**
+     * @brief  Get the current state vector
+     *
+     * @return The current state vector
+     */
+    const Eigen::VectorXd &GetState() const;
+
+    /**
+     * @brief  Get the temporary state vector
+     *
+     * @return The temporary state vector
+     */
+    const Eigen::VectorXd &GetTemporaryState() const;
+
+private:
+    double m_dt; ///< Time step
+    Eigen::VectorXd m_x; ///< State vector (tracks position and 'velocity')
+    Eigen::MatrixXd m_P; ///< Covariance matrix
+    Eigen::MatrixXd m_F; ///< State transition matrix
+    Eigen::MatrixXd m_H; ///< Measurement matrix
+    Eigen::MatrixXd m_R; ///< Measurement covariance matrix
+    Eigen::MatrixXd m_Q; ///< Process covariance matrix
+    Eigen::VectorXd m_xTemp; ///< Temporary state vector
+    Eigen::MatrixXd m_PTemp; ///< Temporary covariance matrix
+};
+
+
+} // namespace lar_content
+
+#endif // #ifndef LAR_KALMAN_FILTER_H
