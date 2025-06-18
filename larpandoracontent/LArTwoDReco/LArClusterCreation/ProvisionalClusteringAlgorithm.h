d9bb1694ceeb690311f08e28d95532670c1be62b
Andrew Chappell <A.Chappell@warwick.ac.uk>
2025-06-18 12:21:54 +0100

    Replace magic numbers with configurable parameters

--- larpandoracontent/LArTwoDReco/LArClusterCreation/ProvisionalClusteringAlgorithm.h
+++ larpandoracontent/LArTwoDReco/LArClusterCreation/ProvisionalClusteringAlgorithm.h
@@ -42,6 +42,14 @@ private:
     float m_maxGap; ///< Maximum calo hit separation (factors in width or height)
     float m_maxGap2dSquared; ///< Square of maximum calo hit separation (factors in width and height)
     ApaHitMap m_apaHitMap; ///< Map to partition hits into APA
+
+    float m_closeApproachThreshold{3.f}; ///< The threshold for a hit being deemded "close" to a cluster
+    float m_processVarianceCoeff{0.0625f}; ///< The process variance coefficient for the Kalman filter
+    float m_measurementVarianceCoeff{0.25f}; ///< The measurement variance coefficient for the Kalman filter
+    float m_proximityCoeff{1.07f}; ///< The proximity coefficient for proximity thresholds
+    float m_mahalanobisCoeff{1.1f}; ///< The Mahalanobis distance coefficient for proximity thresholds
+    float m_mahalanobisRescaling{0.5f}; ///< The rescaling factor for the Mahalanobis distance when comparing to proximity
+    float m_boundaryProximityCoeff{0.1f}; ///< The boundary proximity coefficient for proximity thresholds
 };
 
 } // namespace lar_content
