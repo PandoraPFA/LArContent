a11a9d486f6d78977018892360c893df45b042ae
Yoshita Dabburi <y.dabburi@qmul.ac.uk>
2026-01-05 20:19:48 -0600

    Addressed PR Comments

--- larpandoracontent/LArMonitoring/EventClusterValidationAlgorithm.h
+++ larpandoracontent/LArMonitoring/EventClusterValidationAlgorithm.h
@@ -1,9 +1,8 @@
 /**
  *  @file   larpandoracontent/LArMonitoring/EventClusterValidationAlgorithm.h
  *
- *  @brief  Header file of the event-level cluster validation.
- *
- *  $Log: $
+ *  @brief  Header file of the event-level cluster validation. Calculate metrics that aim to quantify the quality of 2D clusters at
+ *          the level of a single view.
  */
 #ifndef LAR_EVENT_CLUSTER_VALIDATION_ALGORITHM_H
 #define LAR_EVENT_CLUSTER_VALIDATION_ALGORITHM_H 1
@@ -23,19 +22,41 @@ private:
     {
         ClusterMetrics();
 
-        std::vector<int> m_nRecoHits;
-        std::vector<float> m_purities;
-        std::vector<float> m_completenesses;
+        double m_purity;
+        double m_showerPurity;
+        double m_trackPurity;
+        double m_completeness;
+        double m_showerCompleteness;
+        double m_trackCompleteness;
+        double m_ari;
+        double m_showerAri;
+        double m_trackAri;
         int m_nHits;
-        int m_nClusters;
-        int m_nMainMCs;
+        int m_nHitsNullCluster;
+        int m_nShowerTrueHits;
+        int m_nTrackTrueHits;
+        int m_nRecoClusters;
+        int m_nShowerRecoClusters;
+        int m_nTrackRecoClusters;
+        int m_nTrueClusters;
+        int m_nShowerTrueClusters;
+        int m_nTrackTrueClusters;
+        int m_nAriRecoClusters;
+        int m_nShowerAriRecoClusters;
+        int m_nTrackAriRecoClusters;
     };
 
-    enum ValidationType
+    struct MatchedParticleMetrics
     {
-        ALL,
-        SHOWER,
-        TRACK
+        MatchedParticleMetrics();
+
+        std::vector<int> m_pdg;
+        std::vector<int> m_causesShower;
+        std::vector<int> m_isPrimary;
+        std::vector<float> m_trueEnergy;
+        std::vector<int> m_nTrueHits;
+        std::vector<int> m_nMatchedCorrectHits;
+        std::vector<int> m_nMatchedTotalHits;
     };
 
 public:
@@ -45,6 +66,7 @@ public:
 
         const pandora::MCParticle *m_pMainMC;
         const pandora::Cluster *m_pCluster;
+        const pandora::MCParticle *m_pClusterMainMC;
     };
 
     /**
@@ -58,68 +80,159 @@ public:
     ~EventClusterValidationAlgorithm();
 
 private:
-    typedef std::map<const pandora::Cluster *const, std::map<const pandora::MCParticle *const, int>> ContingencyTable;
+    template <typename Ti, typename Tj>
+    using ContingencyTable = std::map<Ti, std::map<Tj, int>>;
 
     pandora::StatusCode Run();
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
     /**
-     *  @brief Retrieve the metrics of every cluster in a view
+     *  @brief Calculate purity and completeness in this view for all/track/shower reco and true clusters.
+     *         Completeness(purity) sum over true(reco) clusters the maximum intersection with any of the reco(true) clusters.
+     *         According to m_hitWeightedPurityCompleteness, this is either normalised by the number of hits in the true(reco) cluster and
+     *         averaged over the total true(reco) clusters, or not normalised and averaged over the total hits.
      *
-     *  @param[in]  hitParents Map of hits being considered to the Cluster/MCParticle they belong to
-     *  @param[out] metrics    Metrics for the clusters in this view
+     *  @param[in]  hitParents Map of hits being considered to the cluster/MC particle they belong to
+     *  @param[out] metrics    Output Metrics for the clustering in this view
      */
-    void GetMetrics(const std::map<const pandora::CaloHit *const, CaloHitParents> &hitParents, ClusterMetrics &metrics) const;
+    void GetClusterMetrics(const std::map<const pandora::CaloHit *const, CaloHitParents> &hitParents, ClusterMetrics &metrics) const;
 
     /**
-     *  @brief Calculate Rand Index for the clusters with the clusters of true MCParticles.
-     *          (Ref. for Adjusted Rand Index: https://link.springer.com/article/10.1007/BF01908075)
+     *  @brief Retrieve the metrics for every MC Particle in a view by matching clusters to a true MC particle
      *
-     *  @param[in] hitParents Map of hits being considered to the Cluster/MCParticle they belong to
+     *  @param[in]  hitParents Map of hits being considered to the cluster/MC particle they belong to
+     *  @param[out] metrics    Output metrics for the matched MC particles in this view
      */
-    float CalcRandIndex(std::map<const pandora::CaloHit *const, CaloHitParents> &hitParents) const;
+    void GetMatchedParticleMetrics(const std::map<const pandora::CaloHit *const, CaloHitParents> &hitParents, MatchedParticleMetrics &metrics) const;
 
     /**
-     *  @brief Find the cluster and MCParticle each hit belongs to
+     *  @brief Calculate Rand Index for the reco clusters with the true clusters for all/track/shower hits.
+     *         According to m_mergeShowerClustersForRandIndex, the shower clusters may be merged using cheating prior to the calculation.
+     *         (Ref. for Adjusted Rand Index: https://link.springer.com/article/10.1007/BF01908075).
+     *
+     *  @param[in]  hitParents Map of hits being considered to the cluster/MC particle they belong to
+     *  @param[out] metrics    Output metrics for the clustering in this view
+     */
+    void CalcRandIndex(std::map<const pandora::CaloHit *const, CaloHitParents> &hitParents, ClusterMetrics &metrics) const;
+
+    /**
+     *  @brief Find the cluster and MC particle each hit belongs to
      *
      *  @param[in]  caloHits   List of hits
      *  @param[in]  clusters   List of clusters
-     *  @param[out] hitParents Map of hits to the Cluster/MCParticle they belong to
+     *  @param[out] hitParents Map of hits to the Cluster/MC particle they belong to
      */
     void GetHitParents(const pandora::CaloHitList &caloHits, const pandora::ClusterList &clusters,
         std::map<const pandora::CaloHit *const, CaloHitParents> &hitParents) const;
 
     /**
-     *  @brief Erase hits associated to an MCParticle that does meet a minimum number of hits in the view
+     *  @brief Find the main MC particle for each cluster, this is the MC particle that contributes the most hits
      *
-     *  @param[in,out] hitParents Map of hits to the Cluster/MCParticle they belong to
+     *  @param[in,out] hitParents Map of hits to the cluster/MC particle they belong to
+     */
+    void GetClusterMainMC(std::map<const pandora::CaloHit *const, CaloHitParents> &hitParents) const;
+
+    /**
+     *  @brief Tries to identify and deal with impossible-to-cluster-correctly delta ray hits by assigning the hit to the
+     *         parent particle's cluster if some conditions are met.
+     *
+     *  @param[in] pCaloHit The hit
+     *  @param[in] pMC      The main MC particle of the hit
+     *
+     *  @return The MC particle the hit should be assigned to, this will either be the inputted MC particle or the parent MC particle
+     */
+    const pandora::MCParticle *FoldPotentialDeltaRayTo(const pandora::CaloHit *const pCaloHit, const pandora::MCParticle *const pMC) const;
+
+    /**
+     *  @brief Finds the ancestor that a child MC particle should be associated with to roll-up an EM shower
+     *
+     *  @param[in] pMC The MC particle
+     *
+     *  @return The ancestor MC particle, this will be the inputted MC particle for an MC particle that should not be rolled-up
+     */
+    const pandora::MCParticle *FoldMCTo(const pandora::MCParticle *const pMC) const;
+
+    /**
+     *  @brief Recursive function to check descendent particles for signature of a shower (e -> gamma -> e).
+     *         Used to identify delta-rays that shower and photons that only undergo compton scatters leaving diffuse hits.
+     *
+     *  @param[in] pMC                  The MC particle to check descendents of
+     *  @param[in] nDescendentElectrons The number of electrons seen while descending from the original photon MC particle
+     *
+     *  @return Flag to indicate if more than 1 electron was seen while descending any of the descendent particle association paths
+     */
+    bool CausesShower(const pandora::MCParticle *const pMC, int nDescendentElectrons) const;
+
+    /**
+     *  @brief Check if an MC particle is electron or photon,
+     *
+     *  @param[in] pMC he MC particle
+     *
+     *  @return Flag to indicate if the MC particle if EM
+     */
+    bool IsEM(const pandora::MCParticle *const pMC) const;
+
+    /**
+     *  @brief Draw the true clusters being compared to
+     *
+     *  @param[in] hitParents Map of hits to the cluster/MC particle they belong to
+     */
+    void VisualizeTargetClusters(std::map<const pandora::CaloHit *const, CaloHitParents> &hitParents) const;
+
+    /**
+     *  @brief Draw the reco clusters used in the Rand Index calculation. These will be different from the clusters inputted to the
+     *         algorithm if MergeShowerClustersForRandIndex is true.
+     *
+     *  @param[in] hitParents Map of hits to the cluster/MC particle they belong to
+     *  @param[in] hitParents Map of hits to the cluster they merge with for the Rand Index Calculation.
+     */
+    void VisualizeRandIndexRecoClusters(std::map<const pandora::CaloHit *const, CaloHitParents> &hitParents,
+        std::map<const pandora::CaloHit *const, const pandora::Cluster *const> &hitMergeTargets) const;
+
+    /**
+     *  @brief Erase hits associated with an MC particle that does meet a minimum number of hits in the view
+     *
+     *  @param[in,out] hitParents Map of hits to the cluster/MC particle they belong to
      */
     void ApplyMCParticleMinSumHits(std::map<const pandora::CaloHit *const, CaloHitParents> &hitParents) const;
 
     /**
-     *  @brief Erase hits not associated with an MCParticle PDG incompatible with track/shower/all
+     *  @brief Handles hits that do not have a reco cluster, this is usually happens when left behind by the 3D hit creation.
+     *         Will count the number of hits with no reco cluster and, according to m_dropNullClusterHits, may erase them.
      *
-     *  @param[in] hitParents Map of hits to the Cluster/MCParticle they belong to
-     *  @param[in] valType    Enum for track/shower/all
+     *  @param[in,out] hitParents Map of hits to the cluster/MC particle they belong to
      */
-    std::map<const pandora::CaloHit *const, CaloHitParents> ApplyPDGCut(
-        std::map<const pandora::CaloHit *const, CaloHitParents> &hitParents, const ValidationType &valType) const;
+    int HandleNullClusterHits(std::map<const pandora::CaloHit *const, CaloHitParents> &hitParents) const;
 
     /**
      *  @brief Update the branches of the TTree for this entry
      *
-     *  @param[in] metrics      Metrics for clusters in a view
-     *  @param[in] randIfx      Adjusted Rand index
-     *  @param[in] branchPrefix Prefix for the branch name
+     *  @param[in] clusterMetrics         Output metrics for clusters in the view
+     *  @param[in] matchedParticleMetrics Output metrics each particle matched to a cluster in the view
+     *  @param[in] view                   The view
      */
-    void SetBranches(ClusterMetrics &metrics, float randIdx, std::string branchPrefix) const;
+    void SetBranches(const ClusterMetrics &clusterMetrics, const MatchedParticleMetrics &matchedParticleMetrics, const int view) const;
 
-    int m_eventNumber;                           ///< To track the current event number
+    // members
+    int m_eventNumber;                                                  ///< To track the current event number
+    std::map<pandora::HitType, float> m_deltaRayLengthThresholdSquared; ///< Threshold for defining small delta rays that will be folded to the parent particle
+    float m_deltaRayParentWeightThreshold; ///< Threshold for weight contribution of parent particle for it take the delta ray's hit
+
+    // members that may be set from xml
     std::string m_fileName;                      ///< The filename of the ROOT output file
     std::string m_treeName;                      ///< The name of the ROOT tree
-    std::string m_caloHitListName;               ///< The name of the hit list containing all 2D hits
+    std::vector<std::string> m_caloHitListNames; ///< The names of the hit lists containing all 2D hits
     std::vector<std::string> m_clusterListNames; ///< The names of the lists of 2D clusters to process
-    int m_minMCHitsPerView; ///< Threshold on total main MCParticle hits in each view for consideration in metric calculations
+    int m_minMCHitsPerView; ///< Threshold on total main MC particle hits in each view for consideration in metric calculations
+    bool m_onlyRandIndex;   ///< Flag to only calculate the adjusted rand index over all particles
+    bool m_foldShowers;     ///< Flag to fold shower MC particles to their leading shower MC particle
+    bool m_handleDeltaRays; ///< Flag to fold short delta rays + ignore contributions from daughter electrons that overlap with their parent
+    bool m_mergeShowerClustersForRandIndex; ///< Flag to merge shower-matched clusters into leading shower MC particle for rand index calculation, note this is only makes any sense for showers folded in the simulation or with m_foldShowers
+    bool m_visualize;                       ///< Flag to display the target clustering derived from MC particles
+    bool m_matchedParticleMetrics;          ///< Flag to calculate a set of high level clustering metrics by truth-matching clusters
+    bool m_dropNullClusterHits;             ///< Flag to ignore any hits associated with the null cluster
+    bool m_hitWeightedPurityCompleteness;   ///< Flag for cluster purity and completeness to be averaged over hits rather than clusters
+    bool m_maximalMatching; ///< Flag for cluster truth-matching to try matching every MC particle to a cluster, rather than requiring clusters to a hit majority from the matched MC particle
 };
 
 } // namespace lar_content
--- larpandoracontent/LArThreeDReco/LArPfoMopUp/ShowerMergingPfoMopUpAlgorithm.cc
+++ larpandoracontent/LArThreeDReco/LArPfoMopUp/ShowerMergingPfoMopUpAlgorithm.cc
@@ -1,4 +1,13 @@
+/**
+*  @file   larpandoracontent/LArThreeDReco/LArPfoMopUp/ShowerMergingPfoMopUpAlgorithm.cc
+*
+*  @brief  Implementation of the shower merging pfo mop up algorithm class.
+*  
+*  $Log: $
+*/
+
 #include "Pandora/AlgorithmHeaders.h"
+
 #include "larpandoracontent/LArHelpers/LArPfoHelper.h"
 #include "larpandoracontent/LArHelpers/LArPointingClusterHelper.h"
 #include "larpandoracontent/LArHelpers/LArGeometryHelper.h"
@@ -6,91 +15,85 @@
 #include "larpandoracontent/LArObjects/LArPfoObjects.h"
 #include "larpandoracontent/LArHelpers/LArMCParticleHelper.h"
 
-#include "ShowerMergingPfoMopUpAlgorithm.h"
+#include "larpandoracontent/LArThreeDReco/LArPfoMopUp/ShowerMergingPfoMopUpAlgorithm.h"
 
 using namespace pandora;
 
 namespace lar_content
 {
 
+ShowerMergingPfoMopUpAlgorithm::ShowerMergingPfoMopUpAlgorithm() :
+    m_alignmentAngle(0.99),
+    m_maxVtxPfosSeparation(3.f),
+    m_stubShowerSeparation(2025)
+{
+}
+
+//---------------------------------------------------------------------------
 
 StatusCode ShowerMergingPfoMopUpAlgorithm::Run()
 {
-
     PfoVector sortedPfos;
     this->GetSortedPfos(sortedPfos);
-   
     std::set < const ParticleFlowObject * > deletedPfos;
 
-
-    int i = 0;
     for (const ParticleFlowObject *const pPfo1 : sortedPfos)
     {  
        if (deletedPfos.find (pPfo1) != deletedPfos.end())
            continue;
+        
+       if (!pPfo1)
+            continue;
 
-       int j =0;
-       i++;
- 
-
-        for (const ParticleFlowObject *const pPfo2 : sortedPfos)
+       for (const ParticleFlowObject *const pPfo2 : sortedPfos)
         {   
-
            if (deletedPfos.find (pPfo2) != deletedPfos.end())
                continue;
-
-            j++;
-            if (!pPfo1 || !pPfo2 || (pPfo1 == pPfo2))
+           
+           if (!pPfo2)
                 continue;
-            
+
+            if (pPfo1 == pPfo2)
+                continue;
+
             if (pPfo1->GetParentPfoList().empty() || pPfo2->GetParentPfoList().empty())
                 continue;
 
             const bool isAligned{this->AreDirectionsAligned(pPfo1, pPfo2)};
-            bool invert = false;
-            const bool isSameVertex{this->HaveSameVertex(pPfo1, pPfo2, invert)};
-           
+            if (!isAligned)
+                continue;
 
-            if (isAligned && isSameVertex)
-            {   
-                
+            const bool isVertexAssociated{this->IsPfoVertexAssociated(pPfo1, pPfo2)};
+            if (!isVertexAssociated)
+                continue;
 
-               if (!invert)
-               {
-                   for ( const Pfo* const daughter : pPfo2->GetDaughterPfoList() )
-                       if (daughter == pPfo1)
+             bool invert{false};
+             {
+                for (const Pfo* const daughter : pPfo2->GetDaughterPfoList())
+                     if (daughter == pPfo1)
+                     {    
                         invert=true;
+                        break;
+                     }
                }
                
-
                 if (invert)
-                { 
                     this->MergeAndDeletePfos(pPfo2, pPfo1);
-
-                }
-
                 else 
-                {
                     this->MergeAndDeletePfos(pPfo1, pPfo2);
-                }
-                    
-                    deletedPfos.insert(pPfo1);
-                    deletedPfos.insert(pPfo2);
-                    break;
-
+                
+                deletedPfos.insert(pPfo1);
+                deletedPfos.insert(pPfo2);
+                break;
             }
         }
-    }
-
     return STATUS_CODE_SUCCESS;
 }
 
-
-//------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
 
 void ShowerMergingPfoMopUpAlgorithm::GetSortedPfos(PfoVector &sortedPfos) const
 {
-    
     for (const std::string &listName : m_inputPfoListNames)
     {   
         const PfoList *pPfoList(nullptr);
@@ -99,57 +102,67 @@ void ShowerMergingPfoMopUpAlgorithm::GetSortedPfos(PfoVector &sortedPfos) const
             sortedPfos.insert(sortedPfos.end(), pPfoList->begin(), pPfoList->end());
     }
     std::sort(sortedPfos.begin(), sortedPfos.end(), LArPfoHelper::SortByNHits);
-
 }
 
 //------------------------------------------------------------------------------------------------------------
-
 
 bool ShowerMergingPfoMopUpAlgorithm::AreDirectionsAligned(const ParticleFlowObject *const pPfo1, const ParticleFlowObject *const pPfo2) const
 {
     if (pPfo1->GetVertexList().empty() || pPfo2->GetVertexList().empty())
         return false;
+    try 
+    {
+         const Vertex *const pVertex1 = LArPfoHelper::GetVertex(pPfo1);
+         const Vertex *const pVertex2 = LArPfoHelper::GetVertex(pPfo2);
 
-    const Vertex *const pVertex1 = LArPfoHelper::GetVertex(pPfo1);
-    const Vertex *const pVertex2 = LArPfoHelper::GetVertex(pPfo2);
+         if (!pVertex1 || !pVertex2)
+             return false;
 
-    if (!pVertex1 || !pVertex2)
-        return false;
+         const LArShowerPCA pca1 = LArPfoHelper::GetPrincipalComponents(pPfo1,pVertex1);
+         const LArShowerPCA pca2 = LArPfoHelper::GetPrincipalComponents(pPfo2,pVertex2);
 
-    const LArShowerPCA pca1 = LArPfoHelper::GetPrincipalComponents(pPfo1,pVertex1);
-    const LArShowerPCA pca2 = LArPfoHelper::GetPrincipalComponents(pPfo2,pVertex2);
-
-    const CartesianVector dir1 = pca1.GetPrimaryAxis();
-    const CartesianVector dir2 = pca2.GetPrimaryAxis();
-
-    const float cosAngle = dir1.GetUnitVector().GetDotProduct(dir2.GetUnitVector());
-    return (cosAngle > m_alignmentAngle);
-
+         const CartesianVector dir1 = pca1.GetPrimaryAxis();
+         const CartesianVector dir2 = pca2.GetPrimaryAxis();
+         const float cosAngle = dir1.GetUnitVector().GetDotProduct(dir2.GetUnitVector());
+         return (cosAngle > m_alignmentAngle);
+    }
+    catch (const StatusCodeException &)
+    {
+    return false;
+    }
 }
 
 //------------------------------------------------------------------------------------------------------------
 
-
-bool ShowerMergingPfoMopUpAlgorithm::HaveSameVertex(const ParticleFlowObject *const pPfo1, const ParticleFlowObject *const pPfo2, bool&invert) const
+bool ShowerMergingPfoMopUpAlgorithm::IsPfoVertexAssociated(const ParticleFlowObject *const pPfo1, const ParticleFlowObject *const pPfo2) const
 {
     if (pPfo1->GetVertexList().empty() || pPfo2->GetVertexList().empty())
-             return false;
+     return false;
+    
+    const Pfo *pNu(nullptr);
+    const Vertex *pNuVertex(nullptr);
+    const Vertex *pVertex1(nullptr);
+    const Vertex *pVertex2(nullptr);
 
-    const Pfo *const pNu = LArPfoHelper::GetParentNeutrino(pPfo1);
-    const Vertex *const pNuVertex = LArPfoHelper::GetVertex(pNu);
-
-    const Vertex *const pVertex1 = LArPfoHelper::GetVertex(pPfo1);
-    const Vertex *const pVertex2 = LArPfoHelper::GetVertex(pPfo2);
+    try 
+    {
+      pNu = LArPfoHelper::GetParentNeutrino(pPfo1);
+      pNuVertex = LArPfoHelper::GetVertex(pNu);
+      pVertex1 = LArPfoHelper::GetVertex(pPfo1);
+      pVertex2 = LArPfoHelper::GetVertex(pPfo2);
+    }
+    catch (const StatusCodeException &)
+    {
+    return false;
+    }
 
     const CartesianVector &nuVtx = pNuVertex->GetPosition();
-
-    const CartesianVector vtx1 = pVertex1->GetPosition();
-    const CartesianVector vtx2 = pVertex2->GetPosition();
-
+    const CartesianVector &vtx1 = pVertex1->GetPosition();
+    const CartesianVector &vtx2 = pVertex2->GetPosition();
     const float distNuVtxToVtx1 = (nuVtx - vtx1).GetMagnitude();
     const float distNuVtxToVtx2 = (nuVtx - vtx2).GetMagnitude();
     
-    if  (distNuVtxToVtx1 > 3.f && distNuVtxToVtx2 > 3.f)
+    if  (distNuVtxToVtx1 > m_maxVtxPfosSeparation && distNuVtxToVtx2 > m_maxVtxPfosSeparation)
       return false;
 
     const Pfo * pStub = nullptr;
@@ -161,12 +174,10 @@ bool ShowerMergingPfoMopUpAlgorithm::HaveSameVertex(const ParticleFlowObject *co
     if ( distNuVtxToVtx1 < distNuVtxToVtx2) 
         { pStub = pPfo1;
           pShw = pPfo2;
-          invert = false;
         }
      else 
         { pStub = pPfo2;
           pShw = pPfo1;
-          invert = true;
         }
 
     if (!LArPfoHelper::IsShower(pShw))
@@ -175,11 +186,8 @@ bool ShowerMergingPfoMopUpAlgorithm::HaveSameVertex(const ParticleFlowObject *co
     CaloHitList StubHits, ShwHits;
     LArPfoHelper::GetCaloHits(pStub, TPC_3D, StubHits);
     LArPfoHelper::GetCaloHits(pShw, TPC_3D, ShwHits);
-
-    
     float largestDist = -1;
     const CaloHit  *pStubEndPoint = nullptr;
-
     for ( const CaloHit * const pCaloHit: StubHits)
        {
          const CartesianVector &pos = pCaloHit->GetPositionVector();
@@ -188,33 +196,27 @@ bool ShowerMergingPfoMopUpAlgorithm::HaveSameVertex(const ParticleFlowObject *co
             { 
               largestDist = endPointDist;
               pStubEndPoint = pCaloHit;
-       
             }
         }
     
     const float distNuVtxToStubEnd = ( nuVtx - pStubEndPoint->GetPositionVector()).GetMagnitude();
     const float distNuVtxToShwStart = ( nuVtx - ShwVtx ).GetMagnitude();
-
     if ( distNuVtxToShwStart < distNuVtxToStubEnd)
       return false;
-
-
     const float StubShwSep = ( ShwVtx - pStubEndPoint->GetPositionVector() ).GetMagnitudeSquared(); 
-    
-
-    return StubShwSep<m_stubShowerSeperation;
-
+    return StubShwSep<m_stubShowerSeparation;
 }
 
 //------------------------------------------------------------------------------------------------------------
 
 StatusCode ShowerMergingPfoMopUpAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
 {
-    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadVectorOfValues(xmlHandle, "InputPfoListNames", m_inputPfoListNames));
-    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "AlignmentAngle", m_alignmentAngle));
-    PANDORA_RETURN_RESULT_IF(STATUS_CODE_SUCCESS, !=, XmlHelper::ReadValue(xmlHandle, "StubShowerSeperation", m_stubShowerSeperation));   
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadVectorOfValues(xmlHandle, "InputPfoListNames", m_inputPfoListNames));
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "AlignmentAngle", m_alignmentAngle));
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "MaxVtxPfosSeparation", m_maxVtxPfosSeparation));
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "StubShowerSeparation", m_stubShowerSeparation));   
     
     return PfoMopUpBaseAlgorithm::ReadSettings(xmlHandle);
 }
 
-}
+} // namespace lar_content
