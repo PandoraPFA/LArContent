6d84af41d91a6f69922ce9f32ef1bbabc39f4253
AndyChappell <andrew.chappell@warwick.ac.uk>
2026-01-12 02:45:42 -0600

    Format code

--- larpandoracontent/LArThreeDReco/LArPfoMopUp/ShowerMergingPfoMopUpAlgorithm.cc
+++ larpandoracontent/LArThreeDReco/LArPfoMopUp/ShowerMergingPfoMopUpAlgorithm.cc
@@ -2,18 +2,18 @@
 *  @file   larpandoracontent/LArThreeDReco/LArPfoMopUp/ShowerMergingPfoMopUpAlgorithm.cc
 *
 *  @brief  Implementation of the shower merging pfo mop up algorithm class.
-*  
+*
 *  $Log: $
 */
 
 #include "Pandora/AlgorithmHeaders.h"
 
+#include "larpandoracontent/LArHelpers/LArClusterHelper.h"
+#include "larpandoracontent/LArHelpers/LArGeometryHelper.h"
+#include "larpandoracontent/LArHelpers/LArMCParticleHelper.h"
 #include "larpandoracontent/LArHelpers/LArPfoHelper.h"
 #include "larpandoracontent/LArHelpers/LArPointingClusterHelper.h"
-#include "larpandoracontent/LArHelpers/LArGeometryHelper.h"
-#include "larpandoracontent/LArHelpers/LArClusterHelper.h"
 #include "larpandoracontent/LArObjects/LArPfoObjects.h"
-#include "larpandoracontent/LArHelpers/LArMCParticleHelper.h"
 
 #include "larpandoracontent/LArThreeDReco/LArPfoMopUp/ShowerMergingPfoMopUpAlgorithm.h"
 
@@ -35,19 +35,19 @@ StatusCode ShowerMergingPfoMopUpAlgorithm::Run()
 {
     PfoVector sortedPfos;
     this->GetSortedPfos(sortedPfos);
-    std::set < const ParticleFlowObject * > deletedPfos;
+    std::set<const ParticleFlowObject *> deletedPfos;
 
     for (const ParticleFlowObject *const pPfo1 : sortedPfos)
-    {  
-       if (!pPfo1 || deletedPfos.find(pPfo1) != deletedPfos.end())
-           continue;
-        
-       for (const ParticleFlowObject *const pPfo2 : sortedPfos)
-        {   
-           if (!pPfo2 || pPfo1 == pPfo2 || deletedPfos.find(pPfo2) != deletedPfos.end())
-               continue;
-            
-           // Only consider merging secondary PFOs; skip if either has no parent   
+    {
+        if (!pPfo1 || deletedPfos.find(pPfo1) != deletedPfos.end())
+            continue;
+
+        for (const ParticleFlowObject *const pPfo2 : sortedPfos)
+        {
+            if (!pPfo2 || pPfo1 == pPfo2 || deletedPfos.find(pPfo2) != deletedPfos.end())
+                continue;
+
+            // Only consider merging secondary PFOs; skip if either has no parent
             if (pPfo1->GetParentPfoList().empty() || pPfo2->GetParentPfoList().empty())
                 continue;
 
@@ -59,27 +59,27 @@ StatusCode ShowerMergingPfoMopUpAlgorithm::Run()
             if (!isVertexAssociated)
                 continue;
 
-             bool invert{false};
-             
-             for (const Pfo* const daughter : pPfo2->GetDaughterPfoList())
-             {    
-                     if (daughter == pPfo1)
-                     {    
-                        invert=true;
-                        break;
-                     }
-               }
-               
-                if (invert)
-                    this->MergeAndDeletePfos(pPfo2, pPfo1);
-                else 
-                    this->MergeAndDeletePfos(pPfo1, pPfo2);
-                
-                deletedPfos.insert(pPfo1);
-                deletedPfos.insert(pPfo2);
-                break;
+            bool invert{false};
+
+            for (const Pfo *const daughter : pPfo2->GetDaughterPfoList())
+            {
+                if (daughter == pPfo1)
+                {
+                    invert = true;
+                    break;
+                }
             }
+
+            if (invert)
+                this->MergeAndDeletePfos(pPfo2, pPfo1);
+            else
+                this->MergeAndDeletePfos(pPfo1, pPfo2);
+
+            deletedPfos.insert(pPfo1);
+            deletedPfos.insert(pPfo2);
+            break;
         }
+    }
     return STATUS_CODE_SUCCESS;
 }
 
@@ -88,9 +88,9 @@ StatusCode ShowerMergingPfoMopUpAlgorithm::Run()
 void ShowerMergingPfoMopUpAlgorithm::GetSortedPfos(PfoVector &sortedPfos) const
 {
     for (const std::string &listName : m_inputPfoListNames)
-    {   
+    {
         const PfoList *pPfoList(nullptr);
-        
+
         if (STATUS_CODE_SUCCESS == PandoraContentApi::GetList(*this, listName, pPfoList))
             sortedPfos.insert(sortedPfos.end(), pPfoList->begin(), pPfoList->end());
     }
@@ -103,25 +103,25 @@ bool ShowerMergingPfoMopUpAlgorithm::AreDirectionsAligned(const ParticleFlowObje
 {
     if (pPfo1->GetVertexList().empty() || pPfo2->GetVertexList().empty())
         return false;
-    try 
+    try
     {
-         const Vertex *const pVertex1 = LArPfoHelper::GetVertex(pPfo1);
-         const Vertex *const pVertex2 = LArPfoHelper::GetVertex(pPfo2);
+        const Vertex *const pVertex1 = LArPfoHelper::GetVertex(pPfo1);
+        const Vertex *const pVertex2 = LArPfoHelper::GetVertex(pPfo2);
 
-         if (!pVertex1 || !pVertex2)
-             return false;
+        if (!pVertex1 || !pVertex2)
+            return false;
 
-         const LArShowerPCA pca1 = LArPfoHelper::GetPrincipalComponents(pPfo1,pVertex1);
-         const LArShowerPCA pca2 = LArPfoHelper::GetPrincipalComponents(pPfo2,pVertex2);
+        const LArShowerPCA pca1 = LArPfoHelper::GetPrincipalComponents(pPfo1, pVertex1);
+        const LArShowerPCA pca2 = LArPfoHelper::GetPrincipalComponents(pPfo2, pVertex2);
 
-         const CartesianVector dir1 = pca1.GetPrimaryAxis();
-         const CartesianVector dir2 = pca2.GetPrimaryAxis();
-         const float cosAngle = dir1.GetUnitVector().GetDotProduct(dir2.GetUnitVector());
-         return (cosAngle > m_alignmentAngle);
+        const CartesianVector dir1 = pca1.GetPrimaryAxis();
+        const CartesianVector dir2 = pca2.GetPrimaryAxis();
+        const float cosAngle = dir1.GetUnitVector().GetDotProduct(dir2.GetUnitVector());
+        return (cosAngle > m_alignmentAngle);
     }
     catch (const StatusCodeException &)
     {
-    return false;
+        return false;
     }
 }
 
@@ -130,23 +130,23 @@ bool ShowerMergingPfoMopUpAlgorithm::AreDirectionsAligned(const ParticleFlowObje
 bool ShowerMergingPfoMopUpAlgorithm::IsPfoVertexAssociated(const ParticleFlowObject *const pPfo1, const ParticleFlowObject *const pPfo2) const
 {
     if (pPfo1->GetVertexList().empty() || pPfo2->GetVertexList().empty())
-     return false;
-    
+        return false;
+
     const Pfo *pNu(nullptr);
     const Vertex *pNuVertex(nullptr);
     const Vertex *pVertex1(nullptr);
     const Vertex *pVertex2(nullptr);
 
-    try 
+    try
     {
-      pNu = LArPfoHelper::GetParentNeutrino(pPfo1);
-      pNuVertex = LArPfoHelper::GetVertex(pNu);
-      pVertex1 = LArPfoHelper::GetVertex(pPfo1);
-      pVertex2 = LArPfoHelper::GetVertex(pPfo2);
+        pNu = LArPfoHelper::GetParentNeutrino(pPfo1);
+        pNuVertex = LArPfoHelper::GetVertex(pNu);
+        pVertex1 = LArPfoHelper::GetVertex(pPfo1);
+        pVertex2 = LArPfoHelper::GetVertex(pPfo2);
     }
     catch (const StatusCodeException &)
     {
-    return false;
+        return false;
     }
 
     const CartesianVector &nuVtx = pNuVertex->GetPosition();
@@ -154,24 +154,26 @@ bool ShowerMergingPfoMopUpAlgorithm::IsPfoVertexAssociated(const ParticleFlowObj
     const CartesianVector &vtx2 = pVertex2->GetPosition();
     const float distNuVtxToVtx1 = (nuVtx - vtx1).GetMagnitude();
     const float distNuVtxToVtx2 = (nuVtx - vtx2).GetMagnitude();
-    
-    if  (distNuVtxToVtx1 > m_maxVtxPfosSeparation && distNuVtxToVtx2 > m_maxVtxPfosSeparation)
-      return false;
 
-    const Pfo * pStub = nullptr;
-    const Pfo * pShw = nullptr;
-   
-    const CartesianVector StubVtx = distNuVtxToVtx1 < distNuVtxToVtx2 ? vtx1:vtx2;
-    const CartesianVector ShwVtx = distNuVtxToVtx1 < distNuVtxToVtx2 ? vtx2:vtx1;
+    if (distNuVtxToVtx1 > m_maxVtxPfosSeparation && distNuVtxToVtx2 > m_maxVtxPfosSeparation)
+        return false;
 
-    if ( distNuVtxToVtx1 < distNuVtxToVtx2) 
-        { pStub = pPfo1;
-          pShw = pPfo2;
-        }
-     else 
-        { pStub = pPfo2;
-          pShw = pPfo1;
-        }
+    const Pfo *pStub = nullptr;
+    const Pfo *pShw = nullptr;
+
+    const CartesianVector StubVtx = distNuVtxToVtx1 < distNuVtxToVtx2 ? vtx1 : vtx2;
+    const CartesianVector ShwVtx = distNuVtxToVtx1 < distNuVtxToVtx2 ? vtx2 : vtx1;
+
+    if (distNuVtxToVtx1 < distNuVtxToVtx2)
+    {
+        pStub = pPfo1;
+        pShw = pPfo2;
+    }
+    else
+    {
+        pStub = pPfo2;
+        pShw = pPfo1;
+    }
 
     if (!LArPfoHelper::IsShower(pShw))
         return false;
@@ -180,35 +182,38 @@ bool ShowerMergingPfoMopUpAlgorithm::IsPfoVertexAssociated(const ParticleFlowObj
     LArPfoHelper::GetCaloHits(pStub, TPC_3D, StubHits);
     LArPfoHelper::GetCaloHits(pShw, TPC_3D, ShwHits);
     float largestDist = -1;
-    const CaloHit  *pStubEndPoint = nullptr;
-    for ( const CaloHit * const pCaloHit: StubHits)
-       {
-         const CartesianVector &pos = pCaloHit->GetPositionVector();
-         const float endPointDist = ( pos - StubVtx).GetMagnitudeSquared(); 
-         if ( endPointDist > largestDist)
-            { 
-              largestDist = endPointDist;
-              pStubEndPoint = pCaloHit;
-            }
+    const CaloHit *pStubEndPoint = nullptr;
+    for (const CaloHit *const pCaloHit : StubHits)
+    {
+        const CartesianVector &pos = pCaloHit->GetPositionVector();
+        const float endPointDist = (pos - StubVtx).GetMagnitudeSquared();
+        if (endPointDist > largestDist)
+        {
+            largestDist = endPointDist;
+            pStubEndPoint = pCaloHit;
         }
-    
-    const float distNuVtxToStubEnd = ( nuVtx - pStubEndPoint->GetPositionVector()).GetMagnitude();
-    const float distNuVtxToShwStart = ( nuVtx - ShwVtx ).GetMagnitude();
-    if ( distNuVtxToShwStart < distNuVtxToStubEnd)
-      return false;
-    const float StubShwSep = ( ShwVtx - pStubEndPoint->GetPositionVector() ).GetMagnitudeSquared(); 
-    return StubShwSep<m_stubShowerSeparationSquared;
+    }
+
+    const float distNuVtxToStubEnd = (nuVtx - pStubEndPoint->GetPositionVector()).GetMagnitude();
+    const float distNuVtxToShwStart = (nuVtx - ShwVtx).GetMagnitude();
+    if (distNuVtxToShwStart < distNuVtxToStubEnd)
+        return false;
+    const float StubShwSep = (ShwVtx - pStubEndPoint->GetPositionVector()).GetMagnitudeSquared();
+    return StubShwSep < m_stubShowerSeparationSquared;
 }
 
 //------------------------------------------------------------------------------------------------------------
 
 StatusCode ShowerMergingPfoMopUpAlgorithm::ReadSettings(const TiXmlHandle xmlHandle)
 {
-    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadVectorOfValues(xmlHandle, "InputPfoListNames", m_inputPfoListNames));
+    PANDORA_RETURN_RESULT_IF_AND_IF(
+        STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadVectorOfValues(xmlHandle, "InputPfoListNames", m_inputPfoListNames));
     PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "AlignmentAngle", m_alignmentAngle));
-    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "MaxVtxPfosSeparation", m_maxVtxPfosSeparation));
-    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "StubShowerSeparationSquared", m_stubShowerSeparationSquared));   
-    
+    PANDORA_RETURN_RESULT_IF_AND_IF(
+        STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=, XmlHelper::ReadValue(xmlHandle, "MaxVtxPfosSeparation", m_maxVtxPfosSeparation));
+    PANDORA_RETURN_RESULT_IF_AND_IF(STATUS_CODE_SUCCESS, STATUS_CODE_NOT_FOUND, !=,
+        XmlHelper::ReadValue(xmlHandle, "StubShowerSeparationSquared", m_stubShowerSeparationSquared));
+
     return PfoMopUpBaseAlgorithm::ReadSettings(xmlHandle);
 }
 
--- larpandoracontent/LArThreeDReco/LArPfoMopUp/ShowerMergingPfoMopUpAlgorithm.h
+++ larpandoracontent/LArThreeDReco/LArPfoMopUp/ShowerMergingPfoMopUpAlgorithm.h
@@ -2,7 +2,7 @@
 *  @file   larpandoracontent/LArThreeDReco/LArPfoMopUp/ShowerMergingPfoMopUpAlgorithm.h
 *
 *  @brief  Header file for  the shower merging pfo mop up algorithm class.
-*  
+*
 *  $Log: $
 */
 #ifndef LAR_SHOWER_MERGING_ALGORITHM_H
@@ -14,7 +14,7 @@ namespace lar_content
 {
 
 /**
- *  @brief ShowerMergingPfoMopUpAlgorithm class 
+ *  @brief ShowerMergingPfoMopUpAlgorithm class
  */
 class ShowerMergingPfoMopUpAlgorithm : public PfoMopUpBaseAlgorithm
 {
@@ -26,42 +26,40 @@ public:
 
 private:
     pandora::StatusCode Run();
-    
+
     /**
      *  @brief Get the sorted list of input particle flow objects (PFOs)
      *
-     *  @param sortedPfos to receive the sorted list of input pfos 
+     *  @param sortedPfos to receive the sorted list of input pfos
      */
     void GetSortedPfos(pandora::PfoVector &sortedPfos) const;
-    
-     /**
-      *  @brief Check if the directions of pfo1 and pfo2 are aligned 
+
+    /**
+      *  @brief Check if the directions of pfo1 and pfo2 are aligned
       *
       *  @param pPfo1 Pointer to the first particle flow object
       *  @param pPfo2 Pointer to the second particle flow object
-      *  return boolean 
+      *  return boolean
       */
     bool AreDirectionsAligned(const pandora::ParticleFlowObject *const pPfo1, const pandora::ParticleFlowObject *const pPfo2) const;
-   
+
     /**
-     *  @brief Check if two pfos can be associated as a upstream stub and a downstream shower pair 
+     *  @brief Check if two pfos can be associated as a upstream stub and a downstream shower pair
      *
      *  @param pPfo1 Pointer to the first particle flow object
      *  @param pPfo2 Pointer to the second particle flow object
-     *  return boolean 
+     *  return boolean
      */
     bool IsPfoVertexAssociated(const pandora::ParticleFlowObject *const pPfo1, const pandora::ParticleFlowObject *const pPfo2) const;
 
     pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
-    pandora::StringVector m_inputPfoListNames;   ///< The input pfo list names 
-    float m_alignmentAngle;                      ///< The maximum angular separation between two pfos
-    float m_maxVtxPfosSeparation;                ///< The maximum distance between the neutrino vertex and start of the pfo 
-    float m_stubShowerSeparationSquared;         ///< The square of the maximum distance between the upstream stub and the downstream shower 
-
-
+    pandora::StringVector m_inputPfoListNames; ///< The input pfo list names
+    float m_alignmentAngle;                    ///< The maximum angular separation between two pfos
+    float m_maxVtxPfosSeparation;              ///< The maximum distance between the neutrino vertex and start of the pfo
+    float m_stubShowerSeparationSquared;       ///< The square of the maximum distance between the upstream stub and the downstream shower
 };
 
-}
+} // namespace lar_content
 
 #endif
