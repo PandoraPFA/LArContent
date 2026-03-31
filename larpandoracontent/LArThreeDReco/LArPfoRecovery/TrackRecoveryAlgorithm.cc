9ec242e13c85ea2b14d52f5dd10b8f77e374829c
Andrew Chappell <A.Chappell@warwick.ac.uk>
2026-03-31 15:20:09 +0100

    Identify candidate hits missing from PFOs

--- larpandoracontent/LArThreeDReco/LArPfoRecovery/TrackRecoveryAlgorithm.cc
+++ larpandoracontent/LArThreeDReco/LArPfoRecovery/TrackRecoveryAlgorithm.cc
@@ -11,6 +11,7 @@
 #include "larpandoracontent/LArHelpers/LArClusterHelper.h"
 #include "larpandoracontent/LArHelpers/LArGeometryHelper.h"
 #include "larpandoracontent/LArHelpers/LArPfoHelper.h"
+#include "larpandoracontent/LArObjects/LArPlaneContextObject.h"
 
 #include "larpandoracontent/LArThreeDReco/LArPfoRecovery/TrackRecoveryAlgorithm.h"
 
@@ -37,7 +38,9 @@ StatusCode TrackRecoveryAlgorithm::Run()
     PfoToViewClusterMap pfoToViewClusterMap;
     ClusterToPfoMap clusterToPfoMap;
     ClusterToHitMap clusterToHitMap;
+    ClusterToFitMap clusterToFitMap;
     HitToClusterToMap hitToClusterMap;
+    ViewToHitsMap viewToHitsMap;
     for (const Pfo *const pPfo : *pPfoList)
     {
         for (const HitType view : {TPC_VIEW_U, TPC_VIEW_V, TPC_VIEW_W, TPC_3D})
@@ -50,13 +53,73 @@ StatusCode TrackRecoveryAlgorithm::Run()
                 pfoToViewClusterMap[pPfo][view] = pCluster;
                 clusterToPfoMap[pCluster] = pPfo;
                 if (view != TPC_3D)
-                    this->FitAndOrderCluster(pCluster, clusterToHitMap[pCluster]);
+                {
+                    auto result = this->FitAndOrderCluster(pCluster, clusterToHitMap[pCluster]);
+                    if (result)
+                        clusterToFitMap.emplace(pCluster, *result);
+                }
                 else
                     LArClusterHelper::GetAllHits(pCluster, clusterToHitMap[pCluster]);
                 for (const CaloHit *const pCaloHit : clusterToHitMap[pCluster])
+                {
                     hitToClusterMap[pCaloHit] = pCluster;
+                    viewToHitsMap[view].emplace_back(pCaloHit);
+                }
             }
         }
+    }
+
+    // Loop over the PFOs and project 3D hits from two views into the third view to look for unassociated hits
+    PANDORA_MONITORING_API(SetEveDisplayParameters(this->GetPandora(), true, DETECTOR_VIEW_XZ, -1.f, 1.f, 1.f));
+    for (const Pfo *const pPfo : *pPfoList)
+    {
+        const Cluster *const pClusterU{pfoToViewClusterMap[pPfo][TPC_VIEW_U]}, *const pClusterV{pfoToViewClusterMap[pPfo][TPC_VIEW_V]},
+            *const pClusterW{pfoToViewClusterMap[pPfo][TPC_VIEW_W]};
+        const CaloHitList &hitsU{clusterToHitMap[pClusterU]}, &hitsV{clusterToHitMap[pClusterV]}, &hitsW{clusterToHitMap[pClusterW]};
+
+        CaloHitSet unmatchedHitsU, unmatchedHitsV, unmatchedHitsW;
+        CaloHitList mergeHitsU, mergeHitsV, mergeHitsW;
+        this->FindUnmatchedHits(hitsU, hitsV, hitsW, unmatchedHitsV, unmatchedHitsW);
+        this->FindUnmatchedHits(hitsV, hitsU, hitsW, unmatchedHitsU, unmatchedHitsW);
+        this->FindUnmatchedHits(hitsW, hitsU, hitsV, unmatchedHitsU, unmatchedHitsV);
+
+        std::cout << "PFO has " << unmatchedHitsU.size() << " unmatched U hits, "
+                  << unmatchedHitsV.size() << " unmatched V hits and " << unmatchedHitsW.size() << " unmatched W hits." << std::endl;
+
+        // Construct 3D hits from the combinatorics of each view pair and project into the third view
+        PANDORA_MONITORING_API(VisualizeCaloHits(this->GetPandora(), &hitsU, "U", RED));
+        PANDORA_MONITORING_API(VisualizeCaloHits(this->GetPandora(), &hitsV, "V", GREEN));
+        PANDORA_MONITORING_API(VisualizeCaloHits(this->GetPandora(), &hitsW, "W", BLUE));
+
+        this->IdentifyHitsToMerge(pClusterU, hitsU, unmatchedHitsU, clusterToFitMap, mergeHitsU);
+        this->IdentifyHitsToMerge(pClusterV, hitsV, unmatchedHitsV, clusterToFitMap, mergeHitsV);
+        this->IdentifyHitsToMerge(pClusterW, hitsW, unmatchedHitsW, clusterToFitMap, mergeHitsW);
+
+        for (const CaloHit *const pCaloHit : mergeHitsU)
+        {
+            const CartesianVector position{pCaloHit->GetPositionVector()};
+            PANDORA_MONITORING_API(AddMarkerToVisualization(this->GetPandora(), &position, "Merge U", RED, 2.f));
+            float rL{0.f}, rT{0.f};
+            clusterToFitMap.at(pClusterU).GetLocalPosition(position, rL, rT);
+            std::cout << "Merge U hit has rL = " << rL << " and rT = " << rT << std::endl;
+        }
+        for (const CaloHit *const pCaloHit : mergeHitsV)
+        {
+            const CartesianVector position{pCaloHit->GetPositionVector()};
+            PANDORA_MONITORING_API(AddMarkerToVisualization(this->GetPandora(), &position, "Merge V", GREEN, 2.f));
+            float rL{0.f}, rT{0.f};
+            clusterToFitMap.at(pClusterV).GetLocalPosition(position, rL, rT);
+            std::cout << "Merge V hit has rL = " << rL << " and rT = " << rT << std::endl;
+        }
+        for (const CaloHit *const pCaloHit : mergeHitsW)
+        {
+            const CartesianVector position{pCaloHit->GetPositionVector()};
+            PANDORA_MONITORING_API(AddMarkerToVisualization(this->GetPandora(), &position, "Merge W", BLUE, 2.f));
+            float rL{0.f}, rT{0.f};
+            clusterToFitMap.at(pClusterW).GetLocalPosition(position, rL, rT);
+            std::cout << "Merge W hit has rL = " << rL << " and rT = " << rT << std::endl;
+        }
+        PANDORA_MONITORING_API(ViewEvent(this->GetPandora()));
     }
 
     return STATUS_CODE_SUCCESS;
@@ -64,16 +127,87 @@ StatusCode TrackRecoveryAlgorithm::Run()
 
 //------------------------------------------------------------------------------------------------------------------------------------------
 
-void TrackRecoveryAlgorithm::FitAndOrderCluster(const Cluster *const pCluster, CaloHitList &orderedHits) const
+std::optional<TwoDSlidingFitResult> TrackRecoveryAlgorithm::FitAndOrderCluster(const Cluster *const pCluster, CaloHitList &orderedHits) const
 {
     if (pCluster->GetNCaloHits() < 3)
     {
         LArClusterHelper::GetAllHits(pCluster, orderedHits);
-        return;
+        return std::nullopt;
     }
     const HitType view{LArClusterHelper::GetClusterHitType(pCluster)};
     TwoDSlidingFitResult sfr(pCluster, 3, LArGeometryHelper::GetWirePitch(this->GetPandora(), view));
     LArClusterHelper::OrderHitsAlongTrajectory(pCluster, sfr, orderedHits);
+
+    return sfr;
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void TrackRecoveryAlgorithm::FindUnmatchedHits(const CaloHitList &hitsA, const CaloHitList &hitsB, const CaloHitList &hitsC, CaloHitSet &unmatchedHitsB,
+    CaloHitSet &unmatchedHitsC) const
+{
+    if (hitsA.empty() || hitsB.empty() || hitsC.empty())
+        return;
+    const LArPlaneContextObject *pPlaneContextObject{dynamic_cast<const LArPlaneContextObject*>(PandoraContentApi::GetEventContextObject(
+        *this, "PlaneContext"))};
+    const HitType viewB{hitsB.front()->GetHitType()}, viewC{hitsC.front()->GetHitType()};
+    for (const CaloHit *const pCaloHitA : hitsA)
+    {
+        const LArPlaneContextObject::HitTriplet *pTriplet{pPlaneContextObject->GetHitTriplet(pCaloHitA)};
+        if (!pTriplet)
+            continue;
+        const CaloHit *const pHitB{viewB == TPC_VIEW_W ? pTriplet->m_wHit : viewB == TPC_VIEW_V ? pTriplet->m_vHit : pTriplet->m_uHit};
+        const CaloHit *const pHitC{viewC == TPC_VIEW_W ? pTriplet->m_wHit : viewC == TPC_VIEW_V ? pTriplet->m_vHit : pTriplet->m_uHit};
+        auto bIterator{std::find(hitsB.begin(), hitsB.end(), pHitB)};
+        auto cIterator{std::find(hitsC.begin(), hitsC.end(), pHitC)};
+        // Only flag hits as unmatched if the other hits in the triplet are present in the PFO
+        if (bIterator == hitsB.end() && cIterator != hitsC.end())
+            unmatchedHitsB.insert(pHitB);
+        if (cIterator == hitsC.end() && bIterator != hitsB.end())
+            unmatchedHitsC.insert(pHitC);
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------
+
+void TrackRecoveryAlgorithm::IdentifyHitsToMerge(const Cluster *pCluster, const CaloHitList &clusterHits, const CaloHitSet &unmatchedHits,
+    const ClusterToFitMap &clusterToFitMap, CaloHitList &mergeHits) const
+{
+    if (pCluster && (clusterHits.size() > 1) && (clusterToFitMap.find(pCluster) != clusterToFitMap.end()))
+    {
+        float rLFront{0.f}, rLBack{0.f}, dummy{0.f};
+        clusterToFitMap.at(pCluster).GetLocalPosition(clusterHits.front()->GetPositionVector(), rLFront, dummy);
+        clusterToFitMap.at(pCluster).GetLocalPosition(clusterHits.back()->GetPositionVector(), rLBack, dummy);
+        for (const CaloHit *const pCaloHit : unmatchedHits)
+        {
+            float rL{0.f};
+            clusterToFitMap.at(pCluster).GetLocalPosition(pCaloHit->GetPositionVector(), rL, dummy);
+            if (rLFront <= rL && rL <= rLBack)
+            {
+                // We're along the track, look for this hit acting as a blocking path
+                for (auto iter = clusterHits.begin(); iter != std::prev(clusterHits.end()); ++iter)
+                {
+                    const CaloHit *const pHit1{*iter}, *const pHit2{*(std::next(iter))};
+                    if (LArClusterHelper::HasBlockedPath({pCaloHit}, pHit1, pHit2))
+                    {
+                        mergeHits.emplace_back(pCaloHit);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                // We're outside the track, extend it
+                mergeHits.emplace_back(pCaloHit);
+            }
+        }
+    }
+    else
+    {
+        // Entire view is missing, or has less than 3 hits, so we should merge all unmatched hits
+        for (const CaloHit *const pCaloHit : unmatchedHits)
+            mergeHits.emplace_back(pCaloHit);
+    }
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------
--- larpandoracontent/LArThreeDReco/LArPfoRecovery/TrackRecoveryAlgorithm.h
+++ larpandoracontent/LArThreeDReco/LArPfoRecovery/TrackRecoveryAlgorithm.h
@@ -10,6 +10,8 @@
 
 #include "Pandora/Algorithm.h"
 
+#include <optional>
+
 namespace lar_content
 {
 
@@ -28,12 +30,12 @@ private:
     typedef std::unordered_map<pandora::HitType, const pandora::Cluster *> ViewToClusterMap;
     typedef std::unordered_map<const pandora::Pfo *, ViewToClusterMap> PfoToViewClusterMap;
     typedef std::unordered_map<const pandora::Cluster *, pandora::CaloHitList> ClusterToHitMap;
+    typedef std::unordered_map<const pandora::Cluster *, const TwoDSlidingFitResult> ClusterToFitMap;
     typedef std::unordered_map<const pandora::CaloHit *, const pandora::Cluster *> HitToClusterToMap;
     typedef std::unordered_map<const pandora::Cluster *, const pandora::Pfo *> ClusterToPfoMap;
+    typedef std::unordered_map<pandora::HitType, pandora::CaloHitList> ViewToHitsMap;
 
     pandora::StatusCode Run();
-
-    pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
     /**
      *  @brief  Performs a sliding linear fit to get an ordered set of hits along a cluster trajectory.
@@ -41,7 +43,39 @@ private:
      *  @param  pCluster the cluster for which hits should be ordered
      *  @param  orderedHits the list in which to store the ordered hits
      */
-    void FitAndOrderCluster(const pandora::Cluster *const pCluster, pandora::CaloHitList &orderedHits) const;
+    std::optional<TwoDSlidingFitResult> FitAndOrderCluster(const pandora::Cluster *const pCluster, pandora::CaloHitList &orderedHits) const;
+
+    /**
+     *  @brief  Identifies hits that are missing from the PFO based on independent triplet matching. hitsA acts as the reference list to look
+     *          up the hit triplets. Hits are deemed missing if two of the three hits in a triplet are present in the PFO.
+     *
+     *  @param  hitsA the list of hits in the reference view
+     *  @param  hitsB the list of hits in the second view
+     *  @param  hitsC the list of hits in the third view
+     *  @param  unmatchedHitsB the set in which to store hits missing in only the second view
+     *  @param  unmatchedHitsC the set in which to store hits missing in only the third view
+     */
+    void FindUnmatchedHits(const pandora::CaloHitList &hitsA, const pandora::CaloHitList &hitsB, const pandora::CaloHitList &hitsC,
+        pandora::CaloHitSet &unmatchedHitsB, pandora::CaloHitSet &unmatchedHitsC) const;
+
+    /**
+     *  @brief  Identifies hits to merge from a collection of hits provisionally identified as being missing from a cluster.
+     *          If a candidate hit lies along the existing set of hits, we look for a gap in the existing set that could be filled by this hit.
+     *          If we find one, we consider this a valid addition. Alternatively, if a candidate hit lies outside the existing set of hits, we
+     *          assume this is an extension of the track and consider this a valid addition. If there are no existing hits in the cluster, we
+     *          assume the view was missing, and add all hits. The key assumption here is that the unmatched hits are already considered good
+     *          candidates based on the triplet matching.
+     *
+     *  @param  pCluster the cluster for which to identify hits to merge
+     *  @param  clusterHits the list of hits already associated to the cluster
+     *  @param  unmatchedHits the set of hits not in the cluster, under consideration for merging
+     *  @param  clusterToFitMap the map containing sliding fit results for clusters
+     *  @param  mergeHits the list in which to store hits to merge
+     */
+    void IdentifyHitsToMerge(const pandora::Cluster *pCluster, const pandora::CaloHitList &clusterHits, const pandora::CaloHitSet &unmatchedHits,
+        const ClusterToFitMap &clusterToFitMap, pandora::CaloHitList &mergeHits) const;
+
+    pandora::StatusCode ReadSettings(const pandora::TiXmlHandle xmlHandle);
 
     std::string m_inputPfoListName; ///< The name of the input PFO list containing track-like PFOs
     pandora::StringVector m_inputClusterListNames; ///< The list of cluster list names to consider when looking for clusters to recover
