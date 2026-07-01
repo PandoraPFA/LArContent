d96e9afa72ab4c4e076a069ca458346a948ec8f9
Isobel Mawby <isobel.mawby@gmail.com>
2026-07-01 07:36:47 -0500

    PR comments

--- larpandoracontent/LArMetrics/ShowerValidationTool.cc
+++ larpandoracontent/LArMetrics/ShowerValidationTool.cc
@@ -314,7 +314,7 @@ void ShowerValidationTool::GetRecoVertexInfo(const CartesianVector &recoShrVtx, 
     if (pMC)
     {
         const CartesianVector trueDir(pMC->GetMomentum().GetUnitVector());
-        float recoShrDirAcc(m_invalidLargeFloat);
+        float recoShrDirAcc(m_invalidAngle);
         try { recoShrDirAcc = trueDir.GetOpeningAngle(recoShrDir); } 
         catch (StatusCodeException &) { recoShrDirAcc = m_invalidLargeFloat; }
         showerTreeVars.m_recoShrDirAcc.push_back(recoShrDirAcc);
